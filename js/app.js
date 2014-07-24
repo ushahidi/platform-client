b4d9b7fadfa81eb4c9edd5fe2452764b989c8311
Seth Hall <seth@ushahidi.com>
2014-07-24 16:06:09 -0400

    Topbar Subnav
    * adds topbar views subnav

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
 // Workspace Menu
 
 $(document).ready(function(){
-  $('.js-menu-trigger').on('click touchstart', function(e){
-    $('.js-menu').toggleClass('is-visible');
-    $('.js-menu-screen').toggleClass('is-visible');
-    e.preventDefault();
-  });
+	$('.js-menu-trigger').on('click touchstart', function(e){
+		$('.js-menu').toggleClass('is-visible');
+		$('.js-menu-screen').toggleClass('is-visible');
+		e.preventDefault();
+	});
 
-  $('.js-menu-screen').on('click touchstart', function(e){
-    $('.js-menu').toggleClass('is-visible');
-    $('.js-menu-screen').toggleClass('is-visible');
-    e.preventDefault();
-  });
+	$('.js-menu-screen').on('click touchstart', function(e){
+		$('.js-menu').toggleClass('is-visible');
+		$('.js-menu-screen').toggleClass('is-visible');
+		e.preventDefault();
+	});
+});
+
+// Views Submenu
+$(document).ready(function(){
+	$('.js-views-subnav--link').on('click touchstart', function(e){
+		$('.js-views-subnav').toggleClass('subnav');
+	});
+	// $('.js-views-menu-link').toggleClass('active');
 });
