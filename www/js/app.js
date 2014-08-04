d3bad21886f6c6770fd3cd8fbe857dcd341504a8
Seth Hall <seth@ushahidi.com>
2014-08-04 12:05:30 -0400

    Cleaned up App.js
    * remove unnecessary `$(document).ready(function(){`

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
-// Workspace Menu
-
 $(document).ready(function(){
+
+// Workspace Menu
 	$('.js-menu-trigger').on('click touchstart', function(e){
 		$('.js-menu').toggleClass('is-visible');
 		$('.js-menu-screen').toggleClass('is-visible');
@@ -12,24 +12,21 @@ $(document).ready(function(){
 		$('.js-menu-screen').toggleClass('is-visible');
 		e.preventDefault();
 	});
-});
 
 // Views Submenu
-$(document).ready(function(){
 	$('.js-views-subnav--link').on('click touchstart', function(e){
 		$('.js-views-subnav').toggleClass('subnav');
 	});
 	// $('.js-views-menu-link').toggleClass('active');
-});
 
 //Dropdown Button
-$(document).ready(function(){
-$('.dropdown-button').on('click touchstart', function(e){
-	console.log('test');
-	$('.menu').toggleClass('show-menu');
-	$('.menu > li').on('click touchstart', function(e){
-	$('.dropdown-button').html($(this).html());
-	$('.menu').removeClass('show-menu');
+	$('.dropdown-button').on('click touchstart', function(e){
+		console.log('test');
+		$('.menu').toggleClass('show-menu');
+		$('.menu > li').on('click touchstart', function(e){
+		$('.dropdown-button').html($(this).html());
+		$('.menu').removeClass('show-menu');
+		});
 	});
-});
+
 });
