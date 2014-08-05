2ceb6ba82cb09d5579f2125c989aeb59a97a375a
Seth Hall <seth@ushahidi.com>
2014-08-05 13:56:12 -0400

    Main Content & Sidebar
    * adds main and sidebar markup and styles
    
    * adds accordion tabs component
    
    * styles homepage sidebar
    
    * accordion tabs JS

--- www/js/app.js
+++ www/js/app.js
@@ -27,4 +27,23 @@ $(document).ready(function(){
 		});
 	});
 
+// Accordion Tabs
+	$('.accordion-tabs').each(function(index) {
+		$(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+	});
+
+	$('.accordion-tabs').on('click', 'li > a', function(event) {
+		if (!$(this).hasClass('is-active')) {
+			event.preventDefault();
+			var accordionTabs = $(this).closest('.accordion-tabs')
+			accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+			$(this).next().toggleClass('is-open').toggle();
+			accordionTabs.find('.is-active').removeClass('is-active');
+			$(this).addClass('is-active');
+		} else {
+			event.preventDefault();
+		}
+	});
+
 });
