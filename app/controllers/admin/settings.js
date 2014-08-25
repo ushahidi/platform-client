132d8ef61cf2f90b29adbfed639895ba8c7ee0ea
Woody Gilk <woody@ushahidi.com>
2014-08-24 19:21:17 -0500

    Add NG stub for settings page, refs T645
    Summary:
    - uses cards for features
    - basic CSS hooked up
    
    Test Plan:
    - click settings page
    - hover cards
    
    Reviewers: eyedol, rjmackay
    
    Reviewed By: rjmackay
    
    Subscribers: srutto, bodacea, dkobia
    
    Maniphest Tasks: T645
    
    Differential Revision: https://phabricator.ushahidi.com/D291

---
+++ app/controllers/admin/settings.js
@@ -0,0 +1,7 @@
+module.exports = function($scope) {
+    $scope.title = 'Admin: Settings';
+    $scope.site = {
+        name: 'Ushahidi Site',
+        owner: 'Your Name'
+    };
+};
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 module.exports = function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
     $routeProvider
@@ -9,5 +10,9 @@ module.exports = function($routeProvider, $locationProvider) {
         .when('/posts/detail', {
             controller: require('./controllers/posts/detail.js'),
             template: fs.readFileSync(__dirname + '/templates/posts/detail.html')
+        })
+        .when('/settings', {
+            controller: require('./controllers/admin/settings.js'),
+            template: fs.readFileSync(__dirname + '/templates/admin/settings.html')
         });
 };
