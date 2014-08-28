feea696a97b1afce0c6d886ef2c6131fbda5b9c9
Seth Hall <seth@ushahidi.com>
2014-08-28 13:52:33 -0400

    Post Detail
    Summary:
    * adds post detail markup and styles
        * markup in `detail.html`
        * sass in `post-detail.scss` & `sidebar--post-detail`
    
    * adjusts @import order and files
    
    * other small html & sass changes
    
    Test Plan:
    * load post detail
    * check markup
    * resize browser for responsive views
    
    Reviewers: rjmackay, shadowhand
    
    Reviewed By: shadowhand
    
    Subscribers: sebm90, digitalafrican, eyedol, lkamau, srutto, dkobia
    
    Differential Revision: https://phabricator.ushahidi.com/D295

--- app/partials.js
+++ app/partials.js
@@ -4,4 +4,5 @@ var fs = require('fs');
 module.exports = angular.module('partials', [])
     .run(['$templateCache', function ($templateCache) {
         $templateCache.put('partials/search-bar.html', fs.readFileSync(__dirname + '/templates/partials/search-bar.html', { encoding : 'utf8' }));
+        $templateCache.put('partials/footer.html', fs.readFileSync(__dirname + '/templates/partials/footer.html', { encoding : 'utf8' }));
     }]);
