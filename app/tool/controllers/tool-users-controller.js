db99eb2c907f1ca5eab9e56dd33c552d8e7ad4fc
Daniel Spaude <daniel@spaude.de>
2015-01-21 11:16:11 +0100

    tools/users: prevent loggedin user from deleting herself or change her own role; fixes T1244
    Summary: * also make sure that filterRole is finally called (also in cases of rejected ajax promises / failed delete or changeRole requests)
    
    Test Plan:
    * go to tools/users
    * select a few users, including yourself
    * try to change the role of the selected users or try to delete the selected users
    * app should inform you that this is not possible
    * everything should work like before when own user is not part of the selection
    
    Reviewers: aMoniker, rjmackay
    
    Reviewed By: rjmackay
    
    Maniphest Tasks: T1244
    
    Differential Revision: https://phabricator.ushahidi.com/D613

--- app/tool/controllers/tool-users-controller.js
+++ app/tool/controllers/tool-users-controller.js
@@ -2,17 +2,27 @@ module.exports = [
     '$scope',
     '$translate',
     '$q',
+    '_',
+    '$window',
     'gravatar',
+    'Session',
     'UserEndpoint',
+    'Notify',
     'RoleHelper',
 function(
     $scope,
     $translate,
     $q,
+    _,
+    $window,
     gravatar,
+    Session,
     UserEndpoint,
+    Notify,
     RoleHelper
 ) {
+    var handleResponseErrors, checkAndNotifyAboutManipulateOwnUser;
+
     $translate('api.users').then(function(title){
         $scope.title = title;
     });
@@ -35,31 +45,63 @@ function(
         }
     };
 
+
+    handleResponseErrors = function(errorResponse){
+        var errors = _.pluck(errorResponse.data && errorResponse.data.errors, 'message');
+        errors && Notify.showAlerts(errors);
+    };
+
+    checkAndNotifyAboutManipulateOwnUser = function(translationKey) {
+        var currentUserId = Session.getSessionDataEntry('userId');
+        if(_.contains($scope.selectedUsers, currentUserId))
+        {
+            $translate(translationKey).then(function(message){
+                Notify.showSingleAlert(message);
+            });
+            return true;
+        }
+        return false;
+    };
+
     $scope.deleteUsers = function() {
+
+        if(checkAndNotifyAboutManipulateOwnUser('user.cannot_delete_yourself'))
+        {
+            return;
+        }
+
         $translate('notify.user.bulk_destroy_confirm', {
             count: $scope.selectedUsers.length
         }).then(function(message) {
-            if (window.confirm(message)) {
+            if ($window.confirm(message)) {
                 var calls = [];
                 angular.forEach($scope.selectedUsers, function(userId) {
                     calls.push( UserEndpoint.delete({ id: userId }).$promise );
                 });
-                $q.all(calls).then($scope.filterRole);
+
+                $q.all(calls).then($scope.filterRole, handleResponseErrors)
+                .finally($scope.filterRole);
             }
         });
     };
 
     $scope.changeRole = function(role) {
+        if(checkAndNotifyAboutManipulateOwnUser('user.cannot_change_your_own_role'))
+        {
+            return;
+        }
+
         $translate('notify.user.bulk_role_change_confirm', {
             count: $scope.selectedUsers.length,
             role:  role.display_name
         }).then(function(message) {
-            if (window.confirm(message)) {
+            if ($window.confirm(message)) {
                 var calls = [];
                 angular.forEach($scope.selectedUsers, function(userId) {
                     calls.push( UserEndpoint.update({ id: userId }, { id: userId, role: role.name }).$promise );
                 });
-                $q.all(calls).then($scope.filterRole);
+                $q.all(calls).then($scope.filterRole, handleResponseErrors)
+                .finally($scope.filterRole);
             }
         });
     };
