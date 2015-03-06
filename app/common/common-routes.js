8437f88188aef51804d2e8b88c8c6b02381ec128
Robbie Mackay <rm@robbiemackay.com>
2015-03-06 15:21:21 +1300

    Remove defunct code
    - add-to-set controller, route and views
    - search-bar partial
    - Links to site settings in user menu

--- app/common/common-routes.js
+++ app/common/common-routes.js
@@ -10,10 +10,6 @@ module.exports = ['$routeProvider', '$locationProvider', function($routeProvider
             controller: require('./controllers/login-controller.js'),
             templateUrl: 'templates/login.html'
         })
-        .when('/posts/add-to-set', {
-            controller: require('./controllers/sets/add-to-set.js'),
-            templateUrl: 'templates/sets/add-to-set.html'
-        })
         .when('/map-settings', {
             controller: require('./controllers/admin/map-settings.js'),
             templateUrl: 'templates/admin/map-settings.html'
