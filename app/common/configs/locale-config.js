e6eb60910d0e160f9abf6ec13daee78e277c627d
Robbie Mackay <rm@robbiemackay.com>
2015-05-13 11:52:55 +1200

    Fix translation sanitization strategy

--- app/common/configs/locale-config.js
+++ app/common/configs/locale-config.js
@@ -1,6 +1,7 @@
 module.exports = ['$translateProvider', function ($translateProvider) {
+    $translateProvider.useSanitizeValueStrategy('escaped');
+
     $translateProvider.translations('en', require('../locales/en.json'));
 
     $translateProvider.preferredLanguage('en');
-
 }];
