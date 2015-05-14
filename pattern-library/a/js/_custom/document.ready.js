3b140e743d998353472cccb81c0f3a77486d7d42
Robbie Mackay <rm@robbiemackay.com>
2015-05-14 15:08:08 +1200

    Remove old pattern-library

--- pattern-library/a/js/_custom/document.ready.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('html').removeClass('no-js').addClass('js');
-	$('.ui-forms').uiForms();
-	tabsInit();
-	toggleInit();
-	removePattern();
-	drawerInit();
-});
--- pattern-library/a/js/_custom/remove.js
+++
@@ -1,7 +0,0 @@
-removePattern = function() {
-	$(document).on('click', '.removable-trigger', function(){
-		$(this).closest('.removable').fadeOut('fast', function(){
-			$(this).remove();
-		});
-	});
-}
