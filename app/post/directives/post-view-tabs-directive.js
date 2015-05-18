39d9618d91dfcfa540806b80a0cf4915f6ec9c87
Robbie Mackay <rm@robbiemackay.com>
2015-05-18 14:34:27 +1200

    Update view tabs

--- app/post/directives/post-view-tabs-directive.js
+++ app/post/directives/post-view-tabs-directive.js
@@ -10,7 +10,7 @@ function (
         templateUrl: 'templates/partials/post-view-tabs.html',
         link: function ($scope, $element, $attrs) {
             // Define available views
-            $scope.views = ['list', 'map', 'graph', 'timeline'];
+            $scope.views = ['list', 'map', 'chart', 'timeline'];
         }
     };
 }];
