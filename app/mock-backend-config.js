cc6c7c293631c7e48aa0d3eab3a7734e5952c8b4
Robbie Mackay <rm@robbiemackay.com>
2015-08-06 11:19:23 +1200

    Handle client creds auth in mock backend, and update site config data

--- app/mock-backend-config.js
+++ app/mock-backend-config.js
@@ -29,7 +29,16 @@ angular.module('e2e-mocks', ['ngMockE2E'])
 
         $httpBackend.whenPOST(CONST.BACKEND_URL + '/oauth/token').respond(function (method, url, data) {
             var reqPayload = JSON.parse(data);
-            if (reqPayload.username === 'admin' && reqPayload.password === 'admin') {
+            if (reqPayload.grant_type === 'password' && reqPayload.username === 'admin' && reqPayload.password === 'admin') {
+                return [200, {
+                    'access_token': 'UmexrkSXVsHeEzGH1TMjYjvX344iB94XZK34nIVw',
+                    'token_type': 'Bearer',
+                    'expires': 2414253574,
+                    'expires_in': 3600,
+                    'refresh_token': 'o1sw8yr6b8BuH00RlIEeLv3v75bzZWZfymquNlKs',
+                    'refresh_token_expires_in': 604800
+                }, {}];
+            } else if (reqPayload.grant_type === 'client_credentials') {
                 return [200, {
                     'access_token': 'UmexrkSXVsHeEzGH1TMjYjvX344iB94XZK34nIVw',
                     'token_type': 'Bearer',
