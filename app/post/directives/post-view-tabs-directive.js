c74ec819f3ba61ed1e232ea16be97d5e36f905a7
Holly Corbett <holly.corbett@gmail.com>
2015-09-01 22:12:01 -0300

    only display enabled views

--- app/post/directives/post-view-tabs-directive.js
+++ app/post/directives/post-view-tabs-directive.js
@@ -1,7 +1,9 @@
 module.exports = [
     '$location',
+    'ConfigEndpoint',
 function (
-    $location
+    $location,
+    ConfigEndpoint
 ) {
     return {
         restrict: 'E',
@@ -12,13 +14,23 @@ function (
         },
         templateUrl: 'templates/posts/post-view-tabs.html',
         link: function ($scope, $element, $attrs) {
-            // Define available views
-            $scope.views = [
-                'list',
-                'map',
-                'chart',
-                'timeline'
-            ];
+
+            $scope.views = [];
+
+            // Get available views from backend
+            ConfigEndpoint.get({ id: 'features' }).$promise.then(function (response) {
+
+                if (response.post_view_map)
+                    $scope.views.push('map');
+                if (response.post_view_list)
+                    $scope.views.push('list');
+                if (response.post_view_chart)
+                    $scope.views.push('chart');
+                if (response.post_view_timeline)
+                    $scope.views.push('timeline');
+
+            });
+
         }
     };
 }];
