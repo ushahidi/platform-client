af97888409519365f468f0f08945b743d82f8042
Vaibhav <bhawsar.vaibhav@gmail.com>
2015-09-18 15:56:34 +0530

    activity timeline widget

--- app/activity/activity-module.js
+++ app/activity/activity-module.js
@@ -1,6 +1,7 @@
 angular.module('ushahidi.activity', [])
+
 .config(require('./activity-routes.js'))
 
+.service('PostEndpoint', require('../post/services/endpoints/post-endpoint.js'))
 
-.service('PostEndpoint', require('../post/services/endpoints/post-endpoint.js'));
-
+.directive('activityTimeline', require('./directives/activity-timeline-directive.js'));
