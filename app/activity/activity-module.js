57652c3618fdc32140c94893ce9a9c8849ec41ec
Vaibhav <bhawsar.vaibhav@gmail.com>
2015-09-24 13:39:32 +0530

    activity: removed redundant module require

--- app/activity/activity-module.js
+++ app/activity/activity-module.js
@@ -2,6 +2,4 @@ angular.module('ushahidi.activity', [])
 
 .config(require('./activity-routes.js'))
 
-.service('PostEndpoint', require('../post/services/endpoints/post-endpoint.js'))
-
 .directive('activityTimeline', require('./directives/activity-timeline-directive.js'));
