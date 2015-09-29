b0a95f0f37b0d41947fc4f48803fa272a10d40fd
Holly Corbett <holly.corbett@gmail.com>
2015-09-28 21:17:34 -0300

    remove old changes the hack-job way

--- app/post/directives/post-view-tabs-directive.js
+++ app/post/directives/post-view-tabs-directive.js
@@ -1,13 +1,10 @@
 module.exports = [
     '$location',
-    'ConfigEndpoint',
 function (
-    $location,
-    ConfigEndpoint
+    $location
 ) {
     return {
         restrict: 'E',
-        replace: true,
         scope: {
             active: '=',
             baseUrl: '@'
@@ -15,22 +12,13 @@ function (
         templateUrl: 'templates/posts/post-view-tabs.html',
         link: function ($scope, $element, $attrs) {
 
-            $scope.views = [];
-
-            // Get available views from backend
-            ConfigEndpoint.get({ id: 'features' }).$promise.then(function (response) {
-
-                if (response.post_view_map)
-                    $scope.views.push('map');
-                if (response.post_view_list)
-                    $scope.views.push('list');
-                if (response.post_view_chart)
-                    $scope.views.push('chart');
-                if (response.post_view_timeline)
-                    $scope.views.push('timeline');
-
-            });
-
+            // Defiine available views
+            $scope.views = [
+                'list',
+                'map',
+                'chart',
+                'timeline'
+            ];
         }
     };
 }];
