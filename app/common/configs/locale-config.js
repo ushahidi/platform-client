6bb7a7415e934f98dc1acaaf2b7c4b25e7ba9f0c
Kyoungrok Jang <kyoungrok.jang@gmail.com>
2015-10-19 17:48:01 +0900

    $translateProvider.translations('ko-KR', require('../locales/ko-KR.json'));

--- app/common/configs/locale-config.js
+++ app/common/configs/locale-config.js
@@ -2,6 +2,7 @@ module.exports = ['$translateProvider', function ($translateProvider) {
     $translateProvider.useSanitizeValueStrategy('escaped');
 
     $translateProvider.translations('en', require('../locales/en.json'));
+    $translateProvider.translations('ko-KR', require('../locales/ko-KR.json'));
 
     $translateProvider.useStaticFilesLoader({
         prefix: 'locales/',
