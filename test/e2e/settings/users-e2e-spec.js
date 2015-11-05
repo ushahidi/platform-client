78b05512f32ed1abcebdadb29dd0347eb594a991
will <will.doran@riseup.net>
2015-11-05 18:08:39 -0500

    retesting return of text from div

--- test/e2e/settings/users-e2e-spec.js
+++ test/e2e/settings/users-e2e-spec.js
@@ -93,10 +93,8 @@ describe('users management', function () {
                                         browser.sleep(500);
                                     });
                                     it('shows an error alert that you cannot change your own role (the user as which your are signed in)', function () {
-                                        element(by.css('#alert-modal-text')).get(0).getText().then(function (text) {
-                                            text.toEqual('You cannot change your own role');
-                                            element(by.css("button#alert-modal-ok")).click();
-                                        });
+                                        expect(element(by.css('#alert-modal-text')).getText()).toEqual('You cannot change your own role');
+                                        element(by.css("button#alert-modal-ok")).click();
                                     });
                                 });
                             });
@@ -115,10 +113,8 @@ describe('users management', function () {
                                 });
 
                                 it('shows an error alert that you cannot delete your own user (the user as which your are signed in)', function () {
-                                    element(by.css('#alert-modal-text')).getText().then(function (text) {
-                                        text.toEqual('You cannot delete your own user');
-                                        element(by.css("button#alert-modal-ok")).click();
-                                    });
+                                    expect(element(by.css('#alert-modal-text')).getText())toEqual('You cannot delete your own user');
+                                    element(by.css("button#alert-modal-ok")).click();
                                 });
                             });
                         });
@@ -153,10 +149,8 @@ describe('users management', function () {
                                         browser.sleep(500);
                                     });
                                     it('shows an alert which asks if you really want to change the roles', function () {
-                                        element(by.css('#confirm-modal-text')).getText().then(function (text) {
-                                            text.toEqual('Are you sure you want to change the role of 4 users to Member?');
-                                            element(by.css("button#confirm-modal-ok")).click();
-                                        });
+                                        expect(element(by.css('#confirm-modal-text')).getText()).toEqual('Are you sure you want to change the role of 4 users to Member?');
+                                        element(by.css("button#confirm-modal-ok")).click();
                                     });
                                 });
 
