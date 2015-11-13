cacf204fecfd37784271feac9a27bb0187577e35
Robbie Mackay <rm@robbiemackay.com>
2015-11-13 13:14:07 +1300

    Make plans module us menu helper

--- app/plans/plans-module.js
+++ app/plans/plans-module.js
@@ -1,4 +1,12 @@
 angular.module('ushahidi.plans', [
+	'ushahidi.common'
 ])
 
-.config(require('./plans-routes.js'));
+.config(require('./plans-routes.js'))
+.config(['MenuHelperProvider', function (MenuHelperProvider) {
+    MenuHelperProvider.addMenuItem('settings', {
+        url : '/settings/plan',
+        text : 'nav.plan_settings'
+    });
+}])
+;
