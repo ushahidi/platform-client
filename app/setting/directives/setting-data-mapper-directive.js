983ccebc65fb600b1a384773455ef28f333b19f8
will <will.doran@riseup.net>
2015-12-11 16:43:45 -0500

    added csv duplicate mapping alter

--- app/setting/directives/setting-data-mapper-directive.js
+++ app/setting/directives/setting-data-mapper-directive.js
@@ -28,6 +28,29 @@ function (
                 csv.maps_to = _.map(csv.maps_to, function (item) {
                     return item ? item.key : null;
                 });
+                
+                var dups = _.countBy(csv.maps_to, function (item) {
+                    return item;     
+                });
+
+
+                var duplicateVars = _.filter(csv.maps_to, function (item) {
+                    if (dups[item] > 1) {
+                        return item; 
+                    };
+                });
+
+                duplicateVars = _.uniq(duplicateVars);
+
+                if(duplicateVars.length > 0) {
+
+                    $translate('notify.data_import.duplicate_fields', {duplicates: duplicateVars.join(', ')}).then(
+                    function (message) {
+                        Notify.showAlerts([message]);
+                    });
+                    return;
+                };
+                
                 csv.completed = true;
                 csv.unmapped = [];
                 DataImportEndpoint.update(csv)
