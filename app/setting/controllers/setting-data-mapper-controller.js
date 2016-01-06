efde2136a3f4efac79344e57fc557f4ca8d07ded
will <will.doran@riseup.net>
2016-01-05 22:53:20 -0500

    fixed linting

--- app/setting/controllers/setting-data-mapper-controller.js
+++ app/setting/controllers/setting-data-mapper-controller.js
@@ -25,23 +25,23 @@ function (
     /*
      * This function returns a map of csv columns to post_type fields.
      * It checks for the presences of the columns names in the set of attribute labels.
-     * If the column is not present the entry is set to undefined. 
+     * If the column is not present the entry is set to undefined.
      *
-     * The match is case insensistive. 
+     * The match is case insensistive.
      */
     $scope.autoMap = function (columns, attributes, mapSize) {
         // Get set of labels
-        var attributeLabels = _.map(attributes, function (attribute){
-            return attribute.label.toLowerCase();  
+        var attributeLabels = _.map(attributes, function (attribute) {
+            return attribute.label.toLowerCase();
         });
-        
+
         // Create map of labels to attributes
         var attributeMap = _.object(attributeLabels, attributes);
-        
+
         // Check if a column name appears in the set of labels, if it does set a mapping
         // to the attribute, otherwise set the mapping to undefined.
         return _.map(columns, function (item, index) {
-            return _.contains(attributeLabels, item.toLowerCase())? attributeMap[item] : undefined;
+            return _.contains(attributeLabels, item.toLowerCase()) ? attributeMap[item] : undefined;
         });
     };
 
