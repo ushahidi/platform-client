dc0304c35dec757c3ec1b95591d7ae5367681106
will <will.doran@riseup.net>
2016-02-05 21:15:45 -0500

    add select toggle for roles

--- app/setting/directives/setting-roles-directive.js
+++ app/setting/directives/setting-roles-directive.js
@@ -15,7 +15,14 @@ function (
             handleResponseErrors = function (errorResponse) {
                 Notify.showApiErrors(errorResponse);
             };
+        
+            $scope.refreshView = function () {
+                $scope.roles = RoleEndpoint.query();
+                $scope.selectedRoles = [];
+            };
             
+            $scope.refreshView();
+           
             $scope.selectedRoles = [];
 
             $scope.deleteRoles = function () {
@@ -32,9 +39,24 @@ function (
                             $translate('notify.user.bulk_destroy_success').then(function (message) {
                                 Notify.showNotificationSlider(message);
                             });
+
+                            $scope.refreshView();
                         }, handleResponseErrors);
                     }, function () {});
                 });
+            };
+
+            $scope.isToggled = function (role) {
+                return $scope.selectedRoles.indexOf(role.id) > -1;
+            };
+
+            $scope.toggleRole = function (role) {
+                var idx = $scope.selectedRoles.indexOf(role.id);
+                if (idx > -1) {
+                    $scope.selectedRoles.splice(idx, 1);
+                } else {
+                    $scope.selectedRoles.push(role.id);
+                }
             };
         }
     };
