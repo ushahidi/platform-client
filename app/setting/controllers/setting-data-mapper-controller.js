8c4f1f332e9d14f17371040ac4eba3b4c914c496
will <will.doran@riseup.net>
2016-02-16 18:16:48 -0500

    adding csv instructions and required field protection

--- app/setting/controllers/setting-data-mapper-controller.js
+++ app/setting/controllers/setting-data-mapper-controller.js
@@ -1,10 +1,22 @@
 module.exports = [
     '$scope',
     'initialData',
+    '_',
 function (
     $scope,
-    initialData
+    initialData,
+    _
 ) {
+    $scope.required_fields = [];
+    $scope.required_fields_map = {};
     $scope.form = initialData.form;
+    // Change required attribute labels to show required
+    _.each($scope.form.attributes, function (attribute, index) {
+        if (attribute.required) {
+            $scope.required_fields.push(attribute.key);
+            $scope.required_fields_map[attribute.key] = attribute.label;
+            $scope.form.attributes[index].label = attribute.label + ' *';
+        }
+    });
     $scope.csv = initialData.csv;
 }];
