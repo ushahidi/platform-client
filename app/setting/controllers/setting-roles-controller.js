405db9f3784bb866323a5522eef805b9f2983f01
will <will.doran@riseup.net>
2016-02-17 13:58:38 -0500

    fixed page title

--- app/setting/controllers/setting-roles-controller.js
+++ app/setting/controllers/setting-roles-controller.js
@@ -1,10 +1,17 @@
 module.exports = [
     '$scope',
+    '$translate',
     'RoleEndpoint',
     'PermissionEndpoint',
 function (
     $scope,
+    $translate,
     RoleEndpoint,
     PermissionEndpoint
 ) {
+    $translate('tool.manage_roles').then(function (title) {
+        $scope.title = title;
+        $scope.$emit('setPageTitle', title);
+    });
+
 }];
