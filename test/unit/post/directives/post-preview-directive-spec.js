bfe3de4f5ae9f2b3476e6780a51f4276fe8d3280
will <will.doran@riseup.net>
2016-02-22 10:24:01 -0500

    adding more directives

---
+++ test/unit/post/directives/post-preview-directive-spec.js
@@ -0,0 +1,77 @@
+var ROOT_PATH = '../../../../';
+
+describe('post preview directive', function () {
+
+    var $rootScope,
+        $scope,
+        isolateScope,
+        GlobalFilter,
+        Notify,
+        element;
+
+    beforeEach(function () {
+        fixture.setBase('mocked_backend/api/v3');
+
+        require(ROOT_PATH + 'test/unit/mock/mock-modules.js');
+
+        var testApp = angular.module('testApp', [
+            'ushahidi.mock',
+            'pascalprecht.translate'
+        ]);
+
+        testApp.directive('postPreview', require(ROOT_PATH + 'app/post/directives/post-preview-directive'));
+
+        require(ROOT_PATH + 'test/unit/simple-test-app-config')(testApp);
+
+        angular.mock.module('testApp');
+    });
+
+    beforeEach(angular.mock.module('client-templates'));
+
+    beforeEach(inject(function (_$rootScope_, $compile, _Notify_, _GlobalFilter_) {
+        $rootScope = _$rootScope_;
+        $scope = _$rootScope_.$new();
+
+        GlobalFilter = _GlobalFilter_;
+        Notify = _Notify_;
+
+        $scope.post = fixture.load('posts/120.json');
+
+        element = '<post-preview post="post"></post-preview>';
+        element = $compile(element)($scope);
+        $scope.$digest();
+        isolateScope = element.children().scope();
+
+    }));
+
+    describe('test directive functions', function () {
+        it('should publish a post to a given role', function () {
+            spyOn(Notify, 'showNotificationSlider');
+
+            $scope.post.id = 'pass';
+            isolateScope.publishPostTo($scope.post);
+            expect(Notify.showNotificationSlider).toHaveBeenCalled();
+        });
+
+        it('should fail to publish a post to a given role', function () {
+            spyOn(Notify, 'showApiErrors');
+
+            $scope.post.id = 'fail';
+            isolateScope.publishPostTo($scope.post);
+            expect(Notify.showApiErrors).toHaveBeenCalled();
+        });
+
+        it('should fail to publish a post to a given role when a required stage is not completed', function () {
+            spyOn(Notify, 'showAlerts');
+
+            $scope.post.id = 'pass';
+            isolateScope.stages[1].required = true;
+            isolateScope.post.completed_stages = [];
+
+            isolateScope.publishPostTo($scope.post);
+            expect(Notify.showAlerts).toHaveBeenCalled();
+        });
+
+
+    });
+});
