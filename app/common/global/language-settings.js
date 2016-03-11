3e881eb768c081cb63120e2a1802e414c5857102
will <will.doran@riseup.net>
2016-03-11 14:59:19 -0500

    wrapping lang setup to avoid race condition with lang value

--- app/common/global/language-settings.js
+++ app/common/global/language-settings.js
@@ -2,34 +2,30 @@ module.exports = [
     '$rootScope',
     '$translate',
     'ConfigEndpoint',
-    'BootstrapConfig',
     'Languages',
 function (
     $rootScope,
     $translate,
     ConfigEndpoint,
-    BootstrapConfig,
     Languages
 ) {
-    var lang = BootstrapConfig.language || 'en-US';
-
-    ConfigEndpoint.get({ id: 'site' }).$promise.then(function (site) {
-        lang = site.language ? site.language : 'en-US';
-    });
 
     $rootScope.rtlEnabled = false;
 
-    $translate.use(lang).then(function (langKey) {
-        if (langKey) {
-            $translate.preferredLanguage(lang);
+    ConfigEndpoint.get({ id: 'site' }).$promise.then(function (site) {
+        var lang = site.language ? site.language : 'en-US';
+        $translate.use(lang).then(function (langKey) {
+            if (langKey) {
+                $translate.preferredLanguage(lang);
 
-            Languages.then(function (languages) {
-                angular.forEach(languages, function (language) {
-                    if (language.code === lang) {
-                        $rootScope.rtlEnabled = language.rtl;
-                    }
+                Languages.then(function (languages) {
+                    angular.forEach(languages, function (language) {
+                        if (language.code === lang) {
+                            $rootScope.rtlEnabled = language.rtl;
+                        }
+                    });
                 });
-            });
-        }
+            }
+        });
     });
 }];
