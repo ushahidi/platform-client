36d3edc634f6f4f531ecd5391fc2a67b674b1774
will <will.doran@riseup.net>
2016-03-15 14:57:21 -0400

    fixed linting

--- test/unit/mock/services/post.js
+++ test/unit/mock/services/post.js
@@ -88,7 +88,7 @@ module.exports = [function () {
                             ]
                         }
                     });
-                 }
+                }
             }};
         }
     };
