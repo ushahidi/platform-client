a29b4c9870bfb3be896742fb4005a32932de234a
Seth Hall <seth@ushahidi.com>
2016-05-05 12:45:40 -0400

    REVISION: html updates/frame-routes/mode-bar active states
    * html updates from PL changes
    * starts frame-routes
    * ModeViewHelper to assist with nav active clas
    * revises mode-bar
      * sets active states per layout
      * sets translations
    * revises svg-icons gulp task

--- app/frame/directives/mode-bar.js
+++ app/frame/directives/mode-bar.js
@@ -1,5 +1,7 @@
 module.exports = [
+    'ModeViewHelper',
 function (
+    ModeViewHelper
 ) {
     return {
         restrict: 'E',
@@ -8,6 +10,14 @@ function (
         },
         templateUrl: 'templates/frame/mode-bar.html',
         link: function ($scope, $element, $attrs) {
+            $scope.views = ModeViewHelper.views();
+
+            // TODO: move this out of function
+            $scope.activeView = 'map';
+            $scope.setActiveView = function(view_name) {
+                $scope.activeView = view_name;
+            };
+
         }
     };
 }];
