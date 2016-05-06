3b7725589606ba8b4801083ff960f1e777004a1d
Robbie Mackay <rm@robbiemackay.com>
2016-05-06 21:22:50 +1200

    Fix issue in test.sh so we can actually pass a suite to protractor

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ set -ev
 
 if [ "${TEST_SUITE}" = "e2e" ]; then
 	if [ "${PROTRACTOR_SUITE}" != "" ]; then
-		npm run protractor -- "--suite $PROTRACTOR_SUITE"
+		npm run protractor -- --suite $PROTRACTOR_SUITE
 	else
     	npm run protractor
     fi
