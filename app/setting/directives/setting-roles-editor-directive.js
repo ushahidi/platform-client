752ded0898af37d45de2da167ed9a727a7d925dc
will <will.doran@riseup.net>
2016-05-09 13:49:24 -0400

    Move delete to role editor directive

--- app/setting/directives/setting-roles-editor-directive.js
+++ app/setting/directives/setting-roles-editor-directive.js
@@ -53,6 +53,45 @@ function (
                 });
                 $scope.processing = false;
             };
+
+            var handleResponseErrors = function (errorResponse) {
+                Notify.showApiErrors(errorResponse);
+            };
+
+            $scope.checkIfLastAdmin = function () {
+                var admins = 0;
+                _.each($scope.roles, function (role) {
+                    if (role.name === 'admin') {
+                        admins++;
+                    }
+                });
+
+                return admins === 1;
+            };
+
+            $scope.deleteRole = function (role) {
+                if (role.name === 'admin' && $scope.checkIfLastAdmin()) {
+                    $translate('notify.role.last_admin')
+                    .then(function (message) {
+                        Notify.showSingleAlert(message);
+                    });
+                    return;
+                }
+                $translate('notify.role.delete_question', {
+                    role: role.display_name
+                }).then(function (message) {
+                    Notify.showConfirm(message).then(function () {
+                        RoleEndpoint.delete({ id: role.id }).$promise.then(function () {
+                            $translate('notify.role.destroy_success', {
+                                role: role.display_name
+                            }).then(function (message) {
+                                Notify.showNotificationSlider(message);
+                            });
+                            $location.path($scope.whereToNext);
+                        }, handleResponseErrors);
+                    }, function () {});
+                });
+            };
         }
     };
 }];
