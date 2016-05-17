cd9afdc1a37ead04eef1b7a6746d1ff87fc0aab1
Seth Hall <seth@ushahidi.com>
2016-05-17 11:22:23 -0400

    REVISIONS: settings
    * updates locales
    * adds `setLayout` function for body classes
    * stubs out simple mode-context

--- app/setting/controllers/setting-controller.js
+++ app/setting/controllers/setting-controller.js
@@ -1,12 +1,16 @@
 module.exports = [
     '$scope',
-    '$translate',
     '$rootScope',
+    '$translate',
 function (
     $scope,
-    $translate,
-    $rootScope
+    $rootScope,
+    $translate
 ) {
+
+    // Change layout class
+    $rootScope.setLayout('layout-c');
+
     $translate('tool.settings').then(function (title) {
         $scope.title = title;
         $rootScope.$emit('setPageTitle', title);
