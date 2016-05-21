cfaae49c456b498e04cdd4c88181d629d19129fd
will <will.doran@riseup.net>
2016-05-21 16:33:10 -0400

    attempting to fix broken html

--- app/common/common-module.js
+++ app/common/common-module.js
@@ -51,10 +51,6 @@ angular.module('ushahidi.common', [
 .directive('colorPicker', require('./directives/color-picker.js'))
 .directive('firstTimeConfig', require('./directives/first-time-config.js'))
 
-.directive('filterSystem', require('./directives/filter-system/filter-system.js'))
-.directive('filterUsers', require('./directives/filter-system/filter-users.js'))
-.directive('filterCategoriess', require('./directives/filter-system/filter-categories.js'))
-.directive('filterPosts', require('./directives/filter-system/filter-posts.js'))
 // Event actions
 .constant('EVENT', {
     ACTIONS : {
@@ -62,6 +58,7 @@ angular.module('ushahidi.common', [
         DELETE : 'delete'
     }
 })
+.directive('filterSearchbar', require('./directives/filter-searchbar/filter-searchbar.js'))
 
 .config(require('./configs/authentication-interceptor.js'))
 .config(require('./configs/locale-config.js'))
