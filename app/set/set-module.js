434e686b7a096b6687dace1a7dd66ed6f1bb1114
Robbie Mackay <rm@robbiemackay.com>
2016-05-31 09:29:56 +1200

    Rename savedsearch directives to saved-search

--- app/set/set-module.js
+++ app/set/set-module.js
@@ -2,10 +2,11 @@ angular.module('ushahidi.sets', [])
 
 .config(require('./set-routes.js'))
 
-.directive('savedsearchEditor', require('./directives/savedsearches/editor-directive.js'))
-.directive('savedsearchCreate', require('./directives/savedsearches/create-directive.js'))
-.directive('savedsearchUpdate', require('./directives/savedsearches/update-directive.js'))
+.directive('savedSearchEditor', require('./directives/savedsearches/editor-directive.js'))
+.directive('savedSearchCreate', require('./directives/savedsearches/create-directive.js'))
+.directive('savedSearchUpdate', require('./directives/savedsearches/update-directive.js'))
 .directive('savedSearchListing', require('./directives/savedsearches/listing.directive.js'))
+.directive('savedSearchModeContext', require('./directives/savedsearches/mode-context.directive.js'))
 
 .directive('collectionModeContext', require('./directives/collections/mode-context.directive.js'))
 .directive('collectionEditor', require('./directives/collections/editor.directive.js'))
