3adcaefb65003ec21cd2bd1193e826a8270480f0
Robbie Mackay <rm@robbiemackay.com>
2016-05-30 14:53:57 +1200

    Wire up real data into saved search dropdown

--- app/set/set-module.js
+++ app/set/set-module.js
@@ -2,10 +2,10 @@ angular.module('ushahidi.sets', [])
 
 .config(require('./set-routes.js'))
 
-.directive('setsMenu', require('./directives/sets-menu-directive.js'))
 .directive('savedsearchEditor', require('./directives/savedsearch-editor-directive.js'))
 .directive('savedsearchCreate', require('./directives/savedsearch-create-directive.js'))
 .directive('savedsearchUpdate', require('./directives/savedsearch-update-directive.js'))
+.directive('savedSearchListing', require('./directives/savedsearches/listing.directive.js'))
 
 .directive('collectionModeContext', require('./directives/collections/mode-context.directive.js'))
 .directive('collectionEditor', require('./directives/collections/editor.directive.js'))
