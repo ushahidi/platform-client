45b194a6a35560395f1271091f6df87363405abc
Jason Mule <jason.mule@gmail.com>
2016-06-02 01:49:14 +0300

    Rename scope to childScope in handler

--- app/user-profile/directives/account-settings.js
+++ app/user-profile/directives/account-settings.js
@@ -15,16 +15,17 @@ function (
         templateUrl: 'templates/users/account_settings.html',
         link: function (scope) {
             var title,
+                childScope,
                 template = '<account-settings ng-show="visible"></account-settings>',
 
                 unbind = $rootScope.$on('event:show:account_settings', function () {
-                    scope = $rootScope.$new();
-                    scope.visible = true;
+                    childScope = $rootScope.$new();
+                    childScope.visible = true;
 
                     UserEndpoint.getFresh({id: 'me'}).$promise
                         .then(function (user) {
                             title = user.realname || user.email;
-                            ModalService.openTemplate(template, title, false, scope, true, true);
+                            ModalService.openTemplate(template, title, false, childScope, true, true);
                         });
                 });
 
