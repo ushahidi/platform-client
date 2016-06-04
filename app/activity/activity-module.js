0b74e74afb1f18933b28665a2d3713b49fe9a6a1
Robbie Mackay <rm@robbiemackay.com>
2016-06-04 13:53:35 +1200

    Fix switched names on chart directives

--- app/activity/activity-module.js
+++ app/activity/activity-module.js
@@ -3,6 +3,6 @@ angular.module('ushahidi.activity', [])
 .config(require('./activity-routes.js'))
 
 .directive('activityTimeline', require('./activity-timeline.directive.js'))
-.directive('activityTimeChart', require('./bar-chart.directive.js'))
-.directive('activityBarChart', require('./time-chart.directive.js'))
+.directive('activityBarChart', require('./bar-chart.directive.js'))
+.directive('activityTimeChart', require('./time-chart.directive.js'))
 ;
