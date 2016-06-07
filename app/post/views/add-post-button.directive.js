d82aaa9af57cb2372dea01d83392313a5bd6269a
Robbie Mackay <rm@robbiemackay.com>
2016-06-07 19:29:27 +1200

    Cleaner toggling of fab style

--- app/post/views/add-post-button.directive.js
+++ app/post/views/add-post-button.directive.js
@@ -19,14 +19,24 @@ function AddPostButtonController(
     $scope,
 	FormEndpoint
 ) {
-
     $scope.forms = [];
     $scope.fabToggle = false;
-    
+    $scope.fabOptionsStyle = { opacity: 0, display: 'none' };
+    $scope.toggleFab = toggleFab;
+
     activate();
 
     function activate() {
         // Load forms
         $scope.forms = FormEndpoint.query();
+    }
+
+    function toggleFab() {
+        $scope.fabToggle = !$scope.fabToggle;
+        if ($scope.fabToggle) {
+            $scope.fabOptionsStyle = { opacity: 1, display: 'flex' };
+        } else {
+            $scope.fabOptionsStyle = { opacity: 0, display: 'none' };
+        }
     }
 }
