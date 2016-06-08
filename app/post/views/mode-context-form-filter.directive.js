d831aa0df1456a736b1fe4fa0a2bd7bbac952c0c
Robbie Mackay <rm@robbiemackay.com>
2016-06-08 17:15:56 +1200

    Add post counts to ModeContextFormFilter
    Fixes ushahidi/platform#1116

--- app/post/views/mode-context-form-filter.directive.js
+++ app/post/views/mode-context-form-filter.directive.js
@@ -10,17 +10,37 @@ function ModeContextFormFilterDirective() {
     };
 }
 
-ModeContextFormFilter.$inject = ['$scope', 'FormEndpoint'];
-function ModeContextFormFilter($scope, FormEndpoint) {
+ModeContextFormFilter.$inject = ['$scope', 'FormEndpoint', 'PostEndpoint', '$q', '_'];
+function ModeContextFormFilter($scope, FormEndpoint, PostEndpoint, $q, _) {
     $scope.forms = [];
     $scope.showOnly = showOnly;
     $scope.hide = hide;
+    $scope.unknown_post_count = 0;
 
     activate();
 
     function activate() {
         // Load forms
         $scope.forms = FormEndpoint.query();
+        var postCountRequest = PostEndpoint.stats({ group_by: 'form' });
+
+        $q.all([$scope.forms.$promise, postCountRequest.$promise]).then(function (responses) {
+            if (!responses[1] || !responses[1].totals || !responses[1].totals[0]) {
+                return;
+            }
+            var values = responses[1].totals[0].values;
+
+            angular.forEach($scope.forms, function (form) {
+                var value = _.findWhere(values, { id: form.id });
+                form.post_count = value ? value.total : 0;
+            });
+
+            // Grab the count for form=null
+            var unknownValue = _.findWhere(values, { id: null });
+            if (unknownValue) {
+                $scope.unknown_post_count = unknownValue.value;
+            }
+        });
     }
 
     function showOnly(formId) {
