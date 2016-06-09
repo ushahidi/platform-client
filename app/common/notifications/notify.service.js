b1cec776068357c27af7d934a0b14494579910e4
Robbie Mackay <rm@robbiemackay.com>
2016-06-09 16:14:49 +1200

    Clean up notify service

--- app/common/notifications/notify.service.js
+++ app/common/notifications/notify.service.js
@@ -1,61 +1,10 @@
-module.exports = [
-    '$window',
-    '_',
-    '$q',
-    '$rootScope',
-function ($window, _, $q, $rootScope) {
+module.exports = Notify;
 
-    var deffered;
+var deferred;
 
-    var showSingleAlert = function (alertMessage) {
-        showAlerts([alertMessage]);
-    };
-
-    var showNotificationSlider = function (message) {
-        $rootScope.$emit('event:show:notification-slider', message);
-    };
-
-    var showLimitSlider = function (message) {
-        $rootScope.$emit('event:show:limit-slider', message);
-    };
-
-    var showAlerts = function (alertMessages, callbackEvent, buttonText, action) {
-        $rootScope.$emit('event:show:alerts', alertMessages, callbackEvent, buttonText, action);
-    };
-
-    var showConfirm = function (message, callbackEvent, buttonText, action) {
-        $rootScope.$emit('event:show:message-confirm', message, callbackEvent, buttonText, action);
-        deffered = $q.defer();
-        return deffered.promise;
-    };
-
-    var showConfirmModal = function (message, callbackEvent, buttonText, action) {
-        $rootScope.$emit('event:show:message-confirm-modal', message, callbackEvent, buttonText, action);
-        deffered = $q.defer();
-        return deffered.promise;
-    };
-
-    var showConfirmSuccess = function (message, callbackEvent, buttonText, action) {
-        $rootScope.$emit('event:show:success-confirm', message, callbackEvent, buttonText, action);
-        deffered = $q.defer();
-        return deffered.promise;
-    };
-
-    var showConfirmAlert = function (message, callbackEvent, buttonText, action) {
-        $rootScope.$emit('event:show:alert-confirm', message, callbackEvent, buttonText, action);
-        deffered = $q.defer();
-        return deffered.promise;
-    };
-
-    var showApiErrors = function (errorResponse) {
-        var errors = _.pluck(errorResponse.data && errorResponse.data.errors, 'message');
-
-        errors && showAlerts(errors);
-    };
-
-    $rootScope.$on('event:confirm:return-confirm', function (event, result) {
-        result ? deffered.resolve(result) : deffered.reject(result);
-    });
+Notify.$inject = ['$window', '_', '$q', '$rootScope'];
+function Notify($window, _, $q, $rootScope) {
+    $rootScope.$on('event:confirm:return-confirm', handleReturnConfirm);
 
     return {
         showSingleAlert: showSingleAlert,
@@ -68,4 +17,54 @@ function ($window, _, $q, $rootScope) {
         showConfirmAlert: showConfirmAlert,
         showLimitSlider: showLimitSlider
     };
-}];
+
+    function showSingleAlert(alertMessage) {
+        showAlerts([alertMessage]);
+    }
+
+    function showNotificationSlider(message) {
+        $rootScope.$emit('event:show:notification-slider', message);
+    }
+
+    function showLimitSlider(message) {
+        $rootScope.$emit('event:show:limit-slider', message);
+    }
+
+    function showAlerts(alertMessages, callbackEvent, buttonText, action) {
+        $rootScope.$emit('event:show:alerts', alertMessages, callbackEvent, buttonText, action);
+    }
+
+    function showConfirm(message, callbackEvent, buttonText, action) {
+        $rootScope.$emit('event:show:message-confirm', message, callbackEvent, buttonText, action);
+        deferred = $q.defer();
+        return deferred.promise;
+    }
+
+    function showConfirmModal(message, callbackEvent, buttonText, action) {
+        $rootScope.$emit('event:show:message-confirm-modal', message, callbackEvent, buttonText, action);
+        deferred = $q.defer();
+        return deferred.promise;
+    }
+
+    function showConfirmSuccess(message, callbackEvent, buttonText, action) {
+        $rootScope.$emit('event:show:success-confirm', message, callbackEvent, buttonText, action);
+        deferred = $q.defer();
+        return deferred.promise;
+    }
+
+    function showConfirmAlert(message, callbackEvent, buttonText, action) {
+        $rootScope.$emit('event:show:alert-confirm', message, callbackEvent, buttonText, action);
+        deferred = $q.defer();
+        return deferred.promise;
+    }
+
+    function showApiErrors(errorResponse) {
+        var errors = _.pluck(errorResponse.data && errorResponse.data.errors, 'message');
+
+        errors && showAlerts(errors);
+    }
+
+    function handleReturnConfirm(event, result) {
+        result ? deferred.resolve(result) : deferred.reject(result);
+    }
+}
