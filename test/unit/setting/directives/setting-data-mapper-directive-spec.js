5446046b1231cf64dbbb657b01f9425f739e7928
Jason Mule <jason.mule@gmail.com>
2016-06-10 22:14:03 +0300

    Spy on error instead of errors

--- test/unit/setting/directives/setting-data-mapper-directive-spec.js
+++ test/unit/setting/directives/setting-data-mapper-directive-spec.js
@@ -52,7 +52,7 @@ describe('setting data mapper directive', function () {
     });
 
     it('should detect duplicate field assignments', function () {
-        spyOn(Notify, 'errors');
+        spyOn(Notify, 'error');
 
         var csv = {
             maps_to: [
@@ -62,11 +62,11 @@ describe('setting data mapper directive', function () {
         };
         $scope.progressToConfigure(csv);
 
-        expect(Notify.errors).toHaveBeenCalled();
+        expect(Notify.error).toHaveBeenCalled();
     });
 
     it('should detect missing field assignments', function () {
-        spyOn(Notify, 'errors');
+        spyOn(Notify, 'error');
 
         var csv = {
             maps_to: [
@@ -82,7 +82,7 @@ describe('setting data mapper directive', function () {
 
         $scope.progressToConfigure(csv);
 
-        expect(Notify.errors).toHaveBeenCalled();
+        expect(Notify.error).toHaveBeenCalled();
     });
 
     it('should cancel the import', function () {
