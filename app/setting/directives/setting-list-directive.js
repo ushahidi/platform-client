3bb896c84f9dfeae95e43d6c6dd2f1a447b7e1af
Robbie Mackay <rm@robbiemackay.com>
2016-06-13 21:07:00 +1200

    Update plans UI + show special plans
    - Rename plan to plans
    - Show legacy and zerorated plan if it applies
    - Show proper name for plan in settings list

--- app/setting/directives/setting-list-directive.js
+++ app/setting/directives/setting-list-directive.js
@@ -27,6 +27,13 @@ function (
             $scope.isAdmin = $rootScope.isAdmin;
             $scope.hasPermission = $rootScope.hasPermission;
             $scope.hasManageSettingsPermission = $rootScope.hasManageSettingsPermission;
+            $scope.tierNames = {
+                'free': 'Mapper',
+                'surveyor': 'Surveyor',
+                'responder': 'Responder',
+                'free-pre-jun-2016': 'Mapper (Legacy)',
+                'zerorated': 'Social Impact'
+            };
 
             Features.loadFeatures().then(function () {
                 $scope.planIsAvailable = Features.isViewEnabled('plan');
