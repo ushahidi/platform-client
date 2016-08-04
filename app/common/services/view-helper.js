939638f9704f6310e05efef9945927182e628bc5
Robbie Mackay <rm@robbiemackay.com>
2016-08-04 16:13:31 +1200

    Update modals and fix collection modal title

--- app/common/services/view-helper.js
+++ app/common/services/view-helper.js
@@ -1,79 +1,24 @@
 module.exports = [
-    '_',
-    'Features',
     '$translate',
-    '$q',
 function (
-    _,
-    Features,
-    $translate,
-    $q
+    $translate
 ) {
     var allViews = [
-            {
-                name: 'map',
-                display_name: $translate.instant('views.map')
-            },
-            {
-                name: 'list',
-                display_name: $translate.instant('views.list')
-            },
-            {
-                name: 'activity',
-                display_name: $translate.instant('views.activity')
-            }
-            // {
-            //     name: 'chart',
-            //     display_name: $translate.instant('views.chart')
-            // },
-            // {
-            //     name: 'timeline',
-            //     display_name: $translate.instant('views.timeline')
-            // }
-        ],
-        availableViews = [],
-        availableViewsDeferred = $q.defer(),
-
-    ViewHelper = {
-        views: function (allViews) {
-            return allViews ? allViews : availableViews;
+        {
+            name: 'map',
+            display_name: $translate.instant('views.map')
         },
-        getView: function (view, views) {
-            if (!views) {
-                views = allViews;
-            }
-            var match = _.findWhere(views, {name: view});
-            return match ? match.display_name : view;
-        },
-        isViewAvailable: function (view) {
-            return availableViewsDeferred.promise.then(function (availableViews) {
-                return _.findWhere(availableViews, {name: view});
-            });
-        },
-        getDefault: function (views) {
-            if (!views) {
-                views = allViews;
-            }
-
-            // default view is set to map for the time being.
-            var match = _.findWhere(views, {name: ''});
-            return match ? match.display_name : 'Map';
+        {
+            name: 'list',
+            display_name: $translate.instant('views.list')
         }
-    };
+    ];
 
-    // Push available views into array
-    // Rely on JS magic to
-    var populateAvailableView = function (featureConfig) {
-        _.each(allViews, function (view) {
-            if (featureConfig.views[view.name]) {
-                availableViews.push(view);
-            }
-        });
-        availableViewsDeferred.resolve(availableViews);
-    };
-    Features.loadFeatures().then(function (features) {
-        populateAvailableView(features);
-    });
+    function views() {
+        return allViews;
+    }
 
-    return ViewHelper;
+    return {
+        views: views
+    };
 }];
