a1ae09dcbb09c3af2928116d55d57a98f09cccbc
Zack Halloran <zack@ushahidi.com>
2016-08-09 21:44:17 -0700

    Added create rights filter to add-post-button directive
    Added a lookup in the form roles resource to determine if the current
    user has access to add to a survey. If the answer is no, that survey is
    removed from the list of surveys in the add-post-button menu.
    
    I’d much rather do this by adding a can-add field to the response
    payload of the Forms end-point.
    
    ushahidi/platform#1282

--- app/post/views/add-post-button.directive.js
+++ app/post/views/add-post-button.directive.js
@@ -12,28 +12,43 @@ function AddPostButtonDirective() {
 }
 
 AddPostButtonController.$inject = [
+    '$q',
     '$scope',
     '$rootScope',
     'FormEndpoint',
-    '$location'
+    'RoleEndpoint',
+    'FormRoleEndpoint',
+    '$location',
+    '_'
 ];
 function AddPostButtonController(
+    $q,
     $scope,
     $rootScope,
     FormEndpoint,
-    $location
+    RoleEndpoint,
+    FormRoleEndpoint,
+    $location,
+    _
 ) {
     $scope.forms = [];
     $scope.fabToggle = false;
     $scope.fabOptionsStyle = { opacity: 0, display: 'none' };
     $scope.toggleFab = toggleFab;
-    $scope.disabled = false;
+    $scope.disabled = true;
 
     activate();
 
     function activate() {
         // Load forms
-        $scope.forms = FormEndpoint.query();
+        allowedForms()
+            .then(function(forms) {
+                $scope.forms = forms;
+                if (forms.length > 0) {
+                    $scope.disabled = false;
+                }
+            });
+        
         $rootScope.$on('post:list:selected', handlePostSelected);
     }
 
@@ -52,5 +67,58 @@ function AddPostButtonController(
 
     function handlePostSelected(event, selectedPosts) {
         $scope.disabled = selectedPosts.length > 0;
+    }
+    
+    function allowedForms() {
+        var allowed_forms = $q.defer();
+        
+        FormEndpoint.query()
+        .$promise
+        .then(function(forms) {
+            if (!$rootScope.isAdmin()) {
+                _.each(forms, function(form, index) {
+                    // if all_roles == FALSE, check to see if the user has access to post to this form
+                    if (!form.all_roles) {
+                        // Remove the form from the list if the user is not logged in
+                        if ($rootScope.currentUser === null) {
+                            delete forms[index];
+                            if (index === forms.length -1) {
+                                allowed_forms.resolve(_.compact(forms));
+                            }
+                        }
+                        else {
+                            // look up role by role name to get the role id
+                            RoleEndpoint.query({ name: $rootScope.currentUser.role })
+                            .$promise
+                            .then(function(role) {
+                                if (role.length === 1 && role[0].id) {
+                                    FormRoleEndpoint.query({ formId: form.id })
+                                    .$promise
+                                    .then(function(roles) {
+                                        var roles_allowed = _.pluck(roles, 'role_id');
+                                        if (!_.contains(roles_allowed, role[0].id)) {
+                                            delete forms[index];
+                                        }
+                                        if (index === forms.length -1) {
+                                            allowed_forms.resolve(_.compact(forms));
+                                        }
+                                    });
+                                } else {
+                                    delete forms[index];
+                                    if (index === forms.length -1) {
+                                        allowed_forms.resolve(_.compact(forms));
+                                    }
+                                }
+                            });
+                        }
+                    }
+                });
+            }
+            else {
+                allowed_forms.resolve(forms);
+            }
+        });
+        
+        return allowed_forms.promise;   
     }
 }
