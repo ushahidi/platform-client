4db955dd828a9597ad0ceceec8b369a8d3e5134c
will <will.doran@riseup.net>
2016-10-04 20:10:42 -0400

    Retain protocol in deployment uri

--- app/common/global/event-handlers.js
+++ app/common/global/event-handlers.js
@@ -13,10 +13,10 @@ function (
         $rootScope.globalLayout = layout;
     };
     // Set embed mode
-    // TODO move this to Util
+    // TODO move this to util
     var pattern = /^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)/g;
     $rootScope.embedUrl = pattern.exec($window.ushahidi.apiUrl);
-    $rootScope.embedUrl = $rootScope.embedUrl[1].replace('api.', '');
+    $rootScope.embedUrl = $rootScope.embedUrl[0].replace('api.', '');
     $rootScope.globalEmbed = ($window.self !== $window.top) ? true : false;
     if ($rootScope.globalEmbed) {
         $rootScope.setLayout('layout-embed');
