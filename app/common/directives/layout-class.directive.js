6a19c4e5fa8eab90bb6589aa9830db18f569f5d7
will <will.doran@riseup.net>
2016-10-27 18:42:52 -0400

    Updating PL version and adding minor fix for embed

--- app/common/directives/layout-class.directive.js
+++ app/common/directives/layout-class.directive.js
@@ -14,8 +14,13 @@ function LayoutClassDirective() {
 LayoutClassController.$inject = ['$scope', '$rootScope', '$window'];
 function LayoutClassController($scope, $rootScope, $window) {
     var isEmbed = ($window.self !== $window.top) ? true : false;
+    // In the case of map we omit the layout-a class
+    var isMap = $window.location.href.includes('map');
+
     if (!isEmbed) {
         $rootScope.setLayout('layout-' + $scope.layout);
+    } else if (isEmbed && isMap) {
+        $rootScope.setLayout('layout-embed');
     } else {
         // If we are in embed mode
         // we must append the layout to the embed layout
