06ccab6b88601d50940241b496cd674cbe7b8cef
Robbie Mackay <rm@robbiemackay.com>
2016-11-02 16:52:55 +1300

    Format value for post datetime fields nicely

--- app/main/posts/detail/post-value.directive.js
+++ app/main/posts/detail/post-value.directive.js
@@ -1,4 +1,4 @@
-module.exports = ['PostEndpoint', function (PostEndpoint) {
+module.exports = ['PostEndpoint', 'moment', function (PostEndpoint, moment) {
     return {
         restrict: 'E',
         replace: true,
@@ -16,6 +16,21 @@ module.exports = ['PostEndpoint', function (PostEndpoint) {
             if ($scope.attribute.type === 'relation') {
                 $scope.value = $scope.value.map(function (entry) {
                     return PostEndpoint.get({ id : entry });
+                });
+            }
+            if ($scope.attribute.type === 'datetime' && $scope.attribute.input === 'date') {
+                $scope.value = $scope.value.map(function (entry) {
+                    return moment(entry).format('LL');
+                });
+            }
+            if ($scope.attribute.type === 'datetime' && $scope.attribute.input === 'datetime') {
+                $scope.value = $scope.value.map(function (entry) {
+                    return moment(entry).format('LLL');
+                });
+            }
+            if ($scope.attribute.type === 'datetime' && $scope.attribute.input === 'time') {
+                $scope.value = $scope.value.map(function (entry) {
+                    return moment(entry).format('LT');
                 });
             }
         }
