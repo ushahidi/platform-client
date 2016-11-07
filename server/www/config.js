9c0cfe1ee79519fd337e395bbd723e5d161318f9
Robbie Mackay <rm@robbiemackay.com>
2016-11-08 12:15:00 +1300

    Remove old server.js, Dockerfile and server/www contents

--- server/www/config.js
+++
@@ -1,7 +0,0 @@
-// Configure your Ushahidi deployment
-//
-// Uncomment lines below to configure your deployment
-// window.ushahidi = {
-// 	backendUrl : "https://ushahidi-platform-api-release.herokuapp.com",
-// 	mapboxApiKey: ""
-// };
