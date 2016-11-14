56fe0ff7ca4bf627e2e105b71cbf518097abf5cb
Robbie Mackay <rm@robbiemackay.com>
2016-11-14 16:04:11 +1300

    Refactor post location editor
    - Make markers draggable

--- app/main/posts/modify/location.directive.js
+++ app/main/posts/modify/location.directive.js
@@ -1,19 +1,7 @@
-module.exports = [
-    '$http',
-    'leafletData',
-    'Geocoding',
-    'Maps',
-    '_',
-    'Notify',
-function (
-    $http,
-    leafletData,
-    Geocoding,
-    Maps,
-    _,
-    Notify
-) {
+module.exports = PostLocationDirective;
 
+PostLocationDirective.$inject = ['$http', 'Leaflet', 'Geocoding', 'Maps', '_', 'Notify', '$window'];
+function PostLocationDirective($http, L, Geocoding, Maps, _, Notify, $window) {
     return {
         restrict: 'E',
         replace: true,
@@ -24,129 +12,117 @@ function (
             required: '='
         },
         template: require('./location.html'),
-        controller: [
-            '$window',
-            '$scope',
-            'Leaflet',
-        function (
-            $window,
-            $scope,
-            Leaflet
-        ) {
-            var markers = {},
-                mapName = $scope.id + '-map';
-            $scope.mapReady = false;
+        link: PostLocationLink
+    };
 
-            angular.extend($scope, Maps.getInitialScope());
+    function PostLocationLink($scope, element, attrs) {
+        var map, marker,
+            zoom = 8;
 
-            // Try to use value from settings
-            Maps.getAngularScopeParams().then(function (params) {
-                // Save initial center for reset
-                $scope.initialCenter = params.center;
+        $scope.processing = false;
+        $scope.searchLocationTerm = '';
+        $scope.searchLocation = searchLocation;
+        $scope.clear = clear;
 
-                // If we already have a location wipe the center
-                if ($scope.model) {
-                    delete params.center;
+        activate();
+
+        function activate() {
+            Maps.createMap(element[0].querySelector('.post-location-map'))
+            .then(function (data) {
+                map = data;
+
+                // init marker with current model value
+                if ($scope.model &&
+                    typeof $scope.model.lat !== 'undefined' &&
+                    typeof $scope.model.lon !== 'undefined'
+                ) {
+                    updateMarkerPosition($scope.model.lat, $scope.model.lon);
+                    centerMapTo($scope.model.lat, $scope.model.lon);
                 }
-                // Then save params into scope
-                angular.extend($scope, params);
 
-                $scope.mapReady = true;
-            });
-
-            // init markers with current model value
-            if ($scope.model) {
-                markers = {
-                    m1 : {
-                        lat: $scope.model.lat,
-                        lng: $scope.model.lon
-                    }
-                };
-
-                $scope.center = {
-                    lat: $scope.model.lat,
-                    lng: $scope.model.lon,
-                    zoom: 4
-                };
-            }
-
-            // leaflet map or location attribute
-            angular.extend($scope, {
-                markers: markers,
-
-                updateLatLon: function (lat, lon) {
-                    $scope.model = {
-                        lat: lat,
-                        lon: lon
-                    };
-                },
-
-                updateMarkerPosition: function (lat, lon) {
-                    $scope.markers.m1 = {
-                        lat: lat,
-                        lng: lon
-                    };
-                },
-
-                centerMapTo: function (lat, lon) {
-                    $scope.center = {
-                        lat : lat,
-                        lng : lon,
-                        zoom: 4
-                    };
-                },
-
-                searchLocation: function () {
-                    var that = this;
-                    $scope.processing = true;
-
-                    Geocoding.search($scope.searchLocationTerm).then(function (coordinates) {
-                        $scope.processing = false;
-
-                        if (!coordinates) {
-                            Notify.error('location.error');
-                            return;
-                        }
-
-                        _.each([
-                            'updateLatLon', 'updateMarkerPosition', 'centerMapTo'
-                        ], function (fn) {
-                            that[fn](coordinates[0], coordinates[1]);
-                        });
-
-                        $scope.searchLocationTerm = '';
-                    });
-                },
-
-                clear: function () {
-                    $scope.model = null;
-                    $scope.center = $scope.initialCenter;
-                    $scope.markers = {};
-                }
-            });
-
-            leafletData.getMap(mapName).then(function (map) {
                 map.on('click', onMapClick);
-                function onMapClick(e) {
-                    var wrappedLatLng = e.latlng.wrap(),
-                        lat = wrappedLatLng.lat,
-                        lon = wrappedLatLng.lng;
-
-                    $scope.updateMarkerPosition(lat, lon);
-                    $scope.updateLatLon(lat, lon);
-                }
+                // treate locationfound same as map click
+                map.on('locationfound', onMapClick);
 
                 // Add locate control, but only on https
                 if (window.location.protocol === 'https:' || window.location.hostname === 'localhost') {
-                    Leaflet.control.locate({
+                    L.control.locate({
                         follow: true
                     }).addTo(map);
                 }
 
-                // treate locationfound same as map click
-                map.on('locationfound', onMapClick);
+                // @todo: Should we watch the model and update map?
             });
-        }]
-    };
+        }
 
-}];
+        function onMapClick(e) {
+            var wrappedLatLng = e.latlng.wrap(),
+                lat = wrappedLatLng.lat,
+                lon = wrappedLatLng.lng;
+
+            updateMarkerPosition(lat, lon);
+            updateModelLatLon(lat, lon);
+        }
+
+        function updateModelLatLon(lat, lon) {
+            $scope.model = {
+                lat: lat,
+                lon: lon
+            };
+        }
+
+        function updateMarkerPosition(lat, lon) {
+            if (marker) {
+                marker.setLatLng([lat, lon]);
+            } else {
+                marker = L.marker([lat, lon], {
+                    draggable: true,
+                    icon: L.icon({
+                        iconUrl: require('leaflet/dist/images/marker-icon.png'),
+                        iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+                        shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+                        iconSize:    [25, 41],
+                        iconAnchor:  [12, 41],
+                        popupAnchor: [1, -34],
+                        tooltipAnchor: [16, -28],
+                        shadowSize:  [41, 41]
+                    })
+                });
+                marker.addTo(map);
+
+                marker.on('dragend', function (ev) {
+                    var latLng = ev.target.getLatLng();
+                    updateModelLatLon(latLng.lat, latLng.lng);
+                });
+            }
+        }
+
+        function centerMapTo(lat, lon) {
+            map.setView([lat, lon], zoom);
+        }
+
+        function searchLocation() {
+            $scope.processing = true;
+
+            Geocoding.search($scope.searchLocationTerm).then(function (coordinates) {
+                $scope.processing = false;
+
+                if (!coordinates) {
+                    Notify.error('location.error');
+                    return;
+                }
+
+                updateModelLatLon(coordinates[0], coordinates[1]);
+                updateMarkerPosition(coordinates[0], coordinates[1]);
+                centerMapTo(coordinates[0], coordinates[1]);
+
+                $scope.searchLocationTerm = '';
+            });
+        }
+
+        function clear() {
+            $scope.model = null;
+            marker.remove();
+        }
+    }
+}
