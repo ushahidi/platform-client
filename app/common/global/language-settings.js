11ed80fc7ea079fbe7666bd181aa9c3c9caf626b
Anna Iosif <hej@annaiosif.se>
2017-03-26 23:08:24 +0200

    using translation-service in language-settings #1651

--- app/common/global/language-settings.js
+++ app/common/global/language-settings.js
@@ -1,12 +1,15 @@
 module.exports = [
+    // '$scope',
     '$rootScope',
     '$translate',
-    'ConfigEndpoint',
+    'TranslationService',
     'Languages',
     'moment',
 function (
+    // $scope,
     $rootScope,
     $translate,
+    TranslationService,
     ConfigEndpoint,
     Languages,
     moment
@@ -17,35 +20,15 @@ function (
     $rootScope.switchRtl = function () {
         $rootScope.rtlEnabled = !$rootScope.rtlEnabled;
     };
-    var lang;
-
-    if (ConfigEndpoint.getLanguageCache()) {
-        lang = ConfigEndpoint.getLanguageCache();
-        translate(lang);
-    } else {
-        ConfigEndpoint.get({ id: 'site' }).$promise.then(function (site) {
-            lang = site.language ? site.language : 'en';
-            translate(lang);
-        });
-    }
-
-    function translate(lang) {
-        $translate.use(lang).then(function (langKey) {
-            if (langKey) {
-                $translate.preferredLanguage(lang);
-                Languages.then(function (languages) {
-                    angular.forEach(languages, function (language) {
-                        if (language.code === lang) {
-                            $rootScope.rtlEnabled = language.rtl;
-                        }
-                    });
-                });
-            }
+    TranslationService.getLanguage().then(function (language) {
+            translate(language);
         });
 
-        if (lang !== 'en') {
-            require(['moment/locale/' + lang + '.js'], function () {
-                moment.locale(lang);
+    function translate(language) {
+        TranslationService.translate(language);
+        if (language !== 'en') {
+            require(['moment/locale/' + language + '.js'], function () {
+                moment.locale(language);
             });
         }
     }
