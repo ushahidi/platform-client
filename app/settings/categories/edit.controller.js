0b3945637b451fc9f01dd0203444fb1118f71788
Anna Iosif <hej@annaiosif.se>
2017-03-29 15:04:35 +0200

    updating edit/create-category-functionality #1397

--- app/settings/categories/edit.controller.js
+++ app/settings/categories/edit.controller.js
@@ -5,10 +5,12 @@ module.exports = [
     '$location',
     'RoleEndpoint',
     'TagEndpoint',
+    'FormEndpoint',
     'Notify',
     '_',
     'Util',
     '$routeParams',
+    '$q',
 function (
     $scope,
     $rootScope,
@@ -16,27 +18,27 @@ function (
     $location,
     RoleEndpoint,
     TagEndpoint,
+    FormEndpoint,
     Notify,
     _,
     Util,
-    $routeParams
+    $routeParams,
+    $q
 ) {
-
     // Redirect to home if not authorized
     if ($rootScope.hasManageSettingsPermission() === false) {
         return $location.path('/');
     }
-    $scope.category = {};
-    TagEndpoint.getFresh({id: $routeParams.id}).$promise.then(function (result) {
-        $scope.category = result;
-    });
 
-    $translate('tag.edit_tag').then(function (title) {
+    $translate('category.edit_tag').then(function (title) {
         $scope.title = title;
         $rootScope.$emit('setPageTitle', title);
     });
+
     // Change mode
     $scope.$emit('event:mode:change', 'settings');
+
+    $scope.category = {};
 
     RoleEndpoint.query().$promise.then(function (roles) {
         $scope.roles = roles;
@@ -44,9 +46,53 @@ function (
 
     $scope.saving = false;
 
+    // getting label to edit
+    TagEndpoint.getFresh({id: $routeParams.id}).$promise.then(function (tag) {
+        $scope.category = tag;
+        // adding parent-object to tag
+        if ($scope.category.parent) {
+            $scope.category.parent = addParent($scope.category.parent.id);
+        }
+        //extract form-ids and make them integers
+        $scope.category.forms = $scope.category.forms.map(function (form) {
+            return parseInt(form.id);
+        });
+    });
+    // checking if label is a parent already
+    TagEndpoint.query({parent_id: $routeParams.id}).$promise.then(function (tag) {
+        if (tag.length === 0) {
+            // getting available parents
+            TagEndpoint.queryFresh({level: 'parent'}).$promise.then(function (tags) {
+                $scope.parents = tags;
+            });
+        }
+    });
+
+    FormEndpoint.queryFresh().$promise.then(function (forms) {
+        $scope.surveys = forms;
+    });
+
+    function addParent(id) {
+        return TagEndpoint.getFresh({id: id});
+    }
+
+    $scope.getParentName = function () {
+        var parentName = 'Nothing';
+        if ($scope.category.parent_id) {
+            $scope.parents.forEach(function (parent) {
+                if (parent.id === $scope.category.parent_id) {
+                    parentName = parent.tag;
+                }
+            });
+        } else if ($scope.category.parent) {
+            parentName = $scope.category.parent.tag;
+        }
+        return parentName;
+    };
+
     $scope.saveCategory = function (tag) {
         $scope.saving = true;
-        // @todo: change this to use original api allowing callback on save and delete cache
+        //@todo: change this to use original api allowing callback on save and delete cache
         TagEndpoint.saveCache(tag).$promise.then(function (result) {
             Notify.notify('notify.category.save_success', {name: tag.tag});
             $location.path('/settings/categories');
@@ -61,7 +107,6 @@ function (
     };
 
     $scope.deleteCategory = function (category) {
-
         Notify.confirmDelete('notify.category.destroy_confirm').then(function () {
             TagEndpoint.delete({ id: category.id }).$promise.then(function () {
                 Notify.notify('notify.category.destroy_success');
