f2dd06d814c3e794b3f33406cf059d65c16a43de
Anna Iosif <hej@annaiosif.se>
2017-04-21 00:14:06 +0200

    fixing filtering of categories in filter-modal #1397

--- app/main/posts/views/filters/filter-category.directive.js
+++ app/main/posts/views/filters/filter-category.directive.js
@@ -15,30 +15,33 @@ function CategorySelectDirective(TagEndpoint, _) {
         if (!ngModel) {
             return;
         }
-
         scope.categories = [];
+        scope.parents = [];
         scope.selectedCategories = [];
-        scope.selectParent = selectParent;
-        scope.selectChild = selectChild;
-
         activate();
 
         function activate() {
             // Load categories
-            TagEndpoint.query().$promise.then(function (result) {
+            TagEndpoint.queryFresh().$promise.then(function (result) {
                 scope.categories = result;
                 // adding children to tags
                 _.each(scope.categories, function (category) {
                     if (category.children) {
                         var children = [];
                         _.each(category.children, function (child) {
-                            _.each(scope.categories, function (category) {
-                                if (category.id === parseInt(child.id)) {
-                                    children.push(category);
+                            _.each(scope.categories, function (childObj) {
+                                if (childObj.id === parseInt(child.id)) {
+                                    children.push(childObj);
                                 }
                             });
                             category.children = children;
                         });
+                    }
+                });
+                // separating parents from children
+                scope.parents = _.filter(scope.categories, function (category) {
+                    if (category.parent_id === null) {
+                        return category;
                     }
                 });
             });
@@ -46,32 +49,22 @@ function CategorySelectDirective(TagEndpoint, _) {
             scope.$watch('selectedCategories', saveValueToView, true);
             ngModel.$render = renderModelValue;
         }
-        function selectParent(parent) {
-            if (_.contains(scope.selectedCategories, parent.id)) {
-                _.each(parent.children, function (child) {
-                    scope.selectedCategories.push(child.id);
-                });
-            } else {
-                _.each(parent.children, function (child) {
-                    scope.selectedCategories = _.filter (scope.selectedCategories, function (id) {
-                        return id !== child.id;
-                    });
-                });
-            }
-        }
-
-        function selectChild(child) {
-            if (!_.contains(scope.selectedCategories, child.parent.id) && _.contains(scope.selectedCategories, child.id)) {
-                scope.selectedCategories.push(child.parent.id);
-            }
-        }
         function renderModelValue() {
             // Update selectCategories w/o breaking references used by checklist-model
             Array.prototype.splice.apply(scope.selectedCategories, [0, scope.selectedCategories.length].concat(ngModel.$viewValue));
         }
 
         function saveValueToView(selectedCategories) {
-            ngModel.$setViewValue(angular.copy(selectedCategories));
+            var selected = [];
+            // removing children that has no parent selected
+            _.each(scope.categories, function (category) {
+                        if (category.parent && _.contains(selectedCategories, category.id) && _.contains(selectedCategories, category.parent.id)) {
+                            selected.push(category.id);
+                        } else if (category.parent_id === null && _.contains(selectedCategories, category.id)) {
+                            selected.push(category.id);
+                        }
+                    });
+            ngModel.$setViewValue(angular.copy(selected));
         }
     }
 }
