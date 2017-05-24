e414015b311323bdad49bddebf7511ec7f46f15b
will <will.doran@riseup.net>
2017-05-24 18:04:11 -0400

    Add animation ofr post delete on list view (#651)

--- app/main/posts/views/post-view-list.directive.js
+++ app/main/posts/views/post-view-list.directive.js
@@ -139,6 +139,7 @@ function PostListController(
         Notify.confirmDelete('notify.post.bulk_destroy_confirm', { count: $scope.selectedPosts.length }).then(function () {
             // ask server to delete selected posts
             // and refetch posts from server
+            $scope.isLoading.state = true;
             var deletePostsPromises = _.map(
                 $scope.selectedPosts,
                 function (postId) {
@@ -149,9 +150,11 @@ function PostListController(
             ;
 
             function handleDeleteErrors(errorResponse) {
+                $scope.isLoading.state = false;
                 Notify.apiErrors(errorResponse);
             }
             function handleDeleteSuccess(deleted) {
+                $scope.isLoading.state = false;
                 Notify.notify('notify.post.destroy_success_bulk');
                 // Remove deleted posts from state
                 var deletedIds = _.pluck(deleted, 'id');
