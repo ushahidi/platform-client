4fadfd9d69af723bd7569b27a1ad3ed0fa587996
Robbie Mackay <rm@robbiemackay.com>
2017-05-30 14:30:22 +1200

    Survey editor: Block selecting child categories without parent
    - Refactor handling of parent/child categories in attribute editor
    - Disable parent category checkbox when children are selected
    - Select parent category when selecting child
    - Use checklist-change to keep state consistent

--- app/settings/surveys/attribute-editor.directive.js
+++ app/settings/surveys/attribute-editor.directive.js
@@ -13,6 +13,8 @@ function (
         link: function ($scope, $element, $attrs) {
             $scope.defaultValueToggle = false;
             $scope.descriptionToggle = false;
+            $scope.disabledCategories = [];
+
             $scope.editName = function () {
                 if (!$scope.editAttribute.id) {
                     $scope.editAttribute.label = '';
@@ -29,15 +31,35 @@ function (
             $scope.canDisplay = function () {
                 return $scope.editAttribute.input !== 'upload' && $scope.editAttribute.type !== 'title' && $scope.editAttribute.type !== 'description' && $scope.editAttribute.input !== 'tags';
             };
-            $scope.selectChild = function (child) {
-                if (!_.contains($scope.editAttribute.options, child.parent.id) && _.contains($scope.editAttribute.options, child.id)) {
-                    $scope.editAttribute.options.push(child.parent.id);
-                }
+
+            $scope.changeCategories = function () {
+                _.each($scope.availableCategories, function (category) {
+                    var selectedChildren = _.chain(category.children)
+                        .pluck('id')
+                        .intersection($scope.editAttribute.options)
+                        .value();
+
+                    // If children are selected, add to disabled categories
+                    if (selectedChildren.length > 0) {
+                        $scope.disabledCategories[category.id] = true;
+
+                        // ... and ensure this category is selected
+                        if (!_.contains($scope.editAttribute.options, category.id)) {
+                            $scope.editAttribute.options.push(category.id);
+                        }
+                    } else {
+                        // or, if no children are selected
+                        // remove from disabled categories
+                        $scope.disabledCategories[category.id] = false;
+                    }
+                });
             };
 
             $scope.canMakePrivate = function () {
                 return $scope.editAttribute.type !== 'tags';
             };
+
+            $scope.changeCategories();
         }
     };
 }];
