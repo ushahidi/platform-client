3fff21e43adb51cfc3bb2b22bc48dbc738e2c546
Ryan Christoffersen <ryan@ryanchristo.com>
2017-07-03 23:11:59 +0700

    Fixed promise handling and combined create directive

--- app/settings/categories/edit.controller.js
+++ app/settings/categories/edit.controller.js
@@ -25,103 +25,179 @@ function (
     $routeParams,
     $q
 ) {
+
     // Redirect to home if not authorized
     if ($rootScope.hasManageSettingsPermission() === false) {
         return $location.path('/');
     }
 
-    $translate('category.edit_tag').then(function (title) {
-        $scope.title = title;
-        $rootScope.$emit('setPageTitle', title);
-    });
+    // Set initial category properties and page title
+    if ($location.path() === '/settings/categories/create') {
+        // Set initial category properties
+        $scope.category = {
+            type: 'category',
+            icon: 'tag',
+            color: '',
+            parent_id: null
+        };
+        // Allow parent category selector
+        $scope.isParent = false;
+        // Translate and set add category page title
+        $translate('category.add_tag').then(function (title) {
+            $scope.title = title;
+            $scope.$emit('setPageTitle', title);
+        });
+    } else {
+        // Get and set initial category properties
+        getCategory();
+        // Translate and set edit category page title
+        $translate('category.edit_tag').then(function (title) {
+            $scope.title = title;
+            $rootScope.$emit('setPageTitle', title);
+        });
+    }
 
     // Change mode
     $scope.$emit('event:mode:change', 'settings');
 
-    $scope.category = {};
+    $scope.addParent = addParent;
+    $scope.deleteCategory = deleteCategory;
+    $scope.getParentName = getParentName;
+    $scope.saveCategory = saveCategory;
 
-    RoleEndpoint.query().$promise.then(function (roles) {
-        $scope.roles = roles;
-    });
+    $scope.cancel = cancel;
 
+    $scope.processing = false;
     $scope.save = $translate.instant('app.save');
     $scope.saving = $translate.instant('app.saving');
-    $scope.processing = false;
-    $scope.isParent = false;
 
-    // getting label to edit
-    TagEndpoint.getFresh({id: $routeParams.id}).$promise.then(function (tag) {
-        $scope.category = tag;
-        // Normalize parent:
-        if ($scope.category.parent) {
-            $scope.category.parent_id = $scope.category.parent.id;
-            $scope.category.parent_id_original = $scope.category.parent.id;
-            delete $scope.category.parent;
-        }
-        if (!$scope.category.parent_id) {
-            $scope.isParent = true;
-        }
-    });
+    activate();
 
-    // getting available parents
-    TagEndpoint.queryFresh({level: 'parent'}).$promise.then(function (tags) {
-        // Remove current tag to avoid circular reference
-        $scope.parents = _.filter(tags, function (tag) {
-            return tag.id !== parseInt($routeParams.id);
+    function activate() {
+        getRoles();
+        getParentCategories();
+    }
+
+    function getRoles() {
+        RoleEndpoint.query().$promise.then(function (roles) {
+            $scope.roles = roles;
         });
-    });
+    }
 
-    $scope.addParent = function (id) {
+    function getParentCategories() {
+        TagEndpoint.queryFresh({ level: 'parent' }).$promise.then(function (tags) {
+            // Remove current tag to avoid circular reference
+            $scope.parents = _.filter(tags, function (tag) {
+                return tag.id !== parseInt($routeParams.id);
+            });
+        });
+    }
+
+    function getCategory() {
+        TagEndpoint.getFresh({ id: $routeParams.id }).$promise.then(function (tag) {
+            $scope.category = tag;
+            // Normalize parent category
+            if ($scope.category.parent) {
+                $scope.category.parent_id = $scope.category.parent.id;
+                $scope.category.parent_id_original = $scope.category.parent.id;
+                delete $scope.category.parent;
+            }
+            if (!$scope.category.parent_id) {
+                $scope.isParent = true;
+            }
+        });
+    }
+
+    function addParent(id) {
         return TagEndpoint.getFresh({id: id});
-    };
+    }
 
-    $scope.getParentName = function () {
+    function getParentName() {
         var parentName = 'Nothing';
-        var parent = _.findWhere($scope.parents, { id: $scope.category.parent_id });
-        if (parent) {
-            parentName = parent.tag;
+        if ($scope.category && $scope.parents) {
+            $scope.parents.forEach(function (parent) {
+                if (parent.id === $scope.category.parent_id) {
+                    parentName = parent.tag;
+                }
+            });
         }
         return parentName;
-    };
+    }
 
-    $scope.saveCategory = function (tag) {
+    function saveCategory(category) {
+        // Set processing to disable user actions
         $scope.processing = true;
-        //@todo: change this to use original api allowing callback on save and delete cache
-        TagEndpoint.saveCache(tag).$promise.then(function (result) {
-            // If parent catgegory, apply parent category permisions to child categories
-            if (tag.children && tag.children.length) {
-                _.each(tag.children, function (child) {
-                    TagEndpoint.saveCache({ id: child.id, role: tag.role });
+        // Save category
+        TagEndpoint
+        .saveCache(category)
+        .$promise
+        .then(function (result) {
+            var promises = [];
+            // If parent category, apply parent category permisions to child categories
+            if (result.children && result.children.length) {
+                _.each(result.children, function (child) {
+                    promises.push(updateChildPermissions(result, child));
                 });
             }
-            // If child catgegory with new parent, apply new permissions to child category
+            // If child category with new parent, apply new permissions to child category
             if (result.parent && result.parent.id !== $scope.category.parent_id_original) {
-                TagEndpoint.getFresh({ id: result.parent.id }).$promise.then(function (parent) {
-                    TagEndpoint.saveCache({ id: result.id, role: parent.role });
-                });
+                promises.push(updateWithParentPermissions(result));
             }
-            Notify.notify('notify.category.save_success', {name: tag.tag});
+            return $q.all(promises);
+        })
+        .then(function () {
+            // Display success message
+            Notify.notify(
+                'notify.category.save_success',
+                { name: $scope.category.tag }
+            );
+            // Redirect to categories list
             $location.path('/settings/categories');
-        }, function (errorResponse) { // error
-            Notify.apiErrors(errorResponse);
-            $scope.processing = false;
+        })
+        // Catch and handle errors
+        .catch(handleResponseErrors);
+    }
+
+    function updateChildPermissions(category, child) {
+        return TagEndpoint
+        .saveCache({ id: child.id, role: category.role })
+        .$promise;
+    }
+
+    function updateWithParentPermissions(category) {
+        return TagEndpoint
+        .getFresh({ id: category.parent.id })
+        .$promise
+        .then(function (parent) {
+            return TagEndpoint
+            .saveCache({ id: category.id, role: parent.role })
+            .$promise;
         });
-    };
+    }
 
-    var handleResponseErrors = function (errorResponse) {
-        Notify.apiErrors(errorResponse);
-    };
-
-    $scope.deleteCategory = function (category) {
-        Notify.confirmDelete('notify.category.destroy_confirm', 'notify.category.destroy_confirm_desc').then(function () {
-            TagEndpoint.delete({ id: category.id }).$promise.then(function () {
+    function deleteCategory(category) {
+        Notify.confirmDelete(
+            'notify.category.destroy_confirm',
+            'notify.category.destroy_confirm_desc'
+        ).then(function () {
+            return TagEndpoint
+            .delete({ id: category.id })
+            .$promise
+            .then(function () {
                 Notify.notify('notify.category.destroy_success');
-            }, handleResponseErrors);
-            $location.url('/settings/categories');
-        }, function () {});
-    };
+                $location.url('/settings/categories');
+            });
+        })
+        .catch(handleResponseErrors);
+    }
 
-    $scope.cancel = function () {
+    function handleResponseErrors(errorResponse) {
+        $scope.processing = false;
+        Notify.apiErrors(errorResponse);
+    }
+
+    function cancel() {
         $location.path('/settings/categories');
-    };
+    }
+
 }];
