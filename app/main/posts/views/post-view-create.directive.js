ee95e4972066769ae9d71bbf443ea58effab4f1f
Anna Iosif <hej@annaiosif.se>
2017-07-28 13:51:26 +0200

    only resetting url to /views/map if the previous route was /views/create #698

--- app/main/posts/views/post-view-create.directive.js
+++ app/main/posts/views/post-view-create.directive.js
@@ -11,31 +11,33 @@ function PostViewCreate() {
     };
 }
 PostViewCreateController.$inject = ['$rootScope', '$scope', 'PostSurveyService', 'MainsheetService', '$location', 'Notify'];
+
 function PostViewCreateController($rootScope, $scope, PostSurveyService, MainsheetService, $location, Notify) {
     // resetting path to map-view when mainsheet-modal is closed
     $rootScope.$on('mainsheet:statechange', function () {
+                if (MainsheetService.getState() && $location.path() === '/views/create') {
                     $location.path('/views/map');
-                });
+                }
+            });
 
     activate();
-
     function activate() {
         PostSurveyService
-        .allowedSurveys()
-        .then(function (forms) {
-            $scope.forms = forms;
-            if (forms.length > 0) {
-                $scope.disabled = false;
-            }
-            if ($scope.forms.length === 1) {
-                $location.path('/posts/create/' + $scope.forms[0].id);
-            } else {
-                MainsheetService.openTemplate(
-                '<add-post-survey-list></add-post-survey-list>', 'app.submit_response', $scope
-            );
-            }
-        }, function (errorResponse) {
-            Notify.apiErrors(errorResponse);
-        });
+            .allowedSurveys()
+            .then(function (forms) {
+                $scope.forms = forms;
+                if (forms.length > 0) {
+                    $scope.disabled = false;
+                }
+                if ($scope.forms.length === 1) {
+                    $location.path('/posts/create/' + $scope.forms[0].id);
+                } else {
+                    MainsheetService.openTemplate(
+                    '<add-post-survey-list></add-post-survey-list>', 'app.submit_response', $scope
+                );
+                }
+            }, function (errorResponse) {
+                Notify.apiErrors(errorResponse);
+            });
     }
 }
