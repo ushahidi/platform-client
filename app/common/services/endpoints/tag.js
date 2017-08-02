afc5c445748c022b0d43ae487a3d55e7c4ea3d31
Robbie Mackay <rm@robbiemackay.com>
2017-08-02 14:22:16 +1200

    Add translations to tags edit, and swap tag language from translation

--- app/common/services/endpoints/tag.js
+++ app/common/services/endpoints/tag.js
@@ -3,11 +3,15 @@ module.exports = [
     '$rootScope',
     'Util',
     'CacheFactory',
+    'TranslationService',
+    '$http',
 function (
     $resource,
     $rootScope,
     Util,
-    CacheFactory
+    CacheFactory,
+    TranslationService,
+    $http
 ) {
     var cache;
     if (!(cache = CacheFactory.get('tagCache'))) {
@@ -24,15 +28,41 @@ function (
             cache: cache,
             transformResponse: function (data /*, header*/) {
                 return Util.transformResponse(data).results;
+            },
+            interceptor: {
+                response: (response) => {
+                    return TranslationService.getLanguage().then((currentLanguage) => {
+                        return response.resource.map((resource) => {
+                            return getLocalized(resource, currentLanguage);
+                        });
+                    });
+                }
             }
         },
         update: {
-            method: 'PUT'
+            method: 'PUT',
+            transformRequest: [
+                (data) => {
+                    if (data.translations && data.translations.en) {
+                        data.tag = data.translations.en.tag;
+                        data.description = data.translations.en.description;
+                    }
+                    return data;
+                },
+                $http.defaults.transformRequest[0]
+            ]
         },
         get: {
             method: 'GET',
             cache: cache,
-            params: {'ignore403': '@ignore403'}
+            params: {'ignore403': '@ignore403'},
+            interceptor: {
+                response: (response) => {
+                    return TranslationService.getLanguage().then((currentLanguage) => {
+                        return getLocalized(response.resource, currentLanguage);
+                    });
+                }
+            }
         },
         deleteEntity: {
             method: 'DELETE'
@@ -73,6 +103,32 @@ function (
     $rootScope.$on('event:authentication:login:succeeded', function () {
         TagEndpoint.queryFresh();
     });
+
+    function getLocalized(resource, currentLanguage) {
+        // Ensure we have at least empty translations
+        resource.translations = angular.extend(
+            {},
+            {
+                en: {
+                    tag: resource.tag,
+                    description: resource.description
+                },
+                ar: {}
+            },
+            resource.translations
+        );
+
+        if (resource.translations) {
+            if (resource.translations[currentLanguage]) {
+                angular.forEach(resource.translations[currentLanguage], (value, key) => {
+                    // Replace original values with translations
+                    resource[key] = value;
+                });
+            }
+        }
+
+        return resource;
+    }
 
     return TagEndpoint;
 
