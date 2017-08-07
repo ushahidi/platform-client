dc34c09ff9474a872fd1639ea801eef085cce1c3
Robbie Mackay <rm@robbiemackay.com>
2017-08-07 14:42:35 +1200

    feature: Add post source filter to the filters modal
    Refs: ushahidi/platform#1861

--- app/main/posts/views/post-filters.service.js
+++ app/main/posts/views/post-filters.service.js
@@ -79,7 +79,8 @@ function PostFiltersService(_, FormEndpoint, TagEndpoint, $q) {
             tags: [],
             form: _.pluck(forms, 'id'),
             set: [],
-            user: false
+            user: false,
+            source: []
         };
     }
 
