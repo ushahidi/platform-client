21dd4aa5bc83e3ea921ba1f0731a9ebc6e8b5a26
Robbie Mackay <rm@robbiemackay.com>
2017-08-18 14:45:22 +1200

    Fix up tos check

--- app/common/auth/tos.service.js
+++ app/common/auth/tos.service.js
@@ -40,11 +40,10 @@ function (
 
     function tosCheck(tosEntry) {
         var deferred = $q.defer();
-        var agreementDate;
 
         // If the tos agreement date is after the to the release date, then resolve
         // Otherwise (the tos agreement is before the release date or the user has never signed) We need to show the ToS modal
-        if (tosEntry.results.length && moment(agreementDate).isAfter(CONST.TOS_RELEASE_DATE)) {
+        if (tosEntry.results.length && moment(tosEntry.results[0].agreement_date).isAfter(CONST.TOS_RELEASE_DATE)) {
             deferred.resolve();
         } else {
             Notify.confirmTos().then(function () {
