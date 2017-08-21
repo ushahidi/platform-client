c85917de8d09e0c8ddbabbe9e850051bcce79497
Robbie Mackay <rm@robbiemackay.com>
2017-08-22 09:31:40 +1200

    Refactor geocoding with cleaner code
    - Use $http instead of $resource
    - Define API at top of file
    - Reformat to use named functions

--- app/common/services/geocoding.js
+++ app/common/services/geocoding.js
@@ -1,72 +1,72 @@
 module.exports = [
     '$q',
-    '$resource',
-    'Util',
+    '$http',
     '_',
 function (
     $q,
-    $resource,
-    Util,
+    $http,
     _
 ) {
-    var osm_geosearch = $resource('//nominatim.openstreetmap.org/search',
-                { format: 'json' },
-                {
-                    query: {
-                        method: 'GET',
-                        isArray: true
-                    }
-                }
-            );
-
-    var google_places_search = {
-        query: function (location_name) {
-            var deferred = $q.defer();
-            window.googlePlaces.textSearch(location_name, function (results) {
-                deferred.resolve(
-                    _.map(results, function (result) {
-                        return {
-                            display_name: result.name + ' - ' + result.formatted_address,
-                            lat: result.geometry.location.lat(),
-                            lon: result.geometry.location.lng()
-                        };
-                    })
-                );
-            });
-            return { $promise: deferred.promise };
-        }
+    return {
+        searchCoordinates: searchCoordinates,
+        searchAllInfo: searchAllInfo
     };
 
-    var build_query = function (location_name) {
+    function searchCoordinates(location_name) {
+        return build_query(location_name).then(function (results) {
+            if (results && results[0]) {
+                if (results[0].lat && results[0].lon) {
+                    return [parseFloat(results[0].lat), parseFloat(results[0].lon)];
+                }
+            }
+
+            return null;
+        });
+    }
+
+    function searchAllInfo(location_name) {
+        return build_query(location_name).then(function (results) {
+            if (results && results[0]) {
+                return results;
+            }
+            return null;
+        });
+    }
+
+    function build_query(location_name) {
         if (window.googlePlaces) { /* google maps */
-            return google_places_search.query({query: location_name});
+            return google_places_search(location_name);
         } else { /* osm nominatim */
-            return osm_geosearch.query({q: location_name});
+            return osm_geosearch(location_name);
         }
-    };
+    }
 
-    var Geocoding = {
-        searchCoordinates: function (location_name) {
-            return build_query(location_name).$promise.then(function (results) {
-                if (results && results[0]) {
-                    if (results[0].lat && results[0].lon) {
-                        return [parseFloat(results[0].lat), parseFloat(results[0].lon)];
-                    }
-                }
+    function osm_geosearch(location_name) {
+        return $http({
+            method: 'GET',
+            url: '//nominatim.openstreetmap.org/search',
+            params: { q: location_name, format: 'json' }
+        }).then((response) => {
+            return response.data;
+        }, () => {
+            // noop
+        });
+    }
 
-                return null;
-            });
-        },
-        searchAllInfo: function (location_name) {
-            return build_query(location_name).$promise.then(function (results) {
-                if (results && results[0]) {
-                    return results;
-                }
-                return null;
-            });
-        }
-    };
-
-    return Util.bindAllFunctionsToSelf(Geocoding);
+    function google_places_search(location_name) {
+        var deferred = $q.defer();
+        window.googlePlaces.textSearch({ query: location_name }, function (results) {
+            deferred.resolve(
+                _.map(results, function (result) {
+                    return {
+                        display_name: result.name + ' - ' + result.formatted_address,
+                        lat: result.geometry.location.lat(),
+                        lon: result.geometry.location.lng()
+                    };
+                })
+            );
+        });
+        return deferred.promise;
+    }
 
 }];
