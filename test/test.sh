c69ba4dfb05700218d6ca80eaa1b950763170009
Romina Suarez <rowasc@gmail.com>
2017-09-05 22:37:46 -0300

    Remove e2e test suite since it's not being used or mantained. Update test instructions

--- test/test.sh
+++ test/test.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 set -ev
 
-if [ "${TEST_SUITE}" = "e2e" ]; then
-	if [ "${PROTRACTOR_SUITE}" != "" ]; then
-		npm run protractor -- "--suite $PROTRACTOR_SUITE"
-	else
-    	npm run protractor
-    fi
-fi
-
 if [ "${TEST_SUITE}" = "lint" ]; then
     gulp jscs
     npm run jshint
