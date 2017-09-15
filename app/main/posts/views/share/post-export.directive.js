04d6800ae98b8a2dcec6683f5f07d630df7c8a0d
Romina Suarez <rowasc@gmail.com>
2017-09-14 23:30:48 -0300

    WIP

--- app/main/posts/views/share/post-export.directive.js
+++ app/main/posts/views/share/post-export.directive.js
@@ -33,53 +33,70 @@ function PostExportController(
     _
 ) {
     $scope.loading = false;
-    $scope.exportPosts = exportPosts;
-
-    function exportPosts() {
+    $scope.exportPostsConfirmation = exportPostsConfirmation;
+    this.getQuery = getQuery;
+    this.doExport = doExport;
+    this.showCSVResults = showCSVResults;
+    /**
+     * Trigger confirm notification for user.
+     * When the user accepts, get the CSV
+     */
+    function exportPostsConfirmation() {
         Notify.confirm('notify.post.export').then(function (message) {
-            $scope.loading = true;
-            /**
-             *
-             * If the filters are not available, apply the defaults
-             */
-
-            if (!$scope.filters || _.isEmpty($scope.filters)) {
-                $scope.filters = PostFilters.getDefaults();
-            }
-            var format = 'csv',  //@todo handle more formats
-                // Prepare filters for export
-                query = angular.extend({}, PostFilters.getQueryParams($scope.filters), {
-                    format: format
-                }),
-
-                site = ConfigEndpoint.get({ id: 'site' }).$promise,
-                postExport = PostEndpoint.export(query);
-
-            // Save export data to file
-            $q.all([site, postExport]).then(function (response) {
-
-                var filename = response[0].name + '-' + (new Date()).toISOString().substring(0, 10) + '.' + format,
-                    data = response[1].data;
-
-                // Create anchor link
-                var anchor = angular.element('<a/>');
-
-                // ...and attach it.
-                angular.element(document.body).append(anchor);
-
-                // Set attributes
-                anchor.attr({
-                    href: 'data:attachment/' + format + ';charset=utf-8,' + encodeURIComponent(data),
-                    download: filename
-                });
-
-                // Show file download dialog
-                anchor[0].click();
-
-                // ... and finally remove the link
-                anchor.remove();
-                $scope.loading = false;
-            });
+            doExport();
         });
     }
+
+    function doExport() {
+        $scope.loading = true;
+        var site = ConfigEndpoint.get({ id: 'site' }).$promise;
+        var query = getQuery();
+        var postExport = PostEndpoint.export(query);
+        var self = this;
+        $q.all([site, postExport]).then(function (response) {
+            self.showCSVResults(response, query.format);
+        }).finally(function (response) {
+            $scope.loading = false;
+        });
+    }
+    function getQuery() {
+        /**
+         * If the filters are not available, apply the defaults
+         */
+        if (!$scope.filters || _.isEmpty($scope.filters)) {
+            $scope.filters = PostFilters.getDefaults();
+        }
+        var format = 'csv';  //@todo handle more formats
+        // Prepare filters for export
+        var query = angular.extend({}, PostFilters.getQueryParams($scope.filters), {
+            format: format
+        });
+        return query;
+    }
+
+    function showCSVResults(response, format) {
+        // Save export data to file
+        var filename = response[0].name + '-' + (new Date()).toISOString().substring(0, 10) + '.' + format,
+            data = response[1].data;
+
+        // Create anchor link
+        var anchor = angular.element('<a/>');
+
+        // ...and attach it.
+        angular.element(document.body).append(anchor);
+
+        // Set attributes
+        anchor.attr({
+            href: 'data:attachment/' + format + ';charset=utf-8,' + encodeURIComponent(data),
+            download: filename
+        });
+
+        // Show file download dialog
+        anchor[0].click();
+
+        // ... and finally remove the link
+        anchor.remove();
+        $scope.loading = false;
+    }
+
 }
