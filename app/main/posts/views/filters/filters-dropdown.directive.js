a85bc78809afd96182e59a025d41ce4e66b85cac
Romina Suarez <rowasc@gmail.com>
2017-10-04 23:46:56 -0300

    Saved search modal added in dropdown filters

--- app/main/posts/views/filters/filters-dropdown.directive.js
+++ app/main/posts/views/filters/filters-dropdown.directive.js
@@ -1,7 +1,7 @@
 module.exports = FiltersDropdown;
 
-FiltersDropdown.$inject = ['PostFilters'];
-function FiltersDropdown(PostFilters) {
+FiltersDropdown.$inject = ['PostFilters', 'ModalService', '$rootScope'];
+function FiltersDropdown(PostFilters, ModalService, $rootScope) {
     return {
         restrict: 'E',
         require: 'ngModel',
@@ -17,11 +17,25 @@ function FiltersDropdown(PostFilters) {
 
     function FiltersDropdownLink($scope, $element, $attrs, ngModel) {
         PostFilters.reactiveFilters = 'disabled';
+        // Init an empty saved search
+        $scope.savedSearch = {
+            view : 'map',
+            role : []
+        };
+
         $scope.applyFiltersLocked = function () {
             PostFilters.reactiveFilters = 'enabled';
         };
         $scope.clearFilters = function () {
             $scope.filtersVar = PostFilters.clearFilters();
+        };
+        $scope.openSavedModal = function () {
+            $scope.savedSearch.filter = $scope.filtersVar;
+            // @TODO Prevent the user from creating one if they somehow manage to get to this point without being logged in
+            $scope.savedSearch.user_id = $rootScope.currentUser ? $rootScope.currentUser.userId : null;
+            ModalService.openTemplate('<saved-search-editor saved-search="savedSearch"></saved-search-editor>', 'set.create_savedsearch', 'star', $scope, false, false);
+
+            //ModalService.openTemplate('<savedsearch-create></savedsearch-create>', 'nav.saved_searches', '/img/iconic-sprite.svg#star', $scope.$parent, true, true);
         };
     }
 }
