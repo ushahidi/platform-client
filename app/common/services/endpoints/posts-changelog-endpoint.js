af1aedea49ac344bc1ddb3ca5a8ea10ad0cad86c
kinstelli <mark@ushahidi.com>
2017-10-11 22:17:20 -0500

    rebuilt branch

---
+++ app/common/services/endpoints/posts-changelog-endpoint.js
@@ -0,0 +1,18 @@
+module.exports = [
+    '$resource',
+    'Util',
+function (
+    $resource,
+    Util
+) {
+    //TODO: fix this endpoint to be integrated with the existing POST endpoint?
+    var PostsChangeLogEndpoint = $resource(Util.apiUrl('/posts/:post_id/changelog/'), {
+        'post_id': '@post_id'
+    }, {
+        get: {
+            method: 'GET'
+        }
+    });
+
+    return PostsChangeLogEndpoint;
+}];
