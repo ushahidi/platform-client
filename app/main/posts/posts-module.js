3466fff0d74573ea1bc72cb6d9fb204e9ff9e936
Romina Suarez <rowasc@gmail.com>
2017-10-12 23:03:48 -0300

    remove unused toolbox directive

--- app/main/posts/posts-module.js
+++ app/main/posts/posts-module.js
@@ -38,7 +38,6 @@ angular.module('ushahidi.posts', [])
 .directive('filterPostSortingOptions', require('./views/filters/filter-post-sorting-options.directive.js'))
 .directive('filterPostOrderAscDesc', require('./views/filters/filter-post-order-asc-desc.directive.js'))
 .directive('filterUnlockedOnTop', require('./views/filters/filter-unlocked-on-top.directive.js'))
-.directive('postTimelineToolbox', require('./views/post-timeline-toolbox.directive.js'))
 .directive('postDataEditor', require('./modify/post-data-editor.directive.js'))
 
 // Timeline, data and Map screen
