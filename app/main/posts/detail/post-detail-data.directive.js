a6d0417502aafc5a057c23b5bc047c5763e19ca9
Anna Iosif <hej@annaiosif.se>
2017-10-13 10:34:08 +0200

    displaying tasks and conversation with author in post-data-detail-view

--- app/main/posts/detail/post-detail-data.directive.js
+++ app/main/posts/detail/post-detail-data.directive.js
@@ -58,6 +58,9 @@ function PostDetailDataController(
     moment,
     PostSurveyService) {
 
+    $scope.$watch('post', function (post) {
+        activate();
+    });
 
     $rootScope.setLayout('layout-d');
     $scope.post_task = {};
@@ -65,6 +68,98 @@ function PostDetailDataController(
     $scope.canCreatePostInSurvey = PostSurveyService.canCreatePostInSurvey;
     $scope.mapDataLoaded = false;
     $scope.form_attributes = [];
+
+    activate();
+
+    function activate() {
+        // Set page title to post title, if there is one available.
+        if ($scope.post.title && $scope.post.title.length) {
+            $scope.$emit('setPageTitle', $scope.post.title);
+        } else {
+            $translate('post.post_details').then(function (title) {
+                $scope.title = title;
+                $scope.$emit('setPageTitle', title);
+            });
+        }
+
+        // Load the post form
+        if ($scope.post.form && $scope.post.form.id) {
+            $q.all([
+                FormEndpoint.getFresh({id: $scope.post.form.id}),
+                FormStageEndpoint.queryFresh({formId:  $scope.post.form.id, postStatus: $scope.post.status}).$promise,
+                FormAttributeEndpoint.queryFresh({formId: $scope.post.form.id}).$promise,
+                TagEndpoint.queryFresh().$promise
+            ]).then(function (results) {
+                $scope.form = results[0];
+                $scope.form_name = results[0].name;
+                $scope.form_description = results[0].description;
+                $scope.form_color = results[0].color;
+                $scope.tags = results[3];
+                // Set page title to '{form.name} Details' if a post title isn't provided.
+                if (!$scope.post.title) {
+                    $translate('post.type_details', { type: results[0].name }).then(function (title) {
+                        $scope.$emit('setPageTitle', title);
+                    });
+                }
+                var tasks = _.sortBy(results[1], 'priority');
+                var attrs = _.chain(results[2])
+                    .sortBy('priority')
+                    .value();
+
+                var attributes = [];
+                _.each(attrs, function (attr) {
+                    if (!_.contains($scope.attributesToIgnore, attr.key)) {
+                        attributes.push(attr);
+                    }
+                });
+                attributes = (attributes.length) ? attributes : attrs;
+
+                angular.forEach(attributes, function (attr) {
+                    this[attr.key] = attr;
+
+                }, $scope.form_attributes);
+                // Make the first task visible
+                if (!_.isEmpty(tasks) && tasks.length > 1) {
+                    $scope.visibleTask = tasks[1].id;
+                    tasks[1].hasFileIcon = true;
+                }
+
+                _.each(tasks, function (task) {
+                    // Set post task id
+                    // NOTE: This assumes that there is only one Post Task per Post
+                    if (task.type === 'post') {
+                        $scope.post_task = task;
+                    } else {
+                        // Mark completed tasks
+                        if (_.indexOf($scope.post.completed_stages, task.id) !== -1) {
+                            task.completed = true;
+                        }
+                    }
+                });
+
+                // Remove post task from tasks
+                tasks = _.filter(tasks, function (task) {
+                    return task.type !== 'post';
+                });
+
+                $scope.tasks = tasks;
+                // Figure out which tasks have values
+                $scope.tasks_with_attributes = [];
+                _.each($scope.post.values, function (value, key) {
+
+                    if ($scope.form_attributes[key]) {
+                        $scope.tasks_with_attributes.push($scope.form_attributes[key].form_stage_id);
+                    }
+                });
+                $scope.tasks_with_attributes = _.uniq($scope.tasks_with_attributes);
+            });
+
+        } else {
+            // for when user switch between posts, if new post has no form, there are no tasks either.
+            $scope.tasks = [];
+        }
+    }
+
     $scope.publishedFor = function () {
         if ($scope.post.status === 'draft') {
             return 'post.publish_for_you';
@@ -79,91 +174,6 @@ function PostDetailDataController(
     $scope.stageIsComplete = function (stageId) {
         return _.includes($scope.post.completed_stages, stageId);
     };
-
-    // Set page title to post title, if there is one available.
-    if ($scope.post.title && $scope.post.title.length) {
-        $scope.$emit('setPageTitle', $scope.post.title);
-    } else {
-        $translate('post.post_details').then(function (title) {
-            $scope.title = title;
-            $scope.$emit('setPageTitle', title);
-        });
-    }
-
-    // Load the post form
-    if ($scope.post.form && $scope.post.form.id) {
-        $q.all([
-            FormEndpoint.getFresh({id: $scope.post.form.id}),
-            FormStageEndpoint.queryFresh({formId:  $scope.post.form.id, postStatus: $scope.post.status}).$promise,
-            FormAttributeEndpoint.queryFresh({formId: $scope.post.form.id}).$promise,
-            TagEndpoint.queryFresh().$promise
-        ]).then(function (results) {
-            $scope.form = results[0];
-            $scope.form_name = results[0].name;
-            $scope.form_description = results[0].description;
-            $scope.form_color = results[0].color;
-            $scope.tags = results[3];
-            // Set page title to '{form.name} Details' if a post title isn't provided.
-            if (!$scope.post.title) {
-                $translate('post.type_details', { type: results[0].name }).then(function (title) {
-                    $scope.$emit('setPageTitle', title);
-                });
-            }
-            var tasks = _.sortBy(results[1], 'priority');
-            var attrs = _.chain(results[2])
-                .sortBy('priority')
-                .value();
-
-            var attributes = [];
-            _.each(attrs, function (attr) {
-                if (!_.contains($scope.attributesToIgnore, attr.key)) {
-                    attributes.push(attr);
-                }
-            });
-            attributes = (attributes.length) ? attributes : attrs;
-
-            angular.forEach(attributes, function (attr) {
-                this[attr.key] = attr;
-
-            }, $scope.form_attributes);
-            // Make the first task visible
-            if (!_.isEmpty(tasks) && tasks.length > 1) {
-                $scope.visibleTask = tasks[1].id;
-                tasks[1].hasFileIcon = true;
-            }
-
-            _.each(tasks, function (task) {
-                // Set post task id
-                // NOTE: This assumes that there is only one Post Task per Post
-                if (task.type === 'post') {
-                    $scope.post_task = task;
-                } else {
-                    // Mark completed tasks
-                    if (_.indexOf($scope.post.completed_stages, task.id) !== -1) {
-                        task.completed = true;
-                    }
-                }
-            });
-
-            // Remove post task from tasks
-            tasks = _.filter(tasks, function (task) {
-                return task.type !== 'post';
-            });
-
-            $scope.tasks = tasks;
-
-            // Figure out which tasks have values
-            $scope.tasks_with_attributes = [];
-            _.each($scope.post.values, function (value, key) {
-
-                if ($scope.form_attributes[key]) {
-                    $scope.tasks_with_attributes.push($scope.form_attributes[key].form_stage_id);
-                }
-            });
-            $scope.tasks_with_attributes = _.uniq($scope.tasks_with_attributes);
-
-        });
-    }
 
     $scope.taskHasValues = function (task) {
         return _.contains($scope.tasks_with_attributes, task.id);
