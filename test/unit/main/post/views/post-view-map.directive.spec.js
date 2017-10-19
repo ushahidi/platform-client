b3a8e3a71a79fb7fb9c555aa72c7a39eb6c863aa
Romina Suarez <rowasc@gmail.com>
2017-10-18 23:17:08 -0300

    WIP Fix tests

--- test/unit/main/post/views/post-view-map.directive.spec.js
+++ test/unit/main/post/views/post-view-map.directive.spec.js
@@ -101,22 +101,6 @@ describe('post view map directive', function () {
         expect(markers.addTo).toHaveBeenCalledWith(map);
     });
 
-    it('reload geojson when filters change', function () {
-        element = $compile(element)($scope);
-        $rootScope.$digest();
-        isolateScope = element.isolateScope();
-
-        isolateScope.$apply(function () {
-            isolateScope.filters = {
-                status : 'published'
-            };
-        });
-
-        expect(L.geoJson).toHaveBeenCalled();
-        expect(L.geoJson.calls.count()).toEqual(3);
-        expect(geojson.addTo).toHaveBeenCalledWith(map);
-    });
-
     it('shows a popup when marker is clicked', function () {
         map.options.clustering = false;
         var layer = geojson.getLayers()[0];
@@ -147,94 +131,5 @@ describe('post view map directive', function () {
             layer: child
         });
         expect(child.openPopup.calls.count()).toEqual(2);
-    });
-    it('does not call PostEndpoint.geojson when filter.q changes and qEnabled is false', function () {
-        /**
-         * create directive with isolate scope
-         */
-        element = $compile(element)($scope);
-        $rootScope.$digest();
-        isolateScope = element.isolateScope();
-        spyOn(isolateScope, 'loadPosts').and.callThrough();
-        /**
-         * Set default qEnabled value to ensure we are dealing
-         * with the directive's logic and not the postFilters defaults
-         * @type {boolean}
-         */
-        isolateScope.$apply(function () {
-            isolateScope.filters = {
-                q : 'U'
-            };
-        });
-        /**
-         * When we initially setup the directive Postendpoint's geojson should
-         * only be called once.
-         */
-        expect(L.geoJson.calls.count()).toEqual(1);
-
-        expect(PostEndpoint.geojson).toHaveBeenCalledTimes(1);
-        /**
-         * Apply q filter to scope. Don't change qEnabled to true.
-         * This should not call PostEndpoint.geojson because the app doesn't
-         * care for changes in the input until the qEnabled property is true
-         */
-        isolateScope.$apply(function () {
-            isolateScope.filters = {
-                q : 'U'
-            };
-        });
-        $rootScope.$digest();
-        expect(PostEndpoint.geojson).toHaveBeenCalledTimes(1);
-        /**
-         * Enable q so that loadPosts is called.
-         * @type {boolean}
-         */
-        PostFilters.qEnabled = true;
-        $rootScope.$digest();
-        expect(isolateScope.loadPosts.calls.count()).toEqual(2);
-
-        /**
-         * Change q twice. qEnabled is now false because we already 'clicked'and set it back to false.
-         * This means loadPosts should be called zero times when we apply our q filter changes at the
-         * moment.
-         */
-        isolateScope.$apply(function () {
-            isolateScope.filters = {
-                q : 'Us'
-            };
-        });
-        $rootScope.$digest();
-        isolateScope.$apply(function () {
-            isolateScope.filters = {
-                q : 'Ushahidi'
-            };
-        });
-        $rootScope.$digest();
-        /**
-         * qEnabled is set to false successfuly if this equals 2
-         */
-        expect(isolateScope.loadPosts.calls.count()).toEqual(2);
-
-        /**
-         * Set qEnabled to true. This should generate 2 more call to loadPosts,
-         * because of how the basic setup is done, within_km generates a second
-         * watch being triggered when we call getQueryParams
-         *
-         *
-         */
-        PostFilters.qEnabled = true;
-        $rootScope.$digest();
-        expect(isolateScope.loadPosts.calls.count()).toEqual(4);
-        /**
-         * If I (for whatever reason) enable qEnabled with no changes,
-         * loadPosts is still called. This is because we are actually changing
-         * the filters and cannot avoid it without impacting the feature, so it's
-         * the expected behavior.
-         * @type {boolean}
-         */
-        PostFilters.qEnabled = true;
-        $rootScope.$digest();
-        expect(isolateScope.loadPosts.calls.count()).toEqual(5);
-
     });
 });
