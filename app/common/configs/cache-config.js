1abe6645798a23e6a1693e7b7e3d8539776cf249
Robbie Mackay <rm@robbiemackay.com>
2017-10-27 13:15:37 +1300

    Reduce cache time to 3 mins

--- app/common/configs/cache-config.js
+++ app/common/configs/cache-config.js
@@ -1,6 +1,6 @@
 module.exports = ['CacheFactoryProvider', function (CacheFactoryProvider) {
     angular.extend(CacheFactoryProvider.defaults, {
-        maxAge: 15 * 60 * 1000, // 15 mins
+        maxAge: 3 * 60 * 1000, // 15 mins
         cacheFlushInterval: 60 * 60 * 1000, // This cache will clear itself every hour.
         storageMode: 'localStorage',
         storagePrefix: 'ush-caches.',
