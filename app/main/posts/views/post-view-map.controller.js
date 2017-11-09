d41825c3b44da555dba84fd7c02aa3c6ef8ce4bb
Romina Suarez <rowasc@gmail.com>
2017-11-09 17:56:22 -0300

    Map with controller

---
+++ app/main/posts/views/post-view-map.controller.js
@@ -0,0 +1,189 @@
+module.exports = ['PostEndpoint', 'Maps', '_', 'PostFilters', 'Leaflet', '$q', '$rootScope', '$compile', '$location', '$timeout', '$scope',
+    function PostViewMap(PostEndpoint, Maps, _, PostFilters, L, $q, $rootScope, $compile, $location, $timeout, $scope) {
+        $scope.filters = PostFilters.getFilters();
+        $scope.isLoading = {state: true};
+        $scope.map;
+        $scope.markers;
+        $scope.limit = 200;
+        $scope.requestBlockSize = 5;
+        $scope.numberOfChunks = 0;
+        $scope.currentGeoJsonRequests = [];
+
+        // Start loading data
+        $scope.posts = loadPosts();
+        $scope.loadPosts = loadPosts;
+        /**
+         * functions
+         */
+        $scope.clearData = function () {
+            if ($scope.markers) {
+                $scope.map.removeLayer($scope.markers);
+                $scope.markers = undefined;
+            }
+        }
+
+        $scope.addPostsToMap = function (posts) {
+            var geojson = L.geoJson(posts, {
+                pointToLayer: Maps.pointToLayer,
+                onEachFeature: $scope.onEachFeature
+            });
+
+            if ($scope.map.options.clustering) {
+
+                $scope.markers = $scope.markers ? $scope.markers : L.markerClusterGroup();
+                // This has to be done individually.
+                // Using clusterLayer.addLayers() breaks the clustering.
+                // Need to investigate as this should have been fixing in v1.0.0
+                angular.forEach(geojson.getLayers(), function (layer) {
+                    $scope.markers.addLayer(layer);
+                });
+            } else {
+                $scope.markers = geojson;
+            }
+            $scope.markers.addTo($scope.map);
+
+            if (posts.features.length > 0) {
+                $scope.map.fitBounds(geojson.getBounds());
+            }
+            // Focus map on data points but..
+            // Avoid zooming further than 15 (particularly when we just have a single point)
+            if ($scope.map.getZoom() > 15) {
+                $scope.map.setZoom(15);
+            }
+        }
+
+
+        $scope.onEachFeature  = function (feature, layer) {
+            layer.on('click', function (e) {
+                // Grab the layer that was actually clicked on
+                var layer = e.layer;
+                // If we somehow got the feature group: grab the first child
+                // because the FeatureGroup doesn't get added to the map when clustering
+                if (layer instanceof L.FeatureGroup) {
+                    layer = layer.getLayers()[0];
+                }
+
+                if (layer.getPopup()) {
+                    layer.openPopup();
+                } else {
+                    $scope.getPostDetails(feature).then(function (details) {
+                        var scope = $rootScope.$new();
+                        scope.post = details;
+                        scope.goToPost = $scope.goToPost;
+                        scope.selectedPost = {post : details};
+
+                        var el = $compile('<post-card selected-post="selectedPost" post="post" short-content="true" click-action="goToPost"></post-card>')(scope);
+
+                        layer.bindPopup(el[0], {
+                            'minWidth': '300',
+                            'maxWidth': '300',
+                            'className': 'pl-popup'
+                        });
+                        layer.openPopup();
+                    });
+                }
+            });
+        }
+
+        $scope.goToPost = function (post) {
+            $location.path('/posts/' + post.id);
+        }
+        $scope.getPostDetails = function (feature) {
+            return PostEndpoint.get({id: feature.properties.id}).$promise;
+        }
+
+        $scope.cancelCurrentRequests = function () {
+            _.each($scope.currentGeoJsonRequests, function (request) {
+                request.$cancelRequest();
+            });
+            $scope.currentGeoJsonRequests = [];
+        };
+
+        function loadPosts(query, offset, currentBlock) {
+            offset = offset || 0;
+            currentBlock = currentBlock || 1;
+
+            query = query || PostFilters.getQueryParams($scope.filters);
+
+            var conditions = _.extend(query, {
+                limit: $scope.limit,
+                offset: offset,
+                has_location: 'mapped'
+            });
+            $scope.isLoading.state = true;
+
+            var request = PostEndpoint.geojson(conditions);
+            $scope.currentGeoJsonRequests.push(request);
+
+            return request.$promise.then(function (posts) {
+
+                // Set number of chunks
+                if (offset === 0 && posts.total > $scope.limit) {
+                    $scope.numberOfChunks = Math.floor((posts.total - $scope.limit) / $scope.limit);
+                    $scope.numberOfChunks += ((posts.total - $scope.limit) % $scope.limit) > 0 ? 1 : 0;
+                }
+
+                // Retrieve blocks of chunks
+                // At the end of a block request the next block of chunks
+                if ($scope.numberOfChunks > 0 && currentBlock === 1) {
+                    var block = $scope.numberOfChunks > $scope.requestBlockSize ? $scope.requestBlockSize : $scope.numberOfChunks;
+                    $scope.numberOfChunks -= $scope.requestBlockSize;
+                    while (block > 0) {
+                        block -= 1;
+                        offset += $scope.limit;
+                        loadPosts(query, offset, block).then($scope.addPostsToMap);
+                    }
+                }
+
+                if ($scope.numberOfChunks <= 0) {
+                    $scope.isLoading.state = false;
+                }
+                return posts;
+            });
+        }
+
+        $scope.reloadMapPosts = function (query) {
+            var test = $scope.loadPosts(query);
+            test.then($scope.addPostsToMap);
+        }
+
+
+        $scope.watchFilters = function () {
+            // whenever the qEnabled var changes, do a dummy update of $scope.filters.reactToQEnabled
+            // to force the $scope.filters watcher to run
+            //$rootScope.$watchTrue(function () {
+            $scope.$watch(function () {
+                return PostFilters.qEnabled;
+            }, function () {
+                if (PostFilters.qEnabled === true) {
+                    $scope.filters.reactToQEnabled = $scope.filters.reactToQEnabled ? !$scope.filters.reactToQEnabled : true;
+                }
+            });
+            // whenever filters change, reload the posts on the map
+            $scope.$watch(function () {
+                return $scope.filters;
+            }, function (newValue, oldValue) {
+                var diff = _.omit(newValue, function (value, key, obj) {
+                    return _.isEqual(oldValue[key], value);
+                });
+                var diffLength = _.keys(diff).length;
+                var qDiffOnly =  _.keys(diff).length === 1 && diff.hasOwnProperty('q');
+                /**
+                 * We only want to call reloadMapPosts if we :
+                 * - Have changes other than q= in the filters
+                 * - Only q= changed but we also have enabled the q filter
+                 */
+                if (diffLength > 0 && !qDiffOnly || (diffLength >= 1 && PostFilters.qEnabled === true)) {
+                    $scope.cancelCurrentRequests();
+                    $scope.clearData();
+                    $scope.reloadMapPosts();
+                }
+                if (PostFilters.qEnabled === true) {
+                    PostFilters.qEnabled = false;
+                }
+            }, true);
+        }
+
+
+    }
+];
--- app/main/posts/views/post-view-map.directive.js
+++ app/main/posts/views/post-view-map.directive.js
@@ -5,218 +5,46 @@ function PostViewMap(PostEndpoint, Maps, _, PostFilters, L, $q, $rootScope, $com
     return {
         restrict: 'E',
         replace: true,
-        scope: {
-            // filters: '=',
-            // isLoading: '='
-        },
+        scope: {},
         template: require('./post-view-map.html'),
-        link: PostViewMapLink,
-        controller: function ($scope) {
-            $scope.filters = PostFilters.getFilters();
-            $scope.isLoading = {state: true};
-            $scope.getUIClass = $location.path() === '/map/noui' ? 'map-only' : 'full-size';
-        }
+        link: {
+            pre: function ($scope, element, attrs) {
+                $scope.getUIClass = 'full-size';
+            },
+            post: PostViewMapLink
+        },
+        controller: require('./post-view-map.controller.js')
     };
 
     function PostViewMapLink($scope, element, attrs, controller) {
-        var map, markers;
-
-        var limit = 200;
-        var requestBlockSize = 5;
-        var numberOfChunks = 0;
-        var currentGeoJsonRequests = [];
-
-        // Start loading data
-        var posts = loadPosts();
+        console.log(controller.$scope);
         var createMapDirective =  Maps.createMap(element[0].querySelector('#map'));
         var createMap = createMapDirective.then(function (data) {
-            map = data;
+            $scope.map = data;
+            setTimeout(function () {
+                $scope.map.invalidateSize();
+                $scope.$apply(); //this triggers a $digest
+            }, 2);
         });
         // When data is loaded
         $q.all({
             map: createMap,
-            posts: posts
+            posts: $scope.posts
         })
         .then(function (data) {
-            addPostsToMap(data.posts);
+            $scope.addPostsToMap(data.posts);
             return data;
         })
-        .then(watchFilters)
+        .then($scope.watchFilters)
         ;
 
         // Cleanup leaflet map
         $scope.$on('$destroy', function () {
-            if (map) {
-                map.remove();
+            if ($scope.map) {
+                $scope.map.remove();
             }
         });
 
 
-        function clearData() {
-            if (markers) {
-                map.removeLayer(markers);
-                markers = undefined;
-            }
-        }
-
-        function addPostsToMap(posts) {
-            var geojson = L.geoJson(posts, {
-                pointToLayer: Maps.pointToLayer,
-                onEachFeature: onEachFeature
-            });
-
-            if (map.options.clustering) {
-
-                markers = markers ? markers : L.markerClusterGroup();
-                // This has to be done individually.
-                // Using clusterLayer.addLayers() breaks the clustering.
-                // Need to investigate as this should have been fixing in v1.0.0
-                angular.forEach(geojson.getLayers(), function (layer) {
-                    markers.addLayer(layer);
-                });
-            } else {
-                markers = geojson;
-            }
-            markers.addTo(map);
-
-            if (posts.features.length > 0) {
-                map.fitBounds(geojson.getBounds());
-            }
-            // Focus map on data points but..
-            // Avoid zooming further than 15 (particularly when we just have a single point)
-            if (map.getZoom() > 15) {
-                map.setZoom(15);
-            }
-
-        }
-        function watchFilters() {
-            // whenever the qEnabled var changes, do a dummy update of $scope.filters.reactToQEnabled
-            // to force the $scope.filters watcher to run
-            //$rootScope.$watchTrue(function () {
-            $scope.$watch(function () {
-                return PostFilters.qEnabled;
-            }, function () {
-                if (PostFilters.qEnabled === true) {
-                    $scope.filters.reactToQEnabled = $scope.filters.reactToQEnabled ? !$scope.filters.reactToQEnabled : true;
-                }
-            });
-            // whenever filters change, reload the posts on the map
-            $scope.$watch(function () {
-                return $scope.filters;
-            }, function (newValue, oldValue) {
-                var diff = _.omit(newValue, function (value, key, obj) {
-                    return _.isEqual(oldValue[key], value);
-                });
-                var diffLength = _.keys(diff).length;
-                var qDiffOnly =  _.keys(diff).length === 1 && diff.hasOwnProperty('q');
-                /**
-                 * We only want to call reloadMapPosts if we :
-                 * - Have changes other than q= in the filters
-                 * - Only q= changed but we also have enabled the q filter
-                 */
-                if (diffLength > 0 && !qDiffOnly || (diffLength >= 1 && PostFilters.qEnabled === true)) {
-                    $scope.cancelCurrentRequests();
-                    clearData();
-                    reloadMapPosts();
-                }
-                if (PostFilters.qEnabled === true) {
-                    PostFilters.qEnabled = false;
-                }
-            }, true);
-        }
-        function reloadMapPosts(query) {
-            var test = loadPosts(query);
-            test.then(addPostsToMap);
-        }
-
-        function goToPost(post) {
-            $location.path('/posts/' + post.id);
-        }
-
-        function onEachFeature(feature, layer) {
-            layer.on('click', function (e) {
-                // Grab the layer that was actually clicked on
-                var layer = e.layer;
-                // If we somehow got the feature group: grab the first child
-                // because the FeatureGroup doesn't get added to the map when clustering
-                if (layer instanceof L.FeatureGroup) {
-                    layer = layer.getLayers()[0];
-                }
-
-                if (layer.getPopup()) {
-                    layer.openPopup();
-                } else {
-                    getPostDetails(feature).then(function (details) {
-                        var scope = $rootScope.$new();
-                        scope.post = details;
-                        scope.goToPost = goToPost;
-                        scope.selectedPost = {post : details};
-
-                        var el = $compile('<post-card selected-post="selectedPost" post="post" short-content="true" click-action="goToPost"></post-card>')(scope);
-
-                        layer.bindPopup(el[0], {
-                            'minWidth': '300',
-                            'maxWidth': '300',
-                            'className': 'pl-popup'
-                        });
-                        layer.openPopup();
-                    });
-                }
-            });
-        }
-
-        function getPostDetails(feature) {
-            return PostEndpoint.get({id: feature.properties.id}).$promise;
-        }
-
-        $scope.cancelCurrentRequests = function () {
-            _.each(currentGeoJsonRequests, function (request) {
-                request.$cancelRequest();
-            });
-            currentGeoJsonRequests = [];
-        };
-
-        function loadPosts(query, offset, currentBlock) {
-            offset = offset || 0;
-            currentBlock = currentBlock || 1;
-
-            query = query || PostFilters.getQueryParams($scope.filters);
-
-            var conditions = _.extend(query, {
-                limit: limit,
-                offset: offset,
-                has_location: 'mapped'
-            });
-            $scope.isLoading.state = true;
-
-            var request = PostEndpoint.geojson(conditions);
-            currentGeoJsonRequests.push(request);
-
-            return request.$promise.then(function (posts) {
-
-                // Set number of chunks
-                if (offset === 0 && posts.total > limit) {
-                    numberOfChunks = Math.floor((posts.total - limit) / limit);
-                    numberOfChunks += ((posts.total - limit) % limit) > 0 ? 1 : 0;
-                }
-
-                // Retrieve blocks of chunks
-                // At the end of a block request the next block of chunks
-                if (numberOfChunks > 0 && currentBlock === 1) {
-                    var block = numberOfChunks > requestBlockSize ? requestBlockSize : numberOfChunks;
-                    numberOfChunks -= requestBlockSize;
-                    while (block > 0) {
-                        block -= 1;
-                        offset += limit;
-                        loadPosts(query, offset, block).then(addPostsToMap);
-                    }
-                }
-
-                if (numberOfChunks <= 0) {
-                    $scope.isLoading.state = false;
-                }
-                return posts;
-            });
-        }
     }
 }
