26d611e727b775ec8eaf531b139d54e6b45d443c
Anna Iosif <hej@annaiosif.se>
2017-11-16 15:13:58 +0100

    adding http-interceptor to trigger loading-bar on http-requests

--- app/app.js
+++ app/app.js
@@ -140,7 +140,36 @@ angular.module('app',
         return window.ushahidi.bootstrapConfig ?
             _.indexBy(window.ushahidi.bootstrapConfig, 'id') :
             { map: {}, site: {}, features: {} };
-    }])// inject the router instance into a `run` block by name
+    }])
+    .factory('loading', function (LoadingProgress, $q, $injector) {
+        /* an interceptor that triggers loading-state in the beginning of
+        * a http-request and remove it when all requests are done */
+
+        return {
+            request: function (config) {
+                if (LoadingProgress.getLoadingState() !== true) {
+                    LoadingProgress.setLoadingState(true);
+                }
+                // we want this to trigger isSaving everytime except when a lock-updates
+                if (config.method === 'PUT' && config.url.indexOf('lock') === -1) {
+                    LoadingProgress.setSavingState(true);
+                }
+                return config;
+            },
+            response: function (response) {
+                var httpService = $injector.get('$http');
+                if (httpService.pendingRequests.length === 0) {
+                    LoadingProgress.setLoadingState(false);
+                    LoadingProgress.setSavingState(false);
+                }
+                return response || $q.when(response);
+            }
+        };
+    })
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('loading');
+    })
+    // inject the router instance into a `run` block by name
     .run(function ($uiRouter, $trace, $location) {
         /** uncomment this to enable the visualizer
         var pluginInstance = $uiRouter.plugin(Visualizer);**/
