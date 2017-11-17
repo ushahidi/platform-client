92c7c60ebd5220c3a0a1c79b00639f635c7b7894
Anna Iosif <hej@annaiosif.se>
2017-11-17 22:47:31 +0100

    adding tests

---
+++ test/unit/mock/services/loadingProgress.service.js
@@ -0,0 +1,13 @@
+module.exports = [function () {
+    return {
+        watchTransitions: function () {},
+        getLoadingState: function () {
+            return true;
+        },
+        getSavingState: function () {
+            return true;
+        },
+        setLoadingState: function () {},
+        setSavingState: function () {}
+    };
+}];
