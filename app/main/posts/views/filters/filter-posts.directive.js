57b4ae998ec9920a761bcec6178d76e06f4d753c
Robbie Mackay <rm@robbiemackay.com>
2017-11-24 12:35:12 +1300

    Move searchbar input into filter-posts and use a single dropdown
    - Use main filter dropdown when searching for text.
    - Change handling to only close modal when clicking outside the filters
      not just outside the modal itself

--- app/main/posts/views/filters/filter-posts.directive.js
+++ app/main/posts/views/filters/filter-posts.directive.js
@@ -13,34 +13,57 @@ function FilterPostsDirective() {
     };
 }
 
-FilterPostsController.$inject = ['$scope', '$timeout','ModalService', 'PostFilters', '$state'];
-function FilterPostsController($scope, $timeout, ModalService, PostFilters, $state) {
+FilterPostsController.$inject = ['$scope', 'PostFilters', '$state', '$document', '$element'];
+function FilterPostsController($scope, PostFilters, $state, $document, $element) {
     $scope.searchSavedToggle = false;
     $scope.qFilter = '';
+    $scope.status = { isopen: false };
     $scope.view = $state.params.view;
+    $scope.hideDropdown = hideDropdown;
+    $scope.showDropdown = showDropdown;
+    $scope.removeQueryFilter = removeQueryFilter;
+    $scope.applyFilters = applyFilters;
     activate();
 
-    $scope.status = {
-        isopen: false
-    };
-
-    $scope.toggleDropdown = function ($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.status.isopen = !$scope.status.isopen;
-    };
     function activate() {
         // @todo define initial filter values
         // $scope.$watch('filters', handleFilterChange, true);
+
+        // Watch all click events on the page
+        $document.on('click', handleDocumentClick);
+
+        $scope.$on('$destroy', () => {
+            $document.off('click', handleDocumentClick);
+        });
     }
 
-    $scope.applyFilters = function () {
+    function applyFilters() {
         PostFilters.qEnabled = true;
         PostFilters.reactiveFilters = true;
         $scope.status.isopen = false;
-    };
+    }
 
-    $scope.removeQueryFilter = function () {
+    function removeQueryFilter() {
         PostFilters.clearFilter('q', '');
-    };
+    }
+
+    function showDropdown() {
+        $scope.status.isopen = true;
+    }
+
+    function hideDropdown() {
+        $scope.status.isopen = false;
+    }
+
+    // Close the dropdown for any clicks outside of the filters
+    function handleDocumentClick(evt) {
+        // If the click was inside the directive
+        if (evt && $element && $element[0].contains(evt.target)) {
+            // Ignore it
+            return;
+        }
+
+        // Otherwise close the dropdown
+        $scope.$apply(hideDropdown);
+    }
 }
