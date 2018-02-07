b562ccc95d9aba0bee32c861ecb1be0336e03b9a
Romina Suarez <romina@ushahidi.com>
2018-02-07 13:52:08 -0300

    feat(filter-category.directive): when a user selects all children of an unselected parent, the parent is selected

--- app/main/posts/views/filters/filter-category.directive.js
+++ app/main/posts/views/filters/filter-category.directive.js
@@ -18,6 +18,11 @@ function CategorySelectDirective(TagEndpoint, _) {
         scope.categories = [];
         scope.parents = [];
         scope.selectedCategories = [];
+        scope.internallyModified = false;
+        scope.internal = function () {
+            scope.internallyModified = false;
+        };
+
         activate();
 
         function activate() {
@@ -67,8 +72,8 @@ function CategorySelectDirective(TagEndpoint, _) {
             }
         }
 
-        function saveValueToView(selectedCategories) {
-            selectedCategories = handleParents(selectedCategories);
+        function saveValueToView(selectedCategories, oldSelection) {
+            selectedCategories = handleParents(selectedCategories, oldSelection);
             ngModel.$setViewValue(angular.copy(selectedCategories), ngModel.$viewValue);
         }
         // unselect children when parent is unselected and all children are selected
@@ -78,28 +83,95 @@ function CategorySelectDirective(TagEndpoint, _) {
         // ngModel.$viewValue > [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
         // current results in this after processing >  [1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12]
         // SHOULD be:  [1, 2, 3, 5, 6, 9, 10, 11, 12] (which means, unselect children [7, 8]
-        function handleParents(newSelection, old) {
+        function handleParents(newSelection, oldSelection) {
             let result = angular.copy(newSelection);
-            const findRemoved = _.difference(ngModel.$viewValue, newSelection);
-            const findAdded = _.difference(newSelection, ngModel.$viewValue);
-            const parentsRemoved = _.filter(findRemoved, (category) => { // return parents that were removed
-                return isParent(category);
-            });
+            if (!scope.internallyModified) {
+                result = childrenRemovedResult(newSelection, oldSelection, result);
+            }
+            if (!scope.internallyModified) {
+                result = parentsRemovedResult(newSelection, oldSelection, result);
+            }
+            if (!scope.internallyModified) {
+                result = parentsAddedResult(newSelection, oldSelection, result);
+            }
+            if (!scope.internallyModified) {
+                result = childrenAllSelectedResult(newSelection, oldSelection, result);
+            }
+            return result;
+        }
 
-            if (findRemoved.length > 0 && parentsRemoved.length > 0) {
-                _.each(parentsRemoved, (parent) => {
-                    const toReject = allChildrenToUnselect(parent);
-                    result = _.without(result, ...toReject);
+        // unselect parent if child was removed
+        function childrenRemovedResult(newSelection, oldSelection, result) {
+            const findRemoved = _.difference(oldSelection, newSelection);
+            const childRemoved = _.filter(findRemoved, (category) => { // return parents that were removed
+                return !isParent(category);
+            });
+            if (childRemoved.length > 0) {
+                result = _.filter(result, (categoryId) => {
+                    return categoryId !== parentToUnSelect(childRemoved[0]);
                 });
-            } else if (findAdded.length > 0) {
+                scope.internallyModified = true;
+            }
+            return result;
+        }
+
+        function parentsAddedResult(newSelection, oldSelection, result) {
+            const findAdded = _.difference(newSelection, oldSelection);
+            if (findAdded.length > 0) {
                 _.each(newSelection, (any) => {
                     const toAdd = childrenToReSelect(any);
                     if (toAdd.length > 0) {
-                        result = result.concat(toAdd);
+                        result = _.uniq(result.concat(toAdd));
+                        scope.internallyModified = true;
                     }
                 });
             }
             return result;
+        }
+
+        function childrenAllSelectedResult(newSelection, oldSelection, result) {
+            const findAdded = _.difference(newSelection, oldSelection);
+            if (findAdded.length > 0) {
+                // for each parent in scope.parents, check if ALL their children are selected if a parent is unselected
+                _.each(scope.parents, (parentCategory) => {
+                    const findParent = _.find(newSelection, (itm) => itm === parentCategory.id);
+                    if (!findParent) {
+                        const contained = _.every(_.pluck(parentCategory.children, 'id'), (childId) => {
+                            return _.find(newSelection, (itm) => itm === childId);
+                        });
+                        if (contained) {
+                            result = _.uniq(result.concat(parentCategory.id));
+                            scope.internallyModified = true;
+                        }
+                    }
+                });
+            }
+            return result;
+        }
+
+        function parentsRemovedResult(newSelection, oldSelection, result) {
+            const findRemoved = _.difference(oldSelection, newSelection);
+            const parentsRemoved = _.filter(findRemoved, (category) => { // return parents that were removed
+                return isParent(category);
+            });
+            if (findRemoved.length > 0 && parentsRemoved.length > 0) {
+                _.each(parentsRemoved, (parent) => {
+                    const toReject = allChildrenToUnselect(parent);
+                    result = _.without(result, ...toReject);
+                    scope.internallyModified = true;
+                });
+            }
+            return result;
+        }
+
+        function parentToUnSelect(childId) {
+            const parent = _.filter(scope.parents, (parent) => {
+                const childFound = _.find(parent.children, (child) => {
+                    return child.id === childId;
+                });
+                return !!childFound;
+            });
+            return parent ? parent[0].id : null;
         }
 
         function childrenToReSelect(parentId) {
