80fbdaac3480be684f9369cc49a7caedbe008945
Romina Suarez <romina@ushahidi.com>
2018-02-09 16:20:15 -0300

    feat(add-category) Reflect changes in categories (add category) in the post-data-editor without browser reload.

--- app/main/posts/modify/post-data-editor.directive.js
+++ app/main/posts/modify/post-data-editor.directive.js
@@ -272,11 +272,23 @@ function PostDataEditorController(
                     // adding category-objects attribute-options
                     attr.options = _.chain(attr.options)
                         .map(function (category) {
-                            return _.findWhere(categories, {id: category});
+                            const ret = _.findWhere(categories, {id: category});
+                            if (ret.children.length > 0) {
+                                ret.children = _.chain(ret.children)
+                                    .map(function (child) {
+                                        return _.findWhere(categories, {id: child.id});
+                                    })
+                                    .filter()
+                                    .value();
+                            }
+                            return ret;
                         })
                         .filter()
                         .value();
+
+
                 }
+
                 // @todo don't assign default when editing? or do something more sane
                 if (!$scope.post.values[attr.key]) {
                     if (attr.input === 'location') {
