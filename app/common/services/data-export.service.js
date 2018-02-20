6c92c6f23cab56d955bbfc64a02d6189ea76c845
Anna Iosif <hej@annaiosif.se>
2018-02-20 14:43:33 +0100

    adding download-functionality once job is done + error-handling

--- app/common/services/data-export.service.js
+++ app/common/services/data-export.service.js
@@ -1,9 +1,11 @@
 module.exports = DataExport;
 
 
-DataExport.$inject = ['$rootScope', 'ConfigEndpoint', 'ExportJobEndpoint', '$q', '_', '$window', '$timeout', 'Notify', '$location', '$interval'];
-function DataExport($rootScope, ConfigEndpoint, ExportJobEndpoint, $q, _, $window, $timeout, Notify, $location, $interval) {
+DataExport.$inject = ['ExportJobEndpoint', 'Notify', '$window', '$timeout', '$interval', 'CONST'];
+function DataExport(ExportJobEndpoint,  Notify, $window, $timeout, $interval, CONST) {
+
     function startExport(query) {
+        query.entity_type = 'post';
         ExportJobEndpoint.save(query).$promise.then(function (job) {
             loadingStatus(true, null, job.id);
             startPolling(job);
@@ -14,17 +16,24 @@ function DataExport($rootScope, ConfigEndpoint, ExportJobEndpoint, $q, _, $windo
 
     function startPolling(job) {
         var polling = $interval(function () {
+            // TODO: Question: Should we also delete the job after the job is done/failed?
             ExportJobEndpoint.getFresh({id: job.id}).$promise.then(function (response) {
                 if (response.status === 'done') {
-                    // TODO: Handle url + update user
+                    // when job is done, we stop the polling...
                     $interval.cancel(polling);
-                } else if (response.status === 'err') {
-                    // TODO: Update user
-                    $interval.cancel(polling);
+                    // ..and download the file
+                    downloadFile(response.url);
+                    // finally we notify the user
+                    loadingStatus(false);
                 }
-            });
-            // TODO: Do we need a timeout? How many times should we poll before we stop?
-        }, 30000);
+            }, function (err) {
+                    // if there is an error while exporting we stop the polling
+                    $interval.cancel(polling);
+                    // and notify the user
+                    Notify.apiErrors(err);
+                }
+            );
+        }, CONST.EXPORT_POLLING_INTERVAL, CONST.EXPORT_POLLING_COUNT);
 
     }
 
@@ -32,63 +41,24 @@ function DataExport($rootScope, ConfigEndpoint, ExportJobEndpoint, $q, _, $windo
         ExportJobEndpoint.delete({id: jobId});
     }
 
-    // TODO: What of below code do we need?
-    function showCSVResults(response, format) {
-        // Save export data to file
-        var filename = response[0].name + '-' + (new Date()).toISOString().substring(0, 10) + '.' + format,
-            data = response[1].data;
-
-        handleArrayBuffer(filename, data, 'csv');
-        loadingStatus(false);
-        $rootScope.$broadcast('event:data_export:complete');
-        return filename;
+    function downloadFile(downloadUrl) {
+        var URL = $window.URL || $window.webkitURL;
+        // Create anchor link
+        var anchor = angular.element('<a/>');
+        anchor.attr({
+            href: downloadUrl
+        });
+        // download file
+        angular.element(document.body).append(anchor);
+        anchor[0].click();
+        anchor[0].remove();
+        // TODO: Question, is below needed? Its from the old code but I am not sure what it does?
+        $timeout(function () {
+            URL.revokeObjectURL(downloadUrl);
+        }, 100); // cleanup
     }
 
-    function handleArrayBuffer(filename, data, type) {
-        /**
-         * If we have the HTML5 Api for File available we use that. If not, a Blob
-         */
-        function createCSVFile() {
-            if (_.isFunction(File)) {
-                return new File([data], filename, { type: type });
-            } else {
-                return new Blob([data], { type: type });
-            }
-        }
-
-        var blob = createCSVFile();
-
-        if (!_.isUndefined($window.navigator.msSaveBlob)) {
-            /** IE specific workaround for "HTML7007"
-             * https://stackoverflow.com/questions/20310688/blob-download-not-working-in-ie
-            **/
-            $window.navigator.msSaveBlob(blob, filename);
-        } else {
-            // var URL = $window.URL || $window.webkitURL;
-            var downloadUrl = URL.createObjectURL(blob);
-            if (filename) {
-                // use HTML5 a[download] attribute to specify filename
-                // Create anchor link
-                var anchor = angular.element('<a/>');
-                anchor.attr({
-                    href: downloadUrl,
-                    download: filename
-                });
-                angular.element(document.body).append(anchor);
-                anchor[0].click();
-                anchor[0].remove();
-            } else {
-                $location.url(downloadUrl);
-            }
-
-            $timeout(function () {
-                URL.revokeObjectURL(downloadUrl);
-            }, 100); // cleanup
-
-        }
-    }
-
-    function loadingStatus(status, err, jobId) {
+    function loadingStatus(status, err, job) {
         var message, // holds the message to the user
             action, // holds info for the action-button
             icon, // holds info about icon to use in the notification
@@ -103,7 +73,7 @@ function DataExport($rootScope, ConfigEndpoint, ExportJobEndpoint, $q, _, $windo
                     callback: cancelExport,
                     text: 'notify.export.cancel_export',
                     actionClass: 'button-destructive',
-                    jobId: jobId
+                    callbackArg: job.id
                 };
                 icon = 'ellipses';
                 loading = true;
@@ -118,10 +88,6 @@ function DataExport($rootScope, ConfigEndpoint, ExportJobEndpoint, $q, _, $windo
     }
 
     return {
-        startExport: startExport,
-        startPolling: startPolling,
-        showCSVResults: showCSVResults,
-        handleArrayBuffer: handleArrayBuffer,
-        loadingStatus: loadingStatus
+        startExport: startExport
     };
 }
