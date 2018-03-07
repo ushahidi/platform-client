5e57d3530469695f81de579ddfa0a4263934dfd1
Anna <anna@ushahidi.com>
2018-03-07 12:13:30 +0100

    adding controller + checking feature-flag

--- app/settings/surveys/targeted-surveys/targeted-edit.controller.js
+++ app/settings/surveys/targeted-surveys/targeted-edit.controller.js
@@ -1,1 +1,22 @@
 // re-route if feature flag is not enabled
+module.exports = [
+    '$scope',
+    'Features',
+    '$state',
+function (
+    $scope,
+    Features,
+    $state
+) {
+    Features.loadFeatures()
+           .then(() => {
+            // WARNING: Add Feature Flag
+            $scope.targetedSurveysEnabled = true;
+            // $scope.targetedSurveysEnabled = Features.isFeatureEnabled('targeted-surveys');
+
+            // reroute if feature-flag is not turned on
+            if (!$scope.targetedSurveysEnabled) {
+                $state.go('settings.surveys.create');
+            }
+        });
+}];
