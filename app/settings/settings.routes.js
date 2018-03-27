9f1da68eac3e3deb1390194a31055e760233db5d
Anna <anna@ushahidi.com>
2018-03-27 13:43:08 +0200

    adding route for published targeted surveys

--- app/settings/settings.routes.js
+++ app/settings/settings.routes.js
@@ -59,7 +59,7 @@ function (
         {
             name: 'settings.surveys',
             url: '/settings/surveys',
-            controller: require('./surveys/surveys.controller.js'),
+            controller: require('./surveys/surveys.controller.  js'),
             template: require('./surveys/surveys.html')
         }
     )
@@ -74,7 +74,22 @@ function (
     .state(
         {
             name: 'settings.surveys.targeted',
+            url: '/targeted-survey'
+        }
+    )
+    .state(
+        {
+            name: 'settings.surveys.targeted.create',
             url: '/targeted-survey/create',
+            controller: require('./surveys/targeted-surveys/targeted-edit.controller.js'),
+            template: require('./surveys/targeted-surveys/targeted-survey-edit.html')
+        }
+    )
+
+    .state(
+        {
+            name: 'settings.surveys.targeted.published',
+            url: '/targeted-survey/published/:id',
             controller: require('./surveys/targeted-surveys/targeted-edit.controller.js'),
             template: require('./surveys/targeted-surveys/targeted-survey-edit.html')
         }
