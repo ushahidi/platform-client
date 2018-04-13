234b1c5e30b58cb1faf1e81e5f9b325b31e27086
Anna <anna@ushahidi.com>
2018-04-13 14:48:24 +0200

    changing notification-message to what will be sent

--- app/settings/surveys/targeted-surveys/targeted-edit.controller.js
+++ app/settings/surveys/targeted-surveys/targeted-edit.controller.js
@@ -294,7 +294,7 @@ function (
 
     function saveContacts(id) {
         FormContactEndpoint.save({formId: id, contacts: $scope.textBoxNumbers, country_code: $scope.selectedCountry.country_code}).$promise.then(function (response) {
-            let messages = $scope.finalNumbers.goodNumbers.length;
+            let messages = $scope.finalNumbers.goodNumbers.length * $scope.survey.attributes.length;
             let notifyMessage = messages === 1 ? 'survey.targeted_survey.publish_notification_one' : 'survey.targeted_survey.publish_notification_many';
             Notify.notifyAction(notifyMessage, {messages}, false, 'thumb-up', 'circle-icon confirmation', {callback: goToDataView, text: 'survey.targeted_survey.notification_button', callbackArg: id, actionClass: 'button button-alpha'});
             $state.go('settings.surveys', {}, { reload: true });
