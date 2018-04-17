5cc0f8387fb17a845e012d77e8df303237426923
Carolyn Commons <crcommons@gmail.com>
2018-04-17 13:59:57 -0600

    adding proptypes to experiment; adjusting so fooBar works -- must not use camel case. exploring why next

--- app/common/directives/ush-logo-react/ush-logo.jsx
+++ app/common/directives/ush-logo-react/ush-logo.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 import { render } from 'react-dom';
 
 
 class MyComponent extends Component {
+
   render() {
     return <div>
-      <p>FooBar: {this.props.fooBar}</p>
+      <p>Foo: {this.props.foo}</p>
       <p>Baz: {this.props.baz}</p>
     </div>
   }
 }
 
 export default MyComponent;
+
+MyComponent.propTypes = {
+    foo: PropTypes.number,
+    baz: PropTypes.number,
+}
