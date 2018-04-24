8a05492271f8fc27d87087b9e7635795e5b0a6f0
Carolyn Commons <crcommons@gmail.com>
2018-04-23 18:00:05 -0600

    testing linter

--- app/common/directives/ush-logo-react/ush-logo.jsx
+++ app/common/directives/ush-logo-react/ush-logo.jsx
@@ -3,9 +3,7 @@ import PropTypes from 'prop-types'
 import { render } from 'react-dom';
 
 
-class MyComponent extends Component {
-
-  render() {
+class MyComponent extends Component{render() {
     return <div>
       <p>Foo: {this.props.foo}</p>
       <p>Baz: {this.props.baz}</p>
