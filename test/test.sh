b19c35c72e1affa84d76b0adb25995f548f04543
Carolyn Commons <crcommons@gmail.com>
2018-04-24 20:01:46 -0600

    chore(test.sh): removing linting with travis
    Need to stop linting by Travis because only staged/changed files should be linted

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 set -ev
 
-if [ "${TEST_SUITE}" = "lint" ]; then
-    npm run lint
-fi
+# CANNOT RUN LINTING BECAUSE LINTING SHOULD ONLY RUN ON NEW FILES NOW
+# if [ "${TEST_SUITE}" = "lint" ]; then
+#     npm run lint
+# fi
 
 if [ "${TEST_SUITE}" = "unit" ]; then
     NODE_ENV=test gulp test
