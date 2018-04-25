55fb6c844d7385fb0ca2ce41bdd606d7d9ecbde3
Mark <mark@kinstelli.com>
2018-04-25 12:18:07 -0500

    fix: update nums to fake nums

--- test/unit/settings/surveys/targeted-edit.controller.spec.js
+++ test/unit/settings/surveys/targeted-edit.controller.spec.js
@@ -186,7 +186,7 @@ describe('setting create targeted survey controller', function () {
                     }
                 };
                 $scope.selectedCountry = { 'country_name': 'United States', 'dial_code': '+1', 'country_code': 'US' };
-                $scope.textBoxNumbers = '3172351967, 3176742327';
+                $scope.textBoxNumbers = '3175551967, 3175552327';
                 $scope.completeStepTwo();
                 expect($scope.activeStep).toEqual(3);
                 expect($scope.stepTwoWarning).toEqual(false);
@@ -199,7 +199,7 @@ describe('setting create targeted survey controller', function () {
                     }
                 };
                 $scope.selectedCountry = { 'country_name': 'United States', 'dial_code': '+1', 'country_code': 'US' };
-                $scope.textBoxNumbers = '3172351967, 3176742327, 123';
+                $scope.textBoxNumbers = '3175551967, 3175552327, 123';
                 $scope.completeStepTwo();
                 expect($scope.activeStep).toEqual(2);
                 expect($scope.stepTwoWarning).toEqual(false);
@@ -212,20 +212,20 @@ describe('setting create targeted survey controller', function () {
                     }
                 };
                 $scope.selectedCountry = { 'country_name': 'United States', 'dial_code': '+1', 'country_code': 'US' };
-                $scope.textBoxNumbers = '3172351967, 3176742327, 123';
+                $scope.textBoxNumbers = '3175551967, 3175552327, 123';
                 $scope.completeStepTwo();
                 expect($scope.activeStep).toEqual(2);
                 expect($scope.stepTwoWarning).toEqual(true);
             });
             it('should reset the phone number object, which enables user to add to/edit numbers', function () {
                 $scope.finalNumbers = {
-                    goodNumbers: ['3172351967', '3176742327'],
-                    goodNumbersString: '3172351967, 3176742327,',
+                    goodNumbers: ['3175551967', '3175552327'],
+                    goodNumbersString: '3175551967, 3175552327,',
                     badNumbersString: '12345',
                     repeatCount: 1,
                     storageObj: {
-                        '3172351967': '3172351967',
-                        '3176742327': '3176742327'
+                        '3175551967': '3175551967',
+                        '3175552327': '3175552327'
                     },
                     badNumberCount: 0
                 };
@@ -253,71 +253,71 @@ describe('setting create targeted survey controller', function () {
                 };
             });
             it('should validate phone numbers as GOOD numbers', function () {
-                $scope.runValidations('3172351967, 3176742327');
+                $scope.runValidations('3175551967, 3175552327');
                 expect($scope.finalNumbers).toEqual({
-                    goodNumbers: ['+13172351967', '+13176742327'],
-                    goodNumbersString: '+13172351967,+13176742327,',
+                    goodNumbers: ['+13175551967', '+13175552327'],
+                    goodNumbersString: '+13175551967,+13175552327,',
                     badNumbersString: '',
                     repeatCount: 0,
                     storageObj: {
-                        '+13172351967': '+13172351967',
-                        '+13176742327': '+13176742327'
+                        '+13175551967': '+13175551967',
+                        '+13175552327': '+13175552327'
                     },
                     badNumberCount: 0
                 });
             });
             it('should validate phone numbers with special characters as GOOD numbers', function () {
-                $scope.runValidations('317-235-1967, 317.674.2327');
+                $scope.runValidations('317-555-1967, 317.555.2327');
                 expect($scope.finalNumbers).toEqual({
-                    goodNumbers: ['+13172351967', '+13176742327'],
-                    goodNumbersString: '+13172351967,+13176742327,',
+                    goodNumbers: ['+13175551967', '+13175552327'],
+                    goodNumbersString: '+13175551967,+13175552327,',
                     badNumbersString: '',
                     repeatCount: 0,
                     storageObj: {
-                        '+13172351967': '+13172351967',
-                        '+13176742327': '+13176742327'
+                        '+13175551967': '+13175551967',
+                        '+13175552327': '+13175552327'
                     },
                     badNumberCount: 0
                 });
             });
             it('should remove duplicate numbers', function () {
-                $scope.runValidations('3172351967, 3176742327, 3172351967');
+                $scope.runValidations('3175551967, 3175552327, 3175551967');
                 expect($scope.finalNumbers).toEqual({
-                    goodNumbers: ['+13172351967', '+13176742327'],
-                    goodNumbersString: '+13172351967,+13176742327,',
+                    goodNumbers: ['+13175551967', '+13175552327'],
+                    goodNumbersString: '+13175551967,+13175552327,',
                     badNumbersString: '',
                     repeatCount: 1,
                     storageObj: {
-                        '+13172351967': '+13172351967',
-                        '+13176742327': '+13176742327'
+                        '+13175551967': '+13175551967',
+                        '+13175552327': '+13175552327'
                     },
                     badNumberCount: 0
                 });
             });
             it('should separate good and bad numbers', function () {
-                $scope.runValidations('3172351967, 3176742327, 3172351967, 12345');
+                $scope.runValidations('3175551967, 3175552327, 3175551967, 12345');
                 expect($scope.finalNumbers).toEqual({
-                    goodNumbers: ['+13172351967', '+13176742327'],
-                    goodNumbersString: '+13172351967,+13176742327,',
+                    goodNumbers: ['+13175551967', '+13175552327'],
+                    goodNumbersString: '+13175551967,+13175552327,',
                     badNumbersString: '12345,',
                     repeatCount: 1,
                     storageObj: {
-                        '+13172351967': '+13172351967',
-                        '+13176742327': '+13176742327'
+                        '+13175551967': '+13175551967',
+                        '+13175552327': '+13175552327'
                     },
                     badNumberCount: 1
                 });
             });
             it('should disregard empty elements', function () {
-                $scope.runValidations('3172351967, 3176742327, 3172351967, 12345,, ');
+                $scope.runValidations('3175551967, 3175552327, 3175551967, 12345,, ');
                 expect($scope.finalNumbers).toEqual({
-                    goodNumbers: ['+13172351967', '+13176742327'],
-                    goodNumbersString: '+13172351967,+13176742327,',
+                    goodNumbers: ['+13175551967', '+13175552327'],
+                    goodNumbersString: '+13175551967,+13175552327,',
                     badNumbersString: '12345,',
                     repeatCount: 1,
                     storageObj: {
-                        '+13172351967': '+13172351967',
-                        '+13176742327': '+13176742327'
+                        '+13175551967': '+13175551967',
+                        '+13175552327': '+13175552327'
                     },
                     badNumberCount: 1
                 });
