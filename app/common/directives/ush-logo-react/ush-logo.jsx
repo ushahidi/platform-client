d9044a1841f6c419df299078cd579a1879da7a4d
Carolyn Commons <crcommons@gmail.com>
2018-05-09 12:06:33 -0600

    style(New react components and settings.module): cleaning up spacing and uch

--- app/common/directives/ush-logo-react/ush-logo.jsx
+++ app/common/directives/ush-logo-react/ush-logo.jsx
@@ -1,36 +1,41 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-//import { connect } from "react-redux";
-import { bindActionCreators } from 'redux';
-import Uirouter from '@uirouter/angularjs';
-
-import connectWithStore from '../../../react-transition/connectWithStore.jsx';
-import * as UshLogoActions from './ush-logo.actions';
-import { getFormsFromState } from './ush-logo.reducer';
+import React from "react";
+import PropTypes from "prop-types";
+// import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import connectWithStore from "../../../react-transition/connectWithStore";
+import * as UshLogoActions from "./ush-logo.actions";
+import { getFormsFromState } from "./ush-logo.reducer";
 
 const MyComponent = props => (
   <div>
-    { props.forms && props.forms[0].id}
+    {props.forms && props.forms[0].id}
     <p className="foo">Foo: {props.foo}</p>
     <p className="baz">Baz: {props.baz}</p>
   </div>
 );
 
-const mapStateToProps = (state) => {
-    return {
-        forms: getFormsFromState(state)
-    }
-};
+const mapStateToProps = state => ({
+  forms: getFormsFromState(state)
+});
 
 function mapDispatchToProps(dispatch) {
-    return {
-        UshLogoActions: bindActionCreators(UshLogoActions, dispatch),
-    };
-};
+  return {
+    UshLogoActions: bindActionCreators(UshLogoActions, dispatch)
+  };
+}
 
 MyComponent.propTypes = {
   foo: PropTypes.number.isRequired,
-  baz: PropTypes.number.isRequired
+  baz: PropTypes.number.isRequired,
+  forms: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired
+    })
+  ).isRequired
 };
 
-export default connectWithStore(MyComponent, mapStateToProps, mapDispatchToProps);
+export default connectWithStore(
+  MyComponent,
+  mapStateToProps,
+  mapDispatchToProps
+);
--- app/settings/settings.module.js
+++ app/settings/settings.module.js
@@ -1,29 +1,41 @@
-import { react2angular } from 'react2angular';
-import TestRouteContainer from './TestRouteContainer.jsx';
+import { react2angular } from "react2angular";
+import TestRouteContainer from "./TestRouteContainer";
 
+angular
+  .module("ushahidi.settings", [])
+  .directive(
+    "afterImportCsv",
+    require("./data-import/data-after-import.directive.js")
+  )
+  .directive("importerCsv", require("./data-import/data-import.directive.js"))
+  .service("ImportNotify", require("./data-import/import.notify.service.js"))
 
-angular.module('ushahidi.settings', [])
-.directive('afterImportCsv', require('./data-import/data-after-import.directive.js'))
-.directive('importerCsv', require('./data-import/data-import.directive.js'))
-.service('ImportNotify', require('./data-import/import.notify.service.js'))
+  .directive("surveyEditor", require("./surveys/survey-editor.directive.js"))
+  .directive("surveyTaskCreate", require("./surveys/task-create.directive.js"))
+  .directive(
+    "surveyAttributeCreate",
+    require("./surveys/attribute-create.directive.js")
+  )
+  .directive(
+    "surveyAttributeEditor",
+    require("./surveys/attribute-editor.directive.js")
+  )
+  .service("SurveyNotify", require("./surveys/survey.notify.service.js"))
 
-.directive('surveyEditor', require('./surveys/survey-editor.directive.js'))
-.directive('surveyTaskCreate', require('./surveys/task-create.directive.js'))
-.directive('surveyAttributeCreate', require('./surveys/attribute-create.directive.js'))
-.directive('surveyAttributeEditor', require('./surveys/attribute-editor.directive.js'))
-.service('SurveyNotify', require('./surveys/survey.notify.service.js'))
+  .directive("settingsMap", require("./site/map.directive.js"))
+  .directive("settingsEditor", require("./site/editor.directive.js"))
 
-.directive('settingsMap', require('./site/map.directive.js'))
-.directive('settingsEditor', require('./site/editor.directive.js'))
+  .directive("filterUsers", require("./users/filter-users.directive.js"))
 
-.directive('filterUsers', require('./users/filter-users.directive.js'))
+  .directive("customRoles", require("./roles/roles.directive.js"))
+  .directive("customRolesEditor", require("./roles/editor.directive.js"))
 
-.directive('customRoles', require('./roles/roles.directive.js'))
-.directive('customRolesEditor', require('./roles/editor.directive.js'))
+  .directive("customWebhooks", require("./webhooks/webhooks.directive.js"))
+  .directive("customWebhooksEditor", require("./webhooks/editor.directive.js"))
 
-.directive('customWebhooks', require('./webhooks/webhooks.directive.js'))
-.directive('customWebhooksEditor', require('./webhooks/editor.directive.js'))
+  .component(
+    "testRouteContainer",
+    react2angular(TestRouteContainer, ["state", "id"])
+  )
 
-.component('testRouteContainer', react2angular(TestRouteContainer, ['state', 'id']))
-
-.config(require('./settings.routes.js'));
+  .config(require("./settings.routes.js"));
