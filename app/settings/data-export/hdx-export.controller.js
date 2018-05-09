e373c2e64a27140cd636f7a66fdf75d747c60765
Anna <anna@ushahidi.com>
2018-05-09 17:48:44 +0200

    fetching surveys and fields for hdx-screen

--- app/settings/data-export/hdx-export.controller.js
+++ app/settings/data-export/hdx-export.controller.js
@@ -1,4 +1,54 @@
 module.exports = [
+    '$scope',
+    '$rootScope',
+    'Features',
+    '$location',
+    'FormEndpoint',
+    'FormAttributeEndpoint',
+    '_',
 function (
+    $scope,
+    $rootScope,
+    Features,
+    $location,
+    FormEndpoint,
+    FormAttributeEndpoint,
+    _
 ) {
+    $scope.selectedFields = [];
+    // Change layout class
+    $rootScope.setLayout('layout-c');
+    // Change mode
+    $scope.$emit('event:mode:change', 'settings');
+
+    // Check if hxl-feature is enabled
+    Features.loadFeatures().then(function () {
+        $scope.hxlEnabled = Features.isFeatureEnabled('hxl');
+        // Redirect to home if not enabled
+        if(!$scope.hxlEnabled) {
+            return $location.path('/');
+        }
+    });
+
+    activate();
+
+    function activate () {
+        getForms();
+    }
+
+    function getForms() {
+        FormEndpoint.queryFresh({targeted_survey: false}).$promise.then(function (response) {
+            $scope.forms = response;
+            attachAttributes();
+        });
+    }
+
+    function attachAttributes() {
+        // requesting attributes and attaches them to the correct form
+        _.each($scope.forms, function (form) {
+            FormAttributeEndpoint.query({formId: form.id}).$promise.then(function (response) {
+                form.attributes = response;
+            });
+        });
+    }
 }];
--- test/unit/settings/data-export/hdx-export.controller.spec.js
+++ test/unit/settings/data-export/hdx-export.controller.spec.js
@@ -2,7 +2,10 @@ describe('hdx-export-controller', function () {
 
     var  $scope,
         $rootScope,
-        $controller;
+        $controller,
+        $location,
+        FormEndpoint,
+        FormAttributeEndpoint;
 
     beforeEach(function () {
         var testApp = makeTestApp();
@@ -11,10 +14,13 @@ describe('hdx-export-controller', function () {
         angular.mock.module('testApp');
     });
 
-    beforeEach(angular.mock.inject(function (_$rootScope_, _$controller_) {
+    beforeEach(angular.mock.inject(function (_$rootScope_, _$controller_, _$location_, _FormEndpoint_, _FormAttributeEndpoint_) {
         $rootScope = _$rootScope_;
         $controller = _$controller_;
         $scope = _$rootScope_.$new();
+        FormEndpoint = _FormEndpoint_;
+        FormAttributeEndpoint = _FormAttributeEndpoint_;
+        $location = _$location_;
 
         $rootScope.hasPermission = function () {
             return true;
@@ -22,6 +28,7 @@ describe('hdx-export-controller', function () {
     }));
 
     beforeEach(function () {
+        $rootScope.setLayout = function () {};
         $controller('hdx-export-controller', {
             $scope: $scope,
             $rootScope: $rootScope
@@ -30,10 +37,17 @@ describe('hdx-export-controller', function () {
         $rootScope.$digest();
         $rootScope.$apply();
     });
-    describe ('controller', function () {
-        it('should be true', function () {
-            $scope.thisIsTrue = true;
-            expect($scope.thisIsTrue).toEqual(true);
+    describe('getForms-function', function () {
+        it('should get all available forms', function () {
+            spyOn(FormEndpoint, 'queryFresh').and.callThrough();
+            $scope.getForms();
+            expect(FormEndpoint.queryFresh).toHaveBeenCalled();
+            expect($scope.forms.length).toEqual(2);
+        });
+        it('should call attachAttributes when done fetching forms', function () {
+            spyOn($scope, 'attachAttributes');
+            $scope.getForms();
+            expect($scope.attachAttributes).toHaveBeenCalled();
         });
     });
 });
