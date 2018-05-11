4cd6d091a55c75d68748cf719b64ae408a28310f
Robbie Mackay <rm@robbiemackay.com>
2018-05-11 16:14:06 +1200

    text: Check Link component calls state.go
    Mock state and validate calls to state.go

--- app/react-transition/Link.spec.jsx
+++ app/react-transition/Link.spec.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 import angular from "angular";
-import uirouter from "@uirouter/angularjs";
 import Link from "./Link";
 
+const mockState = {
+    "go": jest.fn()
+};
+
 beforeAll(() => {
-  angular.module("testApp", [uirouter]);
+  angular.module("testApp", []).service("$state", () => mockState);
   angular.bootstrap(document, ["testApp"]);
 });
 
@@ -21,8 +24,8 @@ test("Link accepts a route string, replace bool, id, className, title, and child
 });
 
 test("Link routes to new component", () => {
-  const mockNavigation = jest.fn();
   const link = shallow(<Link to="posts.data">Click me!</Link>);
   link.find("a").simulate("click");
-  expect(mockNavigation.mock.calls.length).toEqual(1);
+  expect(mockState.go).toHaveBeenCalledTimes(1);
+  expect(mockState.go).toHaveBeenCalledWith("posts.data");
 });
