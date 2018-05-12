97ad60d7af8fa57405ea690c85e0d5bae8f2f919
Anna <anna@ushahidi.com>
2018-05-13 00:52:00 +0200

    export-screen with multiple dropdowns text, buttons and loadingbar

--- app/settings/data-export/hdx-export.controller.js
+++ app/settings/data-export/hdx-export.controller.js
@@ -4,14 +4,25 @@ module.exports = [
     'Features',
     '$location',
     'HxlExport',
+    '_',
+    'LoadingProgress',
 function (
     $scope,
     $rootScope,
     Features,
     $location,
-    HxlExport
+    HxlExport,
+    _,
+    LoadingProgress
 ) {
-    $scope.selectedFields = [];
+    $scope.selectAttribute = selectAttribute;
+    $scope.addAnother = addAnother;
+    $scope.range = range;
+    $scope.selectTag = selectTag;
+    $scope.exportData = exportData;
+    $scope.selectAll = selectAll;
+    $scope.isLoading = LoadingProgress.getLoadingState;
+
     // Change layout class
     $rootScope.setLayout('layout-c');
     // Change mode
@@ -27,10 +38,87 @@ function (
     });
 
     activate();
+    function range(attribute) {
+        if (!attribute.nbAttributes) {
+            attribute.nbAttributes = 1;
+        }
+        return _.range(attribute.nbAttributes);
+    }
+
+    function addAnother(attribute) {
+        attribute.nbAttributes++;
+    }
 
     function activate() {
         HxlExport.getFormsWithTags().then((formsWithTags)=> {
             $scope.forms = formsWithTags;
         });
+    }
+
+    function selectAll(form) {
+        if (form.selected) {
+            _.each(form.attributes, (attribute) => {
+                attribute.selected = [attribute.id];
+            });
+        } else {
+            _.each(form.attributes, (attribute) => {
+                attribute.selected = [];
+            });
+        }
+    }
+
+    function selectTag(attribute) {
+        if (attribute.selectedTag && attribute.selectedTag.tag_name) {
+            attribute.pretty = '#' + attribute.selectedTag.tag_name;
+        } else {
+            attribute.pretty = '';
+        }
+        attribute.nbAttributes = 1;
+        attribute.selectedAttributes = {};
+    }
+
+    function selectAttribute(attribute) {
+        attribute.pretty = '#' + attribute.selectedTag.tag_name;
+        _.each(attribute.selectedAttributes, (hxl_attribute) => {
+            if (hxl_attribute !== '') {
+                attribute.pretty = attribute.pretty + '+' + hxl_attribute.attribute;
+            }
+        });
+    }
+
+    function formatIds() {
+        let data = [];
+        _.each($scope.forms, (form) => {
+                _.each(form.attributes, (attribute) => {
+                        if (attribute.selected && attribute.selected.length > 0) {
+                            let ids = {
+                                    form_attribute_id : attribute.id,
+                                    hxl_tag: null
+                                };
+
+                            if (attribute.selectedTag) {
+                                ids.hxl_tag = {
+                                    hxl_tag_id: attribute.selectedTag.id
+                                };
+
+                                if (attribute.selectedAttributes) {
+                                    ids.hxl_tag.hxl_attribute_ids = [];
+                                    _.each(attribute.selectedAttributes, (hxlAttribute) => {
+                                        ids.hxl_tag.hxl_attribute_ids.push(hxlAttribute.id);
+                                    });
+                                }
+                            }
+                            data.push(ids);
+                        }
+                    });
+            });
+
+        return data;
+    }
+
+    function exportData() {
+        let data = formatIds();
+        //Connect to endpoint
+        console.log(data);
     }
 }];
