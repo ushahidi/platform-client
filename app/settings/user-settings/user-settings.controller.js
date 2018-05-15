81e15acda8be5c6b462b9bedc0cd02ccc33ed8c5
Anna <anna@ushahidi.com>
2018-05-15 12:35:40 +0200

    adding route and controller for user-settings

---
+++ app/settings/user-settings/user-settings.controller.js
@@ -0,0 +1,17 @@
+module.exports = [
+    '$scope',
+    '$rootScope',
+    'Features',
+    '$location',
+    'HxlExport',
+    '_',
+    'LoadingProgress',
+function (
+    $scope,
+    $rootScope,
+    Features,
+    $location,
+    HxlExport,
+    _,
+    LoadingProgress
+) {}];
