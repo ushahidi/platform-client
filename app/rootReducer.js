39c09473bf4ddc808feb2d8203da7f8b9367f8ed
Carolyn Commons <crcommons@gmail.com>
2018-06-04 18:16:07 -0400

    fix(rootReducer, settings.module, settings.routes): changed users to people and user to person

--- app/rootReducer.js
+++ app/rootReducer.js
@@ -1,11 +1,11 @@
 import { combineReducers } from "redux";
 import forms from "./common/directives/ush-logo-react/ush-logo.reducer";
-import users from "./react/common/state/users/users.reducers";
+import people from "./react/common/state/people/people.reducers";
 import roles from "./react/common/state/roles/roles.reducers";
 
 // Combine all reducers into one root reducer
 export default combineReducers({
     forms,
-    users,
+    people,
     roles
 });
