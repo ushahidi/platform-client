fce2e26ffb9343036c5ba52d79b2f04bf8cbf6d5
Anna <anna@ushahidi.com>
2018-06-25 08:53:05 +0200

    Adding button-component and button-story

---
+++ component-library/components/atoms/button/stories.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+
+import Button from "./button";
+
+storiesOf("Button", module)
+    .add("default", () => (
+        <Button buttonType="BUTTON" onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ))
+    .add("beta", () => (
+        <Button buttonType="BETA" onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ))
+    .add("gamma", () => (
+        <Button buttonType="GAMMA" onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ))
+    .add("destructive", () => (
+        <Button buttonType="DESTRUCTIVE" onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ))
+    .add("default, disabled", () => (
+        <Button buttonType="BUTTON" disabled onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ))
+    .add("beta, disabled", () => (
+        <Button buttonType="BETA" disabled onClick={action("clicked")}>
+            Learn more
+        </Button>
+    ));
