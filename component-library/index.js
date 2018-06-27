af8f08b6c440928e07f47e92c090c147b7ec8ab7
Anna <anna@ushahidi.com>
2018-06-27 11:59:59 +0200

    Exporting components

---
+++ component-library/index.js
@@ -0,0 +1,16 @@
+export { default as Button } from "./components/atoms/button/button";
+export { default as Checkbox } from "./components/atoms/checkbox/checkbox";
+export { default as Dropdown } from "./components/atoms/dropdown/dropdown";
+export { default as Input } from "./components/atoms/input/input";
+export { default as Label } from "./components/atoms/label/label";
+export {
+    default as Breadcrumbs
+} from "./components/molecules/breadcrumbs/breadcrumbs";
+export {
+    default as Formfield
+} from "./components/molecules/formfields/formfield";
+export {
+    default as Fieldset
+} from "./components/molecules/formfields/fieldset";
+export { default as Listitem } from "./components/molecules/listitem/listitem";
+export { default as Form } from "./components/organisms/form/form";
