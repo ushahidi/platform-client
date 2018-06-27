83d41a742e1577f9050dda165093cbc30e1fefe5
Robbie Mackay <rm@robbiemackay.com>
2018-06-28 10:47:16 +1200

    feat: Sanitize data sentry to sentry
    - Strip sensitive values from data sent to sentry

--- app/common/raven/raven.js
+++ app/common/raven/raven.js
@@ -12,6 +12,23 @@ if (RAVEN_URL) {
             environment: ENVIRONMENT,
             tags: {
                 git_commit: GIT_COMMIT
+            },
+            sanitizeKeys: [/Authorization/i, /password/i, /accessToken/i, /api_key/i, /client_secret/i],
+            dataCallback: (data) => {
+                // Replace stringified sensitive info
+                if (data.message) {
+                    data.message = data.message.replace(/"Authorization":"(.*?)"/, '"Authorization":"****"');
+                    data.message = data.message.replace(/"client_secret":"(.*?)"/, '"client_secret":"****"');
+                    data.message = data.message.replace(/"password":"(.*?)"/, '"password":"****"');
+                    data.message = data.message.replace(/"accessToken":"(.*?)"/, '"accessToken":"****"');
+                }
+
+                if (data.fingerprint && data.fingerprint[0]) {
+                    data.fingerprint[0] = data.fingerprint[0].replace(/"Authorization":"(.*?)"/, '"Authorization":"****"');
+                    data.fingerprint[0] = data.fingerprint[0].replace(/"client_secret":"(.*?)"/, '"client_secret":"****"');
+                    data.fingerprint[0] = data.fingerprint[0].replace(/"password":"(.*?)"/, '"password":"****"');
+                    data.fingerprint[0] = data.fingerprint[0].replace(/"accessToken":"(.*?)"/, '"accessToken":"****"');
+                }
             }
         })
         .addPlugin(require('raven-js/plugins/angular'), angular)
