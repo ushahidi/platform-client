1e26c5f6e72d2ece21ead32311667938ee8ff434
Robbie Mackay <rm@robbiemackay.com>
2018-07-09 20:36:40 +1200

    feat: Don't define settings and auth routes when in an iframe
    Refs ushahidi/platfrom#1609

--- app/common/directives/embed-only.directive.js
+++ app/common/directives/embed-only.directive.js
@@ -1,4 +1,4 @@
-module.exports = EmbedOnlyDirective;
+import isEmbed from '../services/isEmbed';
 
 EmbedOnlyDirective.$inject = [];
 function EmbedOnlyDirective() {
@@ -8,13 +8,13 @@ function EmbedOnlyDirective() {
     };
 }
 
-EmbedOnlyController.$inject = ['$scope', '$element', '$attrs', '$rootScope', '_', '$window'];
-function EmbedOnlyController($scope, $element, $attrs, $rootScope, _, $window) {
-    var globalEmbed = ($window.self !== $window.top) ? true : false;
-
-    if (globalEmbed && ($attrs.embedOnly === 'false')) {
+EmbedOnlyController.$inject = ['$scope', '$element', '$attrs'];
+function EmbedOnlyController($scope, $element, $attrs) {
+    if (isEmbed() && ($attrs.embedOnly === 'false')) {
         $element.addClass('hidden');
-    } else if (!globalEmbed && ($attrs.embedOnly === 'true')) {
+    } else if (!isEmbed() && ($attrs.embedOnly === 'true')) {
         $element.addClass('hidden');
     }
 }
+
+export default EmbedOnlyDirective;
