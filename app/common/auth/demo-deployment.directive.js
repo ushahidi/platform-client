6bd0f44dda68aacb02226b59415efc198dc07e0a
will <will.doran@riseup.net>
2018-08-17 16:32:12 -0400

    Setting custom messages

--- app/common/auth/demo-deployment.directive.js
+++ app/common/auth/demo-deployment.directive.js
@@ -11,15 +11,51 @@ function DemoDeploymentDirective() {
 }
 
 DemoDeploymentController.$inject = [
-    '$scope'
+    '$scope',
+    '$location',
+    'ConfigEndpoint',
+    'PostEndpoint',
+    'moment'
 ];
 function DemoDeploymentController(
-    $scope
+    $scope,
+    $location,
+    ConfigEndpoint,
+    PostEndpoint,
+    moment
 ) {
 
+    var USHAHIDI = 'Ushahidi';
+
+    $scope.limitReached = false;
+    $scope.expired = false;
+    $scope.days_remaining = 30;
+
+    $scope.upgrade = upgrade;
     activate();
 
     function activate() {
+        ConfigEndpoint.get({id: 'site'}).$promise.then(function (site) {
+            $scope.site_name = site.name ? site.name : USHAHIDI;
+            var expiration_date = moment(site.expiration_date);
+            var extension_date = moment(site.extension_date);
+            var now = moment();
 
+            $scope.expired = now > expiration_date || now > extension_date;
+
+            if (!$scope.expired) {
+                $scope.days_remaining = expiration_date.diff(extension_date, 'days');
+                PostEndpoint.stats().$promise.then(function (results) {
+                    if (results.totals[0]) {
+                        $scope.limitReached = results.totals.values.totals >= 25;
+                    }
+                });
+            }
+        });
+
+    }
+
+    function upgrade() {
+        $location.path('settings/plans');
     }
 }
