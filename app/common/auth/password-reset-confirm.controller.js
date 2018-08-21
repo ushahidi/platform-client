8593e452ac1ccdeb0760f3464d7ce0ded424dfe1
Mark Kinsella <mark@kinstelli.com>
2018-08-21 15:24:03 -0500

    fix(password-reset-confirm-controller) urldecode url param before sending to API via POST

--- app/common/auth/password-reset-confirm.controller.js
+++ app/common/auth/password-reset-confirm.controller.js
@@ -3,7 +3,7 @@ module.exports = PasswordResetConfirmController;
 PasswordResetConfirmController.$inject = ['$rootScope', 'PasswordReset', '$location', '$transition$'];
 function PasswordResetConfirmController($rootScope, PasswordReset, $location, $transition$) {
     var $scope = $rootScope.$new();
-    $scope.token = $transition$.params().token;
+    $scope.token = decodeURIComponent($transition$.params().token);
 
     PasswordReset.openResetConfirm($scope);
     $location.url('/');
