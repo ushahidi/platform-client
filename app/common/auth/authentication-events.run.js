db5a1b7a498202ee24c152e9428c8c6d2038a463
Robbie Mackay <rm@robbiemackay.com>
2018-10-31 14:05:41 +1300

    refactor: Remove unused redirect param from doLogout
    Remove unused redirect param from doLogout and clean up comments

--- app/common/auth/authentication-events.run.js
+++ app/common/auth/authentication-events.run.js
@@ -117,17 +117,13 @@ function AuthenticationEvents($rootScope, $location, Authentication, Session, _,
             });
     }
 
-    function doLogout(redirect) {
+    function doLogout() {
         $rootScope.currentUser = null;
         $rootScope.loggedin = false;
         DemoDeploymentService.demoCheck();
-        // we don' wat to reload until after filters are correctly set with the backend default that the user
-        // would get when logged out
+        // we don't want to reload until after filters are correctly set with
+        // the backend default that the user would get when logged out
         PostFilters.resetDefaults().then(function () {
-            if (redirect) {
-                $location.url(redirect);
-            }
-
             $state.go($state.$current.name ? $state.$current : 'map', null, { reload: true });
         });
     }
