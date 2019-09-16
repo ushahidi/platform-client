46fa04e321a6fd42bffc1adea33905d190be3896
Anna <anna@ushahidi.com>
2019-09-16 10:29:01 +0200

    removing admin-check

--- app/common/verifier/verifier.service.js
+++ app/common/verifier/verifier.service.js
@@ -11,7 +11,7 @@ function (
         debugModeCheck: function () {
                 Verifier.checkDebugMode(BACKEND_URL)
                     .then(function (result) {
-                        if (result && $rootScope.isAdmin()) {
+                        if (result) {
                             Notify.notifyPermanent(`You have debug-mode switched on, for security reasons, we recommend NOT leaving this check routine enabled in the API. 
                         You may disable the check by running the "composer installdebug:disable" command in the API folder.`);
                         }
