3e279a4c7c546238a719c005002acb899f120cd3
Anna <anna@ushahidi.com>
2019-09-16 10:31:55 +0200

    changing language of message

--- app/common/verifier/verifier.service.js
+++ app/common/verifier/verifier.service.js
@@ -12,7 +12,8 @@ function (
                 Verifier.checkDebugMode(BACKEND_URL)
                     .then(function (result) {
                         if (result) {
-                            Notify.notifyPermanent(`You have debug-mode switched on, for security reasons, we recommend NOT leaving this check routine enabled in the API. 
+                            Notify.notifyPermanent(`You have debug-mode switched on. If you are an admin of this deployment, 
+                            we recommend you disable this check and NOT leaving it enabled in the API. 
                         You may disable the check by running the "composer installdebug:disable" command in the API folder.`);
                         }
                     });
