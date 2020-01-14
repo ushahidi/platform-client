abd1e6cf629b3f3437b8145ae306c03ccde88624
Anna <anna@ushahidi.com>
2020-01-14 11:42:49 +0100

    fix: removing white overlay in user-search

--- app/common/directives/filter-system/filter-searchbar.js
+++ app/common/directives/filter-system/filter-searchbar.js
@@ -13,16 +13,28 @@ function ($timeout) {
             $scope.searchResultsVisible = false;
             $scope.form = formControl;
 
+            $scope.applyFilters = function () {
+                $scope.searchResultsVisible = false;
+            };
+
             $scope.showSearchResults = function () {
                 $scope.searchResultsVisible = true;
             };
 
+            $scope.detectSubmit = function (event) {
+                event.stopPropagation();
+                if (event && event.keyCode === 13) {
+                    $scope.hideSearchResults();
+                } else {
+                    $scope.showSearchResults();
+                }
+            };
+
             $scope.hideSearchResults = function () {
-                $timeout(function () {
-                    $scope.searchResultsVisible = false;
-                }, 100);
+                $scope.searchResultsVisible = false;
             };
         };
+
     return {
         restrict: 'E',
         replace: true,
