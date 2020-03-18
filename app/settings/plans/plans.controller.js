b154353ffe249f5462c3e9b3e1835a18a89e0ed0
Anna <anna@ushahidi.com>
2020-03-18 15:39:06 +0100

    fixing tabs in plans-table

--- app/settings/plans/plans.controller.js
+++ app/settings/plans/plans.controller.js
@@ -38,7 +38,7 @@ function (
     $rootScope.setLayout('layout-c');
     window.scrollTo(0, 0);
     $scope.switchTab = switchTab;
-    $scope.activeTab = 'demo';
+    $scope.activeTab = 'basic';
     $translate('nav.plan_settings').then(function (title) {
         $scope.title = title;
         $rootScope.$emit('setPageTitle', title);
