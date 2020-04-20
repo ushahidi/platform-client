70a89cbcc14c0fe851965002f32ebbbfc6b20b2a
Bryan Mutai <mutaiwork@gmail.com>
2020-04-20 23:39:20 +0300

    move video url validation to PostEditorService and invalidate containing form if validation fails.
    Changes:
    - move video url validation to PostEditService.validateVideoUrl
    - use PostEditService.validateVideoUrl to validate url in directive and setValidity of containing form when validation conditions fail.
    - [test] mock validateVideoUrl & bindAllFunctionsToSelf functions (and fix typo)
    - [test] add PostEditService.validateVideoUrl tests

--- app/main/posts/modify/post-video.directive.js
+++ app/main/posts/modify/post-video.directive.js
@@ -17,6 +17,7 @@ PostVideoController.$inject = [
     '$scope',
     '$sce',
     'Util',
+    'PostEditService',
     'Notify'
 ];
 
@@ -24,15 +25,17 @@ function PostVideoController(
     $scope,
     $sce,
     Util,
+    PostEditService,
     Notify
 ) {
     $scope.constructIframe = constructIframe;
-    // $scope.getVideoThumbnail = getVideoThumbnail;
     activate();
     function activate() {
         // Here we make a statement of trust of the URL based on having previously constructed it
-        $scope.video = $sce.trustAsResourceUrl($scope.videoUrl);
-        $scope.previewId = $scope.videoId ? $scope.videoId : Util.simpluUUID();
+        if (!$scope.$parent.form.$error.videoUrlValidation) {
+            $scope.video = $sce.trustAsResourceUrl($scope.videoUrl);
+            $scope.previewId = $scope.videoId ? $scope.videoId : Util.simpluUUID();
+        }
     }
 
     function constructIframe(videoUrl) {
@@ -40,51 +43,45 @@ function PostVideoController(
 
         var preview = angular.element(document.getElementById($scope.previewId));
 
-        if ($scope.videoUrl === undefined) {
+        if ($scope.$parent.form.$error.videoUrlValidation) {
             preview.empty();
+            if ($scope.videoUrl === undefined) {
+                $scope.$parent.form.$setValidity('videoUrlValidation', true, PostVideoController);
+            }
         } else {
             preview.empty();
-            preview.html(createVideo()).wrap('<div class="video_embed-fluid" />');
+            if ($scope.videoUrl !== undefined) {
+                preview.html(createVideo()).wrap('<div class="video_embed-fluid" />');
+            }
         }
     }
 
     // Originates from PL
     function parseVideo(url) {
-        // - Supported YouTube URL formats:
-        //   - http://www.youtube.com/watch?v=My2FRPA3Gf8
-        //   - http://youtu.be/My2FRPA3Gf8
-        //   - https://youtube.googleapis.com/v/My2FRPA3Gf8
-        // - Supported Vimeo URL formats:
-        //   - http://vimeo.com/25451551
-        //   - http://player.vimeo.com/video/25451551
-        // - Also supports relative URLs:
-        //   - //player.vimeo.com/video/25451551
         if (url) {
             // NOTE: It is very important to pay special attention to the santization needs of this regex if it is changed.
             // It is important that it does not allow subdomains other than player or www in order to ensure that a malicious user
             // can not exploit this field to insert malicious content in an iframe
-            var match = url.match(/(http:|https:|)\/\/(player.|www.)?(vimeo\.com|youtu(be\.com|\.be|be\.googleapis\.com))\/(video\/|embed\/|watch\?v=|v\/)?([A-Za-z0-9._%-]*)(\&\S+)?/);
-            $scope.videoUrl = undefined;
+            var match = PostEditService.validateVideoUrl(url);
             if (match) {
                 if (match[3].indexOf('youtu') > -1) {
+                    $scope.$parent.form.$setValidity('videoUrlValidation', true, PostVideoController)
                     // Here we make a statement of trust of the URL based on having pulled out just the id
                     $scope.videoUrl = 'https://www.youtube.com/embed/' + match[6];
                     $scope.video = $sce.trustAsResourceUrl($scope.videoUrl);
                 } else if (match[3].indexOf('vimeo') > -1) {
+                    $scope.$parent.form.$setValidity('videoUrlValidation', true, PostVideoController)
                     // Here we make a statement of trust of the URL based on having pulled out just the id
                     $scope.videoUrl = 'https://player.vimeo.com/video/' + match[6];
                     $scope.video = $sce.trustAsResourceUrl($scope.videoUrl);
-                } else {
-                    urlError(url);
                 }
             } else {
-                urlError(url);
+                $scope.$parent.form.$setValidity('videoUrlValidation', false, PostVideoController)
+                Notify.error('notify.video.incorrect_url', { url: url });
             }
+        } else {
+            $scope.videoUrl = undefined;
         }
-    }
-
-    function urlError(url) {
-        Notify.error('notify.video.incorrect_url', {url: url});
     }
 
     // Originates from PL
@@ -92,17 +89,4 @@ function PostVideoController(
         return '<iframe src="' + $scope.video + '" frameborder="0" allowfullscreen>';
     }
 
-    // // Originates from PL
-    // function getVideoThumbnail(url, cb) {
-    //     // Obtains the video's thumbnail and passed it back to a callback function.
-    //     var videoObj = parseVideo(url);
-    //     if (videoObj.type === 'youtube') {
-    //         cb('//img.youtube.com/vi/' + videoObj.id + '/maxresdefault.jpg');
-    //     } else if (videoObj.type === 'vimeo') {
-    //         // Requires jQuery
-    //         $.get('http://vimeo.com/api/v2/video/' + videoObj.id + '.json', function(data) {
-    //             cb(data[0].thumbnail_large);
-    //         });
-    //     }
-    // }
 }
