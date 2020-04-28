dc683fca833c4956618b28d4d5e888561cf3abd9
himil-vasava <vasava_h@cs.iitr.ac.in>
2020-04-28 20:23:03 +0530

    Sorting users alphabetically

--- app/settings/users/filter-users.directive.js
+++ app/settings/users/filter-users.directive.js
@@ -24,6 +24,7 @@ function (
             _
         ) {
             $scope.filtersMenuOpen = false;
+            $scope.sortalph="realname";
             $scope.cancel = function () {
                 // Reset filters
                 $scope.usersFiltersForm.$rollbackViewValue();
