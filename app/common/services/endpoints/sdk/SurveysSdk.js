80514b0378b92afbe2bf7370b29b36a9740eca57
Romina <rowasc@gmail.com>
2020-05-12 10:45:55 -0300

    Added setToken - lets hope this works?

--- app/common/services/endpoints/sdk/SurveysSdk.js
+++ app/common/services/endpoints/sdk/SurveysSdk.js
@@ -11,11 +11,15 @@ function (
     const token = Session.getSessionDataEntry('accessToken');
     const ushahidi = new UshahidiSdk.Surveys(Util.url(''), token);
     const getSurveys = function(id) {
-        return ushahidi.getSurveys(id);
+        return ushahidi
+                    .setToken(Session.getSessionDataEntry('accessToken'))
+                    .getSurveys(id);
     }
 
     const saveSurvey = function(survey) {
-        return ushahidi.saveSurvey(survey);
+        return ushahidi
+            .setToken(Session.getSessionDataEntry('accessToken'))
+            .saveSurvey(survey);
     }
 
     return {getSurveys, saveSurvey};
