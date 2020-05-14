6008b9d2d111bcab4822933b6164aa0b72d7b2f2
Anna <anna@ushahidi.com>
2020-05-14 09:55:48 +0200

    TRANSLATIONS: Fix errors and cancel for add-language-modal

--- app/common/directives/add-language.directive.js
+++ app/common/directives/add-language.directive.js
@@ -16,15 +16,25 @@ function AddLanguageController($rootScope, $scope, ModalService) {
     $scope.selectLanguage = selectLanguage;
     $scope.selectedLanguage = '';
     $scope.add = add;
+    $scope.cancel = cancel;
+
     function selectLanguage(language) {
-        if ($scope.survey.enabled_languages.available.indexOf(language) > 1 || $scope.survey.enabled_languages.default === language) {
+        if ($scope.survey.enabled_languages.available.indexOf(language) > -1 || $scope.survey.enabled_languages.default === language) {
             $scope.showLangError = true;
+            $scope.langError = 'You cannot select this language since ';
+            $scope.langError = $scope.survey.enabled_languages.default === language ?  `${$scope.langError} it is the default language for this survey.` : `${$scope.langError} there is already a translation for it.`;
         } else {
             $scope.showLangError = false;
         }
     }
-
+    function cancel() {
+        ModalService.close();
+    }
     function add() {
+        if (!$scope.selectedLanguage) {
+            $scope.langError = 'You need to select a language first';
+            $scope.showLangError = true;
+        }
         if (!$scope.showLangError) {
             $scope.switchToLanguage($scope.selectedLanguage);
             $scope.survey.enabled_languages.available.push($scope.selectedLanguage);
