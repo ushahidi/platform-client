b22bfb23d334f4d480377397c4f658a9661fbb96
Anna <anna@ushahidi.com>
2020-06-18 19:49:19 +0200

    TRANSLATIONS:displaying translated values in data-detail-field

--- app/main/posts/detail/post-value.directive.js
+++ app/main/posts/detail/post-value.directive.js
@@ -4,9 +4,9 @@ module.exports = ['PostEndpoint', 'moment', '_','PostsSdk', function (PostEndpoi
         replace: true,
         scope: {
             key: '=',
-            value: '=',
             attribute: '=',
-            type: '='
+            type: '=',
+            activeLanguage: '='
         },
         template: require('./post-value.html'),
         link: function ($scope) {
@@ -14,29 +14,34 @@ module.exports = ['PostEndpoint', 'moment', '_','PostsSdk', function (PostEndpoi
             // Depending on whether we are dealing with a post task or a standard task
             // the css class is swapped. This Boolean manages that distinction.
             $scope.standardTask = $scope.type === 'standard';
-
+            $scope.isText = isText;
             if ($scope.attribute.type === 'relation') {
-                PostsSdk.getPosts($scope.value).then(post=>{
-                    $scope.value = post;
+                PostsSdk.getPosts($scope.attribute.type).then(post=>{
+                    $scope.attribute.value = post;
                     $scope.$apply();
                 });
 
-            } if ($scope.attribute.type === 'varchar' || $scope.attribute.type === 'title' || $scope.attribute.type === 'description' || $scope.attribute.type === 'text') {
-                $scope.value = [$scope.value];
             }
+            function isText(type) {
+                if ($scope.attribute.type === 'varchar' || $scope.attribute.type === 'title' || $scope.attribute.type === 'description' || $scope.attribute.type === 'text' || $scope.attribute.type === 'markdown') {
+                    return true;
+                }
+                return false;
+            }
+
             if ($scope.attribute.type === 'datetime') {
                 if ($scope.attribute.input === 'date') {
-                    $scope.value = $scope.value.map(function (entry) {
+                    $scope.attribute.value = $scope.attribute.value.map(function (entry) {
                         return moment(entry).format('LL');
                     });
                 }
                 if ($scope.attribute.input === 'datetime') {
-                    $scope.value = $scope.value.map(function (entry) {
+                    $scope.attribute.value = $scope.attribute.value.map(function (entry) {
                         return moment(entry).format('LLL');
                     });
                 }
                 if ($scope.attribute.input === 'time') {
-                    $scope.value = $scope.value.map(function (entry) {
+                    $scope.attribute.value = $scope.attribute.value.map(function (entry) {
                         return moment(entry).format('LT');
                     });
                 }
