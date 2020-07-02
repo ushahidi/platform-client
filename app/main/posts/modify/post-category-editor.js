8b6a1976a24723881ee64e93660afb4130a90c33
Anna <anna@ushahidi.com>
2020-07-02 08:18:09 +0200

    TRANSLATIONS: Adding a selectAll button to categories-select

--- app/main/posts/modify/post-category-editor.js
+++ app/main/posts/modify/post-category-editor.js
@@ -21,14 +21,16 @@ function PostCategoryEditorDirective() {
 PostCategoryEditorController.$inject = ['$rootScope','$scope', 'TagEndpoint', 'FormAttributeEndpoint', 'Notify', '_'];
 
 function PostCategoryEditorController($rootScope, $scope, TagEndpoint, FormAttributeEndpoint, Notify, _) {
-    $scope.changeParent = changeParent;
+    $scope.toggleParent = toggleParent;
+    $scope.selectAll = selectAll;
+    $scope.isSelectAll = false;
 
     function activate () {
             groupCategories();
             convertSelected();
             // selecting parent if any children are selected
             _.each($scope.parents, parent => {
-                changeParent(parent);
+                toggleParent(parent);
             })
     }
     activate();
@@ -44,16 +46,35 @@ function PostCategoryEditorController($rootScope, $scope, TagEndpoint, FormAttri
         $scope.selected = _.pluck($scope.selected, 'id');
     }
 
-    function changeParent(parent) {
-        let selectedChildren = _.chain(parent.children)
-                .pluck('id')
-                .intersection($scope.selected)
-                .value();
-            if (selectedChildren.length > 0 && $scope.selected.indexOf(parent.id) === -1) {
-                $scope.selected.push(parent.id)
+    function toggleParent(parent, childId) {
+        if (childId) {
+            if ($scope.selected.indexOf(childId) === -1) {
+                $scope.selected.push(childId);
+            } else {
+                $scope.selected.splice($scope.selected.indexOf(childId),1);
             }
-            if (selectedChildren.length === 0 && $scope.selected.indexOf(parent.id) >= 0) {
-                $scope.selected.splice($scope.selected.indexOf(parent.id),1);
-            }
+        }
+
+        if (parent.children) {
+            let selectedChildren = _.chain(parent.children)
+                        .pluck('id')
+                        .intersection($scope.selected)
+                        .value();
+                    if (selectedChildren.length > 0 && $scope.selected.indexOf(parent.id) === -1) {
+                        $scope.selected.push(parent.id)
+                    }
+                    if (selectedChildren.length === 0 && $scope.selected.indexOf(parent.id) >= 0) {
+                        $scope.selected.splice($scope.selected.indexOf(parent.id),1);
+                    }
+                }
+    }
+
+    function selectAll() {
+        if ($scope.selected.length === $scope.available.length) {
+            $scope.selected = [];
+        } else {
+            $scope.selected = angular.copy($scope.available);
+            convertSelected();
+        }
     }
 }
