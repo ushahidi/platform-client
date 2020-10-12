e65530d29bd355fc3b23856ff64ef7bea19efa9d
renu <renudjain31@gmail.com>
2020-10-12 17:48:44 +0530

    Addes warnings in  case of identical options in checkbox

--- app/settings/surveys/attribute-editor.directive.js
+++ app/settings/surveys/attribute-editor.directive.js
@@ -76,6 +76,20 @@ function (
             $scope.canDisableCaption = function () {
                 return $scope.editAttribute.type === 'media' && $scope.editAttribute.input === 'upload';
             };
+
+            $scope.valuesPermissible = function () {
+                if ($scope.editAttribute.options.length < 1) {
+                    return true;
+                }
+                let tmp = [];
+                for (let x of $scope.editAttribute.options) {
+                    if (tmp.indexOf(x) !== -1) {
+                        return false;
+                    }
+                    tmp.push(x);
+                }
+                return true;
+            };
         }
     };
 }];
