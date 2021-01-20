8e261c8f6894037fc6f2aa224f06d0a615da0cb4
Romina <rowasc@gmail.com>
2021-01-20 13:04:56 -0300

    fix(Category visibility perms): user no longer overrides categories if they cant see them

--- app/main/posts/detail/post-category-value.directive.js
+++ app/main/posts/detail/post-category-value.directive.js
@@ -16,6 +16,11 @@ function PostCategoryValue() {
 PostCategoryValueController.$inject = ['$scope', '_', 'CategoriesSdk'];
 
 function PostCategoryValueController($scope, _, CategoriesSdk) {
+    $scope.display = $scope.categories.filter(f => {
+        if (_.isUndefined(f._ush_hidden)) {
+            return f;
+        }
+    });
     function activate() {}
     activate();
 }
