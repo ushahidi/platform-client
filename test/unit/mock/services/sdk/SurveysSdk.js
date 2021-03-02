7b0f53af35d41eb9d8cac2653a829da97a6db0e9
Anna <anna@ushahidi.com>
2021-03-02 08:13:48 +0100

    Tests (mode-context): Using correct mock

--- test/unit/mock/services/sdk/SurveysSdk.js
+++ test/unit/mock/services/sdk/SurveysSdk.js
@@ -35,39 +35,27 @@ module.exports = [function () {
                     }
                 }
             }},
-        getSurveysTo: function (id, reason) {
+        getSurveysTo: function (reason) {
             return {
                 then: function (successCallback, failCallback) {
-                    if (id) {
-                        successCallback({
+                    successCallback([
+                        {
                             'id': 1,
                             'enabled_languages': {'default': 'en','available' : ['es']},
                             'type': 'report',
-                            'name':'test form',
-                            'description':'Testing form',
-                            'translations':{'es':{'name':'','description':''}},
-                            'tasks': [{'id': 1,'fields': [{'id':1}, {'id':2}]}]
-                        });
-                    } else {
-                        successCallback([
-                            {
-                                'id': 1,
-                                'enabled_languages': {'default': 'en','available' : ['es']},
-                                'type': 'report',
-                                'name':'test form 1',
-                                'description':'Testing form 1',
-                                'translations':{'es':{'name':'Testing form in spanish 1','description':'Testing description in spanish 1'}}
-                            },
-                            {
-                                'id': 2,
-                                'enabled_languages': {'default': 'en','available' : ['it']},
-                                'type': 'report',
-                                'name':'test form 2',
-                                'description':'Testing form 2',
-                                'translations':{'it':{'name':'Testing form in italian 2','description':'Testing description in italian 2'}}
-                            }
-                        ]);
-                    }
+                            'name':'test form 1',
+                            'description':'Testing form 1',
+                            'translations':{'es':{'name':'Testing form in spanish 1','description':'Testing description in spanish 1'}}
+                        },
+                        {
+                            'id': 2,
+                            'enabled_languages': {'default': 'en','available' : ['it']},
+                            'type': 'report',
+                            'name':'test form 2',
+                            'description':'Testing form 2',
+                            'translations':{'it':{'name':'Testing form in italian 2','description':'Testing description in italian 2'}}
+                        }
+                    ]);
                 }
             }
         },
