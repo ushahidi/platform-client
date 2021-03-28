9e747bcdcbaa2422fbda9dc642b1774f21fa29bf
Emmanuelhashy <emmanuelhashy@gmail.com>
2021-03-28 03:29:04 +0100

    Add keyboard accessibility to the 'who can see this' section in the edit collection modal

--- app/common/directives/role-selector.directive.js
+++ app/common/directives/role-selector.directive.js
@@ -37,10 +37,12 @@ function RoleSelectorController($scope, RoleEndpoint, $translate, _) {
         if (_.indexOf($scope.model.role, admin.name) === -1) {
             $scope.model.role.push(admin.name);
         }
+        document.getElementById('add_roles').click();
     }
 
     // adding all available roles to model if user clicks 'Everyone'
     function setEveryone() {
         $scope.model.role = null;
+        document.getElementById('add_everyone').click();
     }
 }
