e679e4f64712c05026829964c83e1887ebf19be6
Anna <anna@ushahidi.com>
2021-05-20 13:22:12 +0200

    Migration(config): Setting bootstrap-config in legacy-app instead of in root-config

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -1,7 +1,8 @@
 require('./app.js');
 import singleSpaAngularJS from 'single-spa-angularjs';
+import fetch from 'cross-fetch';
 
-//exporting lifecycle-functions for angular-app, previous config is set in root-config
+//exporting lifecycle-functions for angular-app
 const ngLifecycles = singleSpaAngularJS({
     angular: angular,
     mainAngularModule: 'app',
@@ -12,5 +13,18 @@ const ngLifecycles = singleSpaAngularJS({
     });
 
 export const bootstrap = ngLifecycles.bootstrap;
-export const mount = ngLifecycles.mount;
+
+export const mount = (opts, mountedInstances, props) => {
+    if (window.ushahidi && window.ushahidi.backendUrl && !window.ushahidi.bootstrapConfig) {
+       return fetch(window.ushahidi.backendUrl, response => {
+        return response.json();
+    }).then(config => {
+        // setting config
+        window.ushahidi.bootstrapConfig = config;
+        return ngLifecycles.mount(opts, mountedInstances, props);
+    });
+    } else {
+        return ngLifecycles.mount(opts, mountedInstances, props);
+    }
+}
 export const unmount = ngLifecycles.unmount;
--- rootconfig/src/ushahidi-root-config.js
+++ rootconfig/src/ushahidi-root-config.js
@@ -5,18 +5,8 @@ import {
     constructLayoutEngine,
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
-import fetch from "cross-fetch";
 require('ushahidi-platform-pattern-library/assets/fonts/Lato/css/fonts.css');
-
 const routes = constructRoutes(microfrontendLayout);
-
-fetch("https://annatest1000.api.ushahidi.io/api/v3/config", (response) => {
-    return response.json();
-}).then((config) => {
-    // setting config
-    let wi = window;
-    wi.ushahidi = wi.ushahidi || {};
-    window.ushahidi.bootstrapConfig = config;
 
     const applications = constructApplications({
         routes,
@@ -28,5 +18,5 @@ fetch("https://annatest1000.api.ushahidi.io/api/v3/config", (response) => {
 
     applications.forEach(registerApplication);
     layoutEngine.activate();
+    
     start();
-});
