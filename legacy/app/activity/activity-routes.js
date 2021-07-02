d039da88bedc435ae25bfb329e3d5451d8f74375
ifycode <marychord@gmail.com>
2021-07-02 02:00:50 -0800

    Move activity-routes-lazyload.js content to activity-routes.js

--- legacy/app/activity/activity-routes.js
+++ legacy/app/activity/activity-routes.js
@@ -1,3 +1,26 @@
 angular.module('ushahidi.activity.routes', [])
 
-.config(require('./activity-routes-lazyload.js'));
+.config([
+    '$stateProvider',
+    '$urlMatcherFactoryProvider',
+    function (
+        $stateProvider,
+        $urlMatcherFactoryProvider
+    ) {
+        $urlMatcherFactoryProvider.strictMode(false);
+
+        $stateProvider
+        .state({
+            name: 'activity',
+            url: '/activity',
+            controller: require('./activity.controller.js'),
+            template: require('./activity.html'),
+            lazyLoad: function ($transition$) {
+                const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+                return System.import('/activity.js').then(mod => {
+                    $ocLazyLoad.load(mod.ACTIVITY_MODULE);
+                });
+            }
+        });
+    }]
+);
