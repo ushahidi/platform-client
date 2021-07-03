d92e6211b3f73acee8d962b1b788009f8bc35fff
ifycode <marychord@gmail.com>
2021-07-02 23:50:23 -0800

    Delete settings.route-lazyload, move its content to settings.route

--- legacy/app/settings/settings.routes.js
+++ legacy/app/settings/settings.routes.js
@@ -1,3 +1,28 @@
 angular.module('ushahidi.settings.routes', [])
 
-.config(require('./settings.route-lazyload.js'));
+.config([
+    '$stateProvider',
+    '$urlMatcherFactoryProvider',
+    function ($stateProvider, $urlMatcherFactoryProvider) {
+        $urlMatcherFactoryProvider.strictMode(false);
+        /* todo: these routes should only exist when the user is admin! */
+        $stateProvider
+            .state({
+                name: 'settings',
+                controller: require('./settings.controller.js'),
+                template: require('./settings.html')
+            })
+            .state({
+                name: 'settings.list',
+                url: '/settings',
+                template: require('./settings-list.html'),
+                controller: require('./settings-list.controller.js'),
+                lazyLoad: function ($transition$) {
+                    const $ocLazyLoad = $transition$.injector().get('$ocLazyLoad');
+                    return System.import('/settings.js').then(mod => {
+                        $ocLazyLoad.load(mod.SETTINGS_MODULE);
+                    });
+                }
+            })
+    }
+]);
