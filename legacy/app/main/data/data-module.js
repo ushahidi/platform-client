ed736d502f37743552b7e76081fc416405e34783
Ifycode <marychord@gmail.com>
2021-07-23 04:52:15 +0100

    Re-group data-module content

--- legacy/app/main/data/data-module.js
+++ legacy/app/main/data/data-module.js
@@ -1,27 +1,27 @@
 angular.module('ushahidi.data', [])
 
+// Post-view-data
+.directive('postViewData', require('./post-view-data.directive.js'))
+
+// Post card related
+.directive('postCard', require('./post-card/post-card.directive.js'))
+.directive('postPreviewMedia', require('./post-card/post-preview-media.directive.js'))
+.directive('postActions', require('./post-card/post-actions.directive.js'))
+.service('PostActionsService', require('./post-card/post-actions.service.js'))
+.directive('postMetadata', require('./post-card/post-metadata.directive.js'))
+.service('PostMetadataService', require('./post-card/post-metadata.service.js'))
+.directive('collectionToggleLink', require('./post-card/collection-toggle/collection-toggle-link.js'))
+
 // Shared directives + services
-.directive('postActions', require('./common/post-actions.directive.js'))
-.directive('postMetadata', require('./common/post-metadata.directive.js'))
-.service('PostMetadataService', require('./common/post-metadata.service.js'))
-.service('PostSurveyService', require('./common/post-survey.service.js'))
+.service('PostSurveyService', require('./no-groups-yet/post-survey.service.js'))
 
 // Post Locking
-.service('PostLockService', require('./common/post-lock.service.js'))
-.directive('postLock', require('./common/post-lock.directive.js'))
-
-// Detail Screen
-.service('PostActionsService', require('./common/post-actions.service.js'))
+.service('PostLockService', require('./no-groups-yet/post-lock.service.js'))
+.directive('postLock', require('./no-groups-yet/post-lock.directive.js'))
 
 // Post editing workflows
-.directive('surveyLanguageSelector', require('./common/survey-language-selector.directive.js'))
+.directive('surveyLanguageSelector', require('./no-groups-yet/survey-language-selector.directive.js'))
 
-// Timeline, data and Map screen
-.directive('postCard', require('./views/post-card.directive.js'))
-.directive('postPreviewMedia', require('./views/post-preview-media.directive.js'))
-.directive('postViewData', require('./views/post-view-data.directive.js'))
-
-.directive('collectionToggleButton', require('./common/collection-toggle/collection-toggle-button.js'))
-.directive('collectionToggleLink', require('./common/collection-toggle/collection-toggle-link.js'))
+.directive('collectionToggleButton', require('./no-groups-yet/collection-toggle/collection-toggle-button.js'))
 
 .config(require('./data-routes.js'))
