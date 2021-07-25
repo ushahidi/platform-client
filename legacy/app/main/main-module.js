f447aae4be1881d027e3b385ec1b48eb94e7e383
Ifycode <marychord@gmail.com>
2021-07-25 19:50:12 +0100

    Isolate data module

--- legacy/app/main/main-module.js
+++ legacy/app/main/main-module.js
@@ -1,9 +1,7 @@
 angular.module('ushahidi.main', [
-    'ushahidi.data',
     'ushahidi.posts',
     'ushahidi.donation'
 ]);
 
-require('./data/data-module.js');
 require('./posts/posts-module.js');
 require('./donation/donation-module.js');
