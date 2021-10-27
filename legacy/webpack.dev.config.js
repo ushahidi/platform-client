1989d634ab35d08c0c82a9a7b19cf8c47c5d63ad
Anna <anna@ushahidi.com>
2021-10-27 10:21:12 +0200

    Fix(dev-env): Adding correct paths to dayjs and locales in dev-environment

--- legacy/webpack.dev.config.js
+++ legacy/webpack.dev.config.js
@@ -8,7 +8,13 @@ config.devServer = {
   headers: {
     'Access-Control-Allow-Origin': '*'
   },
-  stats: 'errors-only'
+  stats: 'errors-only',
+  writeToDisk: true
 
 }
+// this is modules required within the code (for example dayjs),
+// needed for proxy in dev-environment
+config.output.chunkFilename = 'legacy-modules/[name].js'
+
+ 
 module.exports = config;
