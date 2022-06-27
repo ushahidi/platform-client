0a773d27c2e3df24c78fa6b3e6131adb795c84ec
David Losada Carballo <david@tuxpiper.com>
2022-06-27 13:38:03 +0200

    chore: temporarily disable custom gtm and datalayer event handling

--- root/src/ushahidi-root-config.js
+++ root/src/ushahidi-root-config.js
@@ -6,7 +6,7 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 import { getPageMetadata, setBootstrapConfig } from "@ushahidi/utilities";
-import { datalayer } from "./datalayer.js";
+// import { datalayer } from "./datalayer.js";
 
 require("./loading.scss");
 
@@ -25,7 +25,7 @@ const layoutEngine = constructLayoutEngine({ routes, applications });
 
 applications.forEach(registerApplication);
 layoutEngine.activate();
-datalayer.initialize();
+// datalayer.initialize();
 
 const showError = function (show) {
     const element = document.querySelector('#bootstrap-error');
