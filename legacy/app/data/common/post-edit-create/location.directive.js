760d95c4c5b72315cfae17e3c18c2be179a1a07b
John Kamau <chegejohn159@gmail.com>
2022-08-16 11:14:33 +0300

    remove package-lock.json and clean the filter function

--- legacy/app/data/common/post-edit-create/location.directive.js
+++ legacy/app/data/common/post-edit-create/location.directive.js
@@ -1,26 +1,46 @@
 module.exports = PostLocationDirective;
 
-PostLocationDirective.$inject = ['$document', '$http', 'Leaflet', 'Geocoding', 'Maps', '_', 'Notify', '$window'];
-function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, $window) {
+PostLocationDirective.$inject = [
+    "$document",
+    "$http",
+    "Leaflet",
+    "Geocoding",
+    "Maps",
+    "_",
+    "Notify",
+    "$window",
+];
+function PostLocationDirective(
+    $document,
+    $http,
+    L,
+    Geocoding,
+    Maps,
+    _,
+    Notify,
+    $window
+) {
     return {
-        restrict: 'E',
+        restrict: "E",
         replace: true,
-        require: 'ngModel',
+        require: "ngModel",
         scope: {
-            id: '@',
-            name: '@',
-            required: '='
+            id: "@",
+            name: "@",
+            required: "=",
         },
-        template: require('./location.html'),
-        link: PostLocationLink
+        template: require("./location.html"),
+        link: PostLocationLink,
     };
 
     function PostLocationLink($scope, element, attrs, ngModel) {
-        var currentPositionControl, map, marker,
+        var currentPositionControl,
+            map,
+            marker,
             zoom = 8;
 
         $scope.processing = false;
-        $scope.searchLocationTerm = '';
+        $scope.searchLocationTerm = "";
         $scope.searchLocation = searchLocation;
         $scope.manualModel = { lat: null, lon: null };
         $scope.searchTimeout;
@@ -41,9 +61,9 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
         activate();
 
         function activate() {
-
-            Maps.createMap(element[0].querySelector('.map'))
-            .then(function (data) {
+            Maps.createMap(element[0].querySelector(".map")).then(function (
+                data
+            ) {
                 map = data;
 
                 // Init marker with current model value
@@ -52,27 +72,34 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
                 // Update Map if model changes
                 ngModel.$render = renderViewValue;
 
-                map.on('click', onMapClick);
+                map.on("click", onMapClick);
                 // treat locationfound same as map click
-                map.on('locationfound', onMapClick);
+                map.on("locationfound", onMapClick);
                 // handle failure to find location
-                map.on('locationerror', onLocationError);
+                map.on("locationerror", onLocationError);
                 // Add locate control, but only on https
-                if (window.location.protocol === 'https:' || window.location.hostname === 'localhost') {
+                if (
+                    window.location.protocol === "https:" ||
+                    window.location.hostname === "localhost"
+                ) {
                     $scope.showCurrentPositionControl = true;
-                    currentPositionControl = L.control.locate({
-                        locateOptions: {
-                            maximumAge: 60000 // 1 minute
-                        }
-                    }).addTo(map);
+                    currentPositionControl = L.control
+                        .locate({
+                            locateOptions: {
+                                maximumAge: 60000, // 1 minute
+                            },
+                        })
+                        .addTo(map);
                 }
             });
 
-            $document.on('click', onDocumentClick);
+            $document.on("click", onDocumentClick);
         }
 
         function onDocumentClick(event) {
-            if (!element[0].querySelector('.searchbar').contains(event.target)) {
+            if (
+                !element[0].querySelector(".searchbar").contains(event.target)
+            ) {
                 $scope.hideSearchResults();
             }
         }
@@ -89,24 +116,31 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
         }
 
         function onLocationError() {
-            Notify.error('location.my_location_error');
+            Notify.error("location.my_location_error");
         }
 
         function renderViewValue() {
-            if (ngModel.$viewValue &&
-                typeof ngModel.$viewValue.lat !== 'undefined' &&
-                typeof ngModel.$viewValue.lon !== 'undefined'
+            if (
+                ngModel.$viewValue &&
+                typeof ngModel.$viewValue.lat !== "undefined" &&
+                typeof ngModel.$viewValue.lon !== "undefined"
             ) {
-                updateMarkerPosition(ngModel.$viewValue.lat, ngModel.$viewValue.lon);
+                updateMarkerPosition(
+                    ngModel.$viewValue.lat,
+                    ngModel.$viewValue.lon
+                );
                 centerMapTo(ngModel.$viewValue.lat, ngModel.$viewValue.lon);
-                updateManualLatLon(ngModel.$viewValue.lat, ngModel.$viewValue.lon);
+                updateManualLatLon(
+                    ngModel.$viewValue.lat,
+                    ngModel.$viewValue.lon
+                );
             }
         }
 
         function updateModelLatLon(lat, lon) {
             ngModel.$setViewValue({
                 lat: lat,
-                lon: lon
+                lon: lon,
             });
             updateManualLatLon(lat, lon);
         }
@@ -126,13 +160,25 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
             // Only show button if the field is dirty
             if ($scope.manualPos.$dirty) {
                 // if no ngModel but valid values in the fields, show button
-                if (!ngModel.$viewValue || !ngModel.$viewValue.lat || !ngModel.$viewValue.lon) {
-                    return isLatitude($scope.manualModel.lat) && isLongitude($scope.manualModel.lon)
+                if (
+                    !ngModel.$viewValue ||
+                    !ngModel.$viewValue.lat ||
+                    !ngModel.$viewValue.lon
+                ) {
+                    return (
+                        isLatitude($scope.manualModel.lat) &&
+                        isLongitude($scope.manualModel.lon)
+                    );
                 }
                 // if valid values in the field that is different to ngModel, show button
-            return isLatitude($scope.manualModel.lat) && isLongitude($scope.manualModel.lon) && $scope.manualModel.lat !== ngModel.$viewValue.lat || $scope.manualModel.lon !== ngModel.$viewValue.lon;
+                return (
+                    (isLatitude($scope.manualModel.lat) &&
+                        isLongitude($scope.manualModel.lon) &&
+                        $scope.manualModel.lat !== ngModel.$viewValue.lat) ||
+                    $scope.manualModel.lon !== ngModel.$viewValue.lon
+                );
+            }
         }
-    }
 
         function updateManualLatLon(lat, lon) {
             $scope.manualModel.lat = lat;
@@ -144,7 +190,7 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
                 updateMarkerPosition(lat, lon);
                 centerMapTo(lat, lon);
                 updateModelLatLon(lat, lon);
-            };
+            }
         }
 
         function updateMarkerPosition(lat, lon) {
@@ -153,11 +199,11 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
             } else {
                 marker = L.marker([lat, lon], {
                     draggable: true,
-                    icon: Maps.pointIcon()
+                    icon: Maps.pointIcon(),
                 });
                 marker.addTo(map);
 
-                marker.on('dragend', function (ev) {
+                marker.on("dragend", function (ev) {
                     $scope.$apply(() => {
                         var latLng = ev.target.getLatLng();
                         updateModelLatLon(latLng.lat, latLng.lng);
@@ -180,37 +226,41 @@ function PostLocationDirective($document, $http, L, Geocoding, Maps, _, Notify, 
 
         // for dropdown
         function showSearchResults() {
-            element[0].querySelector('#searchbar-results').classList.add('active');
+            element[0]
+                .querySelector("#searchbar-results")
+                .classList.add("active");
         }
 
         function hideSearchResults() {
-            element[0].querySelector('#searchbar-results').classList.remove('active');
+            element[0]
+                .querySelector("#searchbar-results")
+                .classList.remove("active");
         }
-        
+
         function handleActiveSearch(event) {
             if (event.keyCode === 13) {
+                event.preventDefault();
                 $scope.processing = true;
                 if ($scope.searchTimeout) {
                     clearTimeout($scope.searchTimeout);
                 }
                 $scope.searchTimeout = setTimeout($scope.searchLocation, 250);
-            }
-            if (event.keyCode === 13) {
-                event.preventDefault();
                 return false;
             }
         }
 
         function searchLocation() {
             $scope.processing = true;
-            Geocoding.searchAllInfo($scope.searchLocationTerm).then(function (results) {
+            Geocoding.searchAllInfo($scope.searchLocationTerm).then(function (
+                results
+            ) {
                 $scope.processing = false;
                 $scope.searchResults = results;
             });
         }
 
         function chooseLocation(location) {
-            $scope.searchLocationTerm = '';
+            $scope.searchLocationTerm = "";
             updateModelLatLon(location.lat, location.lon);
             updateMarkerPosition(location.lat, location.lon);
             centerMapTo(location.lat, location.lon);
